506b67624f96b8d5aa408a1c95e67becbdb01cb1
DecathectZero <tylerhan97@gmail.com>
2018-10-28 15:22:24 -0400

    sidebar stuff

--- app/src/views/pages/Storyboard/Menu.js
+++ app/src/views/pages/Storyboard/Menu.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import MenuItem from './MenuItem';
+import { Link } from 'react-router-dom'
 
 const sections = [{
     title: 'Basic',
@@ -24,20 +25,69 @@ const sections = [{
                    // <div className="mt-5">
                      //   <small><a href="https://getstoryflow.com">STORY SCHOOL <i className="fas fa-question-circle"/></a></small>
                    // </div>
+
+const tabs = [
+    {tab: "blocks", icon: <i className="fas fa-plus-square"/>},
+    {tab: "flows", icon: <i className="fas fa-clone"/>}
+]
+
 class Menu extends PureComponent {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            open: true,
+            tab: 'blocks'
+        }
+
+        this.openTab = this.openTab.bind(this);
+    }
+
+    openTab(tab) {
+        this.setState({
+            open: true,
+            tab: tab
+        });
+    }
+
+
+
+                // <div className='no-select'>
+
+                // </div>
 
     render() {
         return (
-            <div className='Menu no-select'>
-                <div className='sidebar'>
+            <div className="Menu">
+                <div className='toolbar'>
+                    <div className="top-down">
+                        {tabs.map(tab => {
+                            return (
+                                <div className="tool" onClick={() => this.openTab(tab.tab)}>
+                                    {tab.icon}
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className="spacer"/>
+                    <div className="bottom-up">
+                        <a className="tool no-underline" href="https://getstoryflow.com/storyschool" 
+                        target="_blank" rel="noopener noreferrer">
+                            <i className="fas fa-graduation-cap"/>
+                        </a>
+                        <div className="tool" onClick={this.props.helpModal}>
+                            <i className="fas fa-question-circle"/>
+                        </div>
+                    </div>
+                </div>
+                <div id="sidebar" className={this.state.open ? 'open' : ''}>
                     <p className="saved">{this.props.lastSave}</p>
                     <hr/>
                     <div className='block-title'>
-                        <h5 onClick={this.props.helpModal}>Blocks <small><i className="far fa-question-circle"/></small></h5>
-                        <p className="font-weight-light">Drag-n-drop blocks into your flow <i className="far fa-arrow-right"/></p>
+                        <div className="" onClick={() => this.setState({open: false})}>
+                            <h5 className="mb-0">Blocks</h5>
+                            <div className="close pr-1 pl-3 py-3">×</div>
+                        </div>
                     </div>
                     {sections.map((section, i) => {
                         return <div key={i} className="section">

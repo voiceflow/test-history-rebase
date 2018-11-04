a0337ea5b0232d0575740e8645977c8640c1c6d4
groovy-tunes <chinsenjeremy@gmail.com>
2018-11-04 15:39:30 -0500

    Started Marketplace

---
+++ src/views/pages/Marketplace/Marketplace.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Marketplace extends Component {
+	constructor(props){
+		super(props);
+
+		console.log("Yeet");
+	}
+
+	render() {
+		return (
+			<div>
+				HWHEHEHHE
+			</div>
+		)
+	}
+}
+
+export default Marketplace;

45b2a5a2c4ab5880e16e7117c5df81e41cb327d4
DecathectZero <tylerhan97@gmail.com>
2018-11-04 22:51:09 -0500

    new speak block

--- build/service-worker.js
+++ build/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d38d922542037a32b50d6483f14780d3.js"
+  "/precache-manifest.274bfc127963d37921a72fc21e721dd8.js"
 );
 
 workbox.clientsClaim();

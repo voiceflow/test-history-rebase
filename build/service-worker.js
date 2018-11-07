368e414a4eb6e9ecdb8f1ce4480e5c6271c39b60
DecathectZero <tylerhan97@gmail.com>
2018-11-06 20:12:33 -0500

    new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.51aa1ee89d68dac757b1fd80238d5659.js"
+  "/precache-manifest.1cc46b682ac1b5c719919329feb4e6db.js"
 );
 
 workbox.clientsClaim();

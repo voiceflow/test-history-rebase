bf7a9662140f93865bcd09334a353b0f055a86f8
groovy-tunes <chinsenjeremy@gmail.com>
2018-11-07 22:09:21 -0500

    Added backend routes for marketplace and basic buttons to publishing for testing

--- src/views/pages/Storyboard/TitleBar.js
+++ src/views/pages/Storyboard/TitleBar.js
@@ -85,9 +85,7 @@ class TitleBar extends PureComponent {
                 {this.props.preview ? null : 
                     <div className="title-group">
                         <MUIButton variant="contained" className="white-btn save-btn mr-2" onClick={this.props.onSave}>{this.props.saving ? <i className="fas fa-sync-alt fa-spin"/> : "Save Draft"}</MUIButton>
-                        <MUIButton variant="contained" className="white-btn publish-btn" onClick={this.props.publish}>Publish 
-                            <span className="button-circle"><i className="fab fa-amazon"/></span>
-                        </MUIButton>
+                        <MUIButton variant="contained" className="white-btn save-btn" onClick={this.props.publish}>Publish</MUIButton>
                     </div>
                 }
                 <div className="project">

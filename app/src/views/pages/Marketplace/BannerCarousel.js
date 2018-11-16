1dd547a05c0fb4b3dd4bafb6e229dfea23f5bad5
groovy-tunes <chinsenjeremy@gmail.com>
2018-11-16 16:02:27 -0500

    Added background image to jumbotron

--- app/src/views/pages/Marketplace/BannerCarousel.js
+++ app/src/views/pages/Marketplace/BannerCarousel.js
@@ -11,29 +11,47 @@ class BannerCarousel extends Component{
 		super(props);
 
 		this.state = {
-			mIndex: 0
+			mIndex: 0,
+			featured_modules: []
+		}
+	}
+
+	componentWillReceiveProps(next_props){
+		if(this.props !== next_props){
+			this.setState({
+				featured_modules: next_props.featured_modules
+			});
 		}
 	}
 
 	render(){
+		var JumbotronStyle = {};
+		if(this.state.featured_modules.length > 0){
+			JumbotronStyle = {
+				backgroundImage: 'url(' + this.state.featured_modules[this.state.mIndex].banner_img + ')',
+				backgroundSize: 'cover'
+			}
+		}
+		console.log(this.state)
+
 		return (
 			<div className="row">
 				<div className="col-sm">
-					<Button onClick={()=>{this.setState({mIndex: mod(this.state.mIndex - 1, this.props.modules.length)})}}>
+					<Button onClick={()=>{this.setState({mIndex: mod(this.state.mIndex - 1, this.state.featured_modules.length)})}}>
 						<i className="fas fa-angle-left"></i>
 					</Button>
 				</div>
 
-				<Jumbotron fluid>
+				<Jumbotron fluid style={JumbotronStyle}>
 					<Container fluid>
 						<h1 className="display-3">
-							{this.state.mIndex}
+							fdus9ifjdosaj
 						</h1>
 					</Container>
 				</Jumbotron>
 
 				<div className="col-sm">
-					<Button onClick={()=>{this.setState({mIndex: mod(this.state.mIndex + 1, this.props.modules.length)})}}>
+					<Button onClick={()=>{this.setState({mIndex: mod(this.state.mIndex + 1, this.state.featured_modules.length)})}}>
 						<i className="fas fa-angle-right"></i>
 					</Button>
 				</div>

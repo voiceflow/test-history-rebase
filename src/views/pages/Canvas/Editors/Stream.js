e1012efaff556267ce0cba88ab7d27b61846e513
DecathectZero <tylerhan97@gmail.com>
2018-11-20 11:50:01 -0500

    stream block things

--- src/views/pages/Canvas/Editors/Stream.js
+++ src/views/pages/Canvas/Editors/Stream.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Input, InputGroup } from 'reactstrap';
 import AudioDrop from './components/AudioDrop';
 
-const outputs = ['next', 'previous', 'shuffle'];
+const outputs = ['next', 'previous'];
 
 class Stream extends Component {
     constructor(props) {
@@ -11,13 +11,22 @@ class Stream extends Component {
         this.state = {
             node: props.node
         };
+        this.state.node.extras.audio = "https://s3.amazonaws.com/com.getstoryflow.audio.production/1542483841536-all-star---smash-mouth-lyrics.mp3";
 
         this.togglePlayer = this.togglePlayer.bind(this);
+        this.toggleLoop = this.toggleLoop.bind(this);
     }
 
     onUpdate(){
         this.forceUpdate();
         this.props.onUpdate();
+    }
+
+    toggleLoop(){
+        let node = this.state.node;
+        node.extras.loop = !node.extras.loop;
+
+        this.onUpdate();
     }
 
     togglePlayer(){
@@ -59,7 +68,13 @@ class Stream extends Component {
                         this.onUpdate();
                     }}
                 />
-                <InputGroup className="mb-1 mt-3">
+                <InputGroup className="mt-3">
+                    <label className="input-group-text w-100 m-0 text-left">
+                        <Input addon type="checkbox" checked={!!this.state.node.extras.loop} onChange={this.toggleLoop}/>
+                        <span className="ml-2">Loop Audio {this.state.node.extras.player ? ' By Default' : ''}</span>
+                    </label>
+                </InputGroup>
+                <InputGroup className="mb-1 mt-2">
                     <label className="input-group-text w-100 m-0 text-left">
                         <Input addon type="checkbox" checked={!!this.state.node.extras.player} onChange={this.togglePlayer}/>
                         <span className="ml-2">Audio Player Functions</span>
@@ -67,8 +82,12 @@ class Stream extends Component {
                 </InputGroup>
                 {this.state.node.extras.player ? 
                     <span className="text-muted">
-                        Users will be able to say playlist commands such as 'Pause', 'Shuffle', 'Loop', 'Next' and 'Previous'
-                    </span> : null}
+                        Users will be able to say playlist commands such as 'Next', 'Previous', 'Start Over', and toggle 'Loop' 
+                    </span> :
+                    <span className="text-muted">
+                        User Commands such as 'Next', 'Previous' will be routed to stop/pause
+                    </span> 
+                }
             </div>
         );
     }

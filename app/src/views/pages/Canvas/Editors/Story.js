dcb50819e249bae48d4ae259c106d54606138d71
Mike <michael@getstoryflow.com>
2018-11-27 12:09:55 -0500

    changes

--- app/src/views/pages/Canvas/Editors/Story.js
+++ app/src/views/pages/Canvas/Editors/Story.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import YouTube from 'react-youtube';
 
 class Story extends Component {
     render() {
+        const opts = {
+      height: '187px',
+      width: '100%',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 0
+      }
+            
+        };
         return (
             <div>
-                <p className="modal-bg-txt text-center mt-5">
+            <p className="modal-bg-txt text-center mt-5 mb-2">
                    Let's get started! 😊
                 </p>
-            <p className="modal-txt-no-pad text-center mb-4"> Watch this 2 minute introduction video by Voiceflow's CEO to familiarize yourself with our creator tool!</p>
+            <p className="modal-txt-no-pad text-center mb-3"> Watch this 5 minute introduction video by Voiceflow's CEO to familiarize yourself with our creator tool!</p>
+            <YouTube
+        videoId="WQnBhEVTTCA"
+        opts={opts}
+        onReady={this._onReady}
+      />
             </div>
         );
     }

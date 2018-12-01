152bea95b1927b7b60865823bba405d2288ea557
Peter Stakoun <peter@stakoun.com>
2018-11-30 20:06:49 -0500

    Signup test

--- app/src/__tests__/App.test.js
+++ app/src/__tests__/App.test.js
@@ -5,12 +5,27 @@ import App from '../App';
 
 jest.mock('react-ga');
 
+
 describe('App', () => {
   it('renders without crashing', () => {
-    shallow(<App />);
+    mount(<App />);
   });
   it('redirects to login if unauthenticated', () => {
     const app = mount(<App />);
-    expect(app.exists('.login-card')).toBe(true);
+    expect(app.exists('#signup-form')).toBe(true);
+  });
+  it('disallows duplicate accounts', () => {
+    const app = mount(<App />);
+    app.find('#signup-form input[name="r_name"]')
+      .simulate('change', {target: {value:'Voiceflow Tester'}});
+    app.find('#signup-form input[name="r_email"]')
+      .simulate('change', {target: {value:'tests@getvoiceflow.com'}});
+    app.find('#signup-form input[name="r_password"]')
+      .simulate('change', {target: {value:'password'}});
+    app.find('#signup-form button[type="submit"]')
+      .simulate('click');
+    setTimeout(() => {
+      expect(app.exists('#signup-form .alert-danger')).toBe(true);
+    }, 500);
   });
 });

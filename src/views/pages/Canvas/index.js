c64b030ea9859b376bcf5d98d8f9dac1a765894a
DecathectZero <tylerhan97@gmail.com>
2018-12-02 20:53:00 -0500

    msc changes on jira board

--- src/views/pages/Canvas/index.js
+++ src/views/pages/Canvas/index.js
@@ -74,7 +74,7 @@ class Canvas extends Component {
         this.replaceWithTemplate = this.replaceWithTemplate.bind(this);
         this.createWithTemplate = this.createWithTemplate.bind(this);
         this.createFlowFromTemplate = this.createFlowFromTemplate.bind(this);
-        this.onFlowRenamed = this.onFlowRenamed.bind(this);
+        // this.onFlowRenamed = this.onFlowRenamed.bind(this);
 
         // preview mode
         this.preview = !!this.props.preview;
@@ -680,15 +680,15 @@ class Canvas extends Component {
         // this.props.history.push('/dashboard');
     }
 
-    onFlowRenamed(id) {
-        let nodes = this.state.engine.getDiagramModel().getNodes();
-        for (let key in nodes) {
-            if (nodes[key].extras.type === 'flow' && nodes[key].extras.diagram_id === id) {
-                nodes[key].name = this.state.diagrams.find(x => x.id === id).name;
-                this.repaint();
-            }
-        }
-    }
+    // onFlowRenamed(id) {
+    //     let nodes = this.state.engine.getDiagramModel().getNodes();
+    //     for (let key in nodes) {
+    //         if (nodes[key].extras.type === 'flow' && nodes[key].extras.diagram_id === id) {
+    //             nodes[key].name = this.state.diagrams.find(x => x.id === id).name;
+    //             this.repaint();
+    //         }
+    //     }
+    // }
 
     unsave(e) {
         if(e && e.node && !e.isCreated){

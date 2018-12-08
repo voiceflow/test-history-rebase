a18b41337b788c2dc2adf551c238423c93489902
DecathectZero <tylerhan97@gmail.com>
2018-12-08 18:44:16 -0500

    menu system

---
+++ src/views/pages/Canvas/Sidebars/Variables.js
@@ -0,0 +1,5 @@
+const defaultVariables = {
+    'sessions': 'The Number of times a particular user has opened the app',
+    'user_id': 'The user\'s Amazon/Google unique id',
+    'timestamp': 'UNIX timestamp (number of seconds since January 1st, 1970 at UTC.)'
+}

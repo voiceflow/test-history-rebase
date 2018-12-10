60f4a1899c4d56180c22786a811d45e50c295b4d
Mike <michael@getstoryflow.com>
2018-12-10 17:33:23 -0500

    the fuk

--- src/views/pages/Canvas/Menu.js
+++ src/views/pages/Canvas/Menu.js
@@ -1,51 +1,12 @@
-import React, { PureComponent } from 'react';
-import MenuItem from './MenuItem';
-import TemplateItem from './TemplateItem';
-import ModuleItem from './ModuleItem';
-import { InputGroup, Input, InputGroupAddon, Button, FormGroup, Label, ButtonGroup, Collapse } from 'reactstrap';
-import isVarName from 'is-var-name';
-import FlowButton from './FlowButton';
-import {Tooltip} from 'react-tippy';
-import cloneDeep from 'lodash/cloneDeep'
-
-const defaultVariables = {
-    'sessions': 'The Number of times a particular user has opened the app',
-    'user_id': 'The user\'s Amazon/Google unique id',
-    'timestamp': 'UNIX timestamp (number of seconds since January 1st, 1970 at UTC.)'
-}
-
-const sections = [{
-    title: 'Basic',
-    items: [
-        { text: 'Speak', type: 'speak', icon: <i class="fas fa-comment"></i>, tip: 'Tell Alexa to play sounds or talk to the user' },
-        { text: 'Choice', type: 'choice', icon: <i className="fas fa-project-diagram"/>, tip: 'Listen for the user to make a choice from a list of options you set'  },
-        { text: 'Command', type: 'command', icon: <i class="fas fa-exclamation"></i>, tip: 'Add shortcuts for your users to navigate your skill quickly'},
-         { text: 'Random', type: 'random', icon: <i className="fas fa-random"/>, tip: 'Choose randomly from a set number of paths' },
-        { text: 'Stream', type: 'stream', icon: <i class="fas fa-play"></i>, tip: 'Stream long audio files & URLs for the user' },
-    ]
-},{
-    title: 'Advanced',
-    items: [
-        { text: 'Set', type: 'set', icon: <i className="fas fa-code"/>, tip: 'Set the value of a variable, or many variables at once'  },
-        { text: 'If', type: 'if', icon: <i className="fas fa-code-branch"/>, tip: 'Set conditions that activate paths only when true' },
-        { text: 'Capture', type: 'capture', icon: <i className="fas fa-microphone"/>, tip: 'Capture what the user says into a variable'  },
-        { text: 'API', type: 'api', icon: <i className="fas fa-globe"/>, tip: 'Use external APIs and store responses into variables'  },
-        { text: 'Flow', type: 'flow', icon: <i className="fas fa-clone"/>, tip: 'Organize your project into manageable sections or perform computations'},
-   ]
-},{
-    title: 'Functional',
-    items: [
-        { text: 'Permissions', type: 'permissions', icon: <i className="fas fa-lock"/>, tip: 'Ask users for access to their info (Name, Email, Phone)'  },
-        { text: 'Combine', type: 'combine', icon: <i className="fas fa-compress-alt"/>, tip: 'Combine Different Audio Files to bypass Amazon 5 Audio limit' },
-        { text: 'Comment', type: 'comment', icon: <i className="fas fa-sticky-note"/>, tip: 'Add notes to your diagram'},
-    ]
-},{
-    title: 'Business',
-    items: [
-        { text: 'Mail', type: 'mail', icon: <i className="far fa-envelope"/> },        
-    ]
-}]
-
+import React, { PureComponent } from 'react'
+import {Tooltip} from 'react-tippy'
+import FlowButton from './Sidebars/components/FlowButton'
+import Blocks from './Sidebars/Blocks'
+import Variables from './Sidebars/Variables'
+import Project from './Sidebars/Project'
+import Confirm from './../../components/Modals/ConfirmModal'
+import {Alert} from 'reactstrap'
+import axios from 'axios'
 const tabs = {
     top: [
         {tab: "blocks", icon: <i className="fas fa-plus-square"/>, tip: 'Blocks'},

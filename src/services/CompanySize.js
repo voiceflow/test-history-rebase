070708e43da6cc3c3e459871a25ca955e1c04e22
DecathectZero <tylerhan97@gmail.com>
2018-12-11 17:25:49 -0500

    updated user onboarding

--- src/services/CompanySize.js
+++ src/services/CompanySize.js
@@ -9,30 +9,30 @@ export default [
     },
     {
         value: "C", 
-        label: "11-50"
+        label: "10-50"
     },
     {
         value: "D", 
-        label: "51-200"
+        label: "50-200"
     },
     {
         value: "E", 
-        label: "201-500"
+        label: "200-500"
     },
     {
         value: "F", 
-        label: "501-1000"
+        label: "500-1,000"
     },
     {
         value: "G", 
-        label: "1001-5000"
+        label: "1,000-5,000"
     },
     {
         value: "H", 
-        label: "5001-10,000"
+        label: "5,000-10,000"
     },
     {
         value: "I", 
-        label: "10,001+"
+        label: "10,000+"
     }
 ]

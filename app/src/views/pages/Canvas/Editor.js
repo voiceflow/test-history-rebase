6728f5f0bb8c223c203ce1bc9c67137862559016
DecathectZero <tylerhan97@gmail.com>
2018-12-12 21:12:40 -0500

    new API block body and block expand

--- app/src/views/pages/Canvas/Editor.js
+++ app/src/views/pages/Canvas/Editor.js
@@ -19,6 +19,7 @@ import Mail from './Editors/Mail';
 import Stream from './Editors/Stream';
 import Permissions from './Editors/Permissions';
 import {
+    Modal, Button, ModalBody, ModalHeader,
     UncontrolledDropdown,
     DropdownToggle,
     DropdownMenu,
@@ -34,7 +35,9 @@ class Editor extends Component {
             voices: [],
             templates: [],
             permission_options: [],
-            dropdownOpen: false
+            dropdownOpen: false,
+            modal: false,
+            expanded: false
         }
 
         this.BlockViewer = this.BlockViewer.bind(this)
@@ -246,8 +249,13 @@ class Editor extends Component {
         //         <Button color="danger" size="sm" className="py-0 mt-1" onClick={this.props.removeNode}>Confirm</Button>
         //     </React.Fragment>}
         // >
+        let Editor 
+        if(type){
+            Editor = this.BlockViewer()
+        }
+
         return (
-            <div id="Editor" className={(this.props.open && type ? 'open':'')} onClick={this.props.onClick}>
+            <div id="Editor" className={(this.props.open && type && !this.state.modal ? 'open':'')} onClick={this.props.onClick}>
                 {type ?
                     <div className="controls" key={this.state.node.id}>
                         <div className="top">
@@ -257,31 +265,57 @@ class Editor extends Component {
                                     <div className={"block " + type} onClick={() => this.props.setHelp({type: this.state.node.extras.type})}>
                                         {type} block <i className="fas fa-question-circle mr-1"/>
                                     </div>
-                                    <UncontrolledDropdown nav inNavbar>
-                                        <DropdownToggle className="delete-block" nav tag="div">
-                                            <i className="fas fa-cog"/>
-                                        </DropdownToggle>
-                                        <DropdownMenu right className="arrow arrow-right no-select" style={{right: '-3px', marginTop: '5px'}}>
-                                            <DropdownItem header>
-                                                Block Options
-                                            </DropdownItem>
-                                            {/*this.state.node.extras.type === 'flow' && 
-                                                <DropdownItem onClick={this.copyFlow}>Copy Flow</DropdownItem>*/
-                                            }
-                                            <DropdownItem onClick={this.props.copyNode} className="pointer">
-                                                <i className="fas fa-copy text-muted"/> Copy
-                                            </DropdownItem>
-                                            <DropdownItem onClick={this.props.removeNode} className="pointer">
-                                                <i className="fas fa-file-times text-muted"/> Delete
-                                            </DropdownItem>
-                                        </DropdownMenu>
-                                    </UncontrolledDropdown>
+                                    <div className="d-flex pl-2">
+                                        <div 
+                                            className="delete-block"
+                                            onClick={()=>this.setState({
+                                                expanded: true,
+                                                modal: true
+                                            })}
+                                        >
+                                            <i className="far fa-expand-arrows-alt"/>
+                                        </div>
+                                        <UncontrolledDropdown nav inNavbar>
+                                            <DropdownToggle className="delete-block" nav tag="div">
+                                                <i className="fas fa-cog"/>
+                                            </DropdownToggle>
+                                            <DropdownMenu right className="arrow arrow-right no-select" style={{right: '-3px', marginTop: '5px'}}>
+                                                <DropdownItem header>
+                                                    Block Options
+                                                </DropdownItem>
+                                                {/*this.state.node.extras.type === 'flow' && 
+                                                    <DropdownItem onClick={this.copyFlow}>Copy Flow</DropdownItem>*/
+                                                }
+                                                <DropdownItem onClick={this.props.copyNode} className="pointer">
+                                                    <i className="fas fa-copy text-muted"/> Copy
+                                                </DropdownItem>
+                                                <DropdownItem onClick={this.props.removeNode} className="pointer">
+                                                    <i className="fas fa-file-times text-muted"/> Delete
+                                                </DropdownItem>
+                                            </DropdownMenu>
+                                        </UncontrolledDropdown>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div id="editor-section">
                             {this.renderTitle()}
-                            {this.BlockViewer()}
+                            {Editor}
+                            {this.state.expanded &&
+                                <React.Fragment>
+                                    <Modal 
+                                        isOpen={this.state.modal} 
+                                        toggle={()=>this.setState({modal: false})}
+                                        onClosed={()=>this.setState({expanded: false})}
+                                        size="lg"
+                                    >
+                                        <ModalHeader toggle={()=>this.setState({modal: false})}>{this.state.node.name} Settings</ModalHeader>
+                                        <ModalBody className="pb-5">
+                                            {Editor}
+                                        </ModalBody>
+                                    </Modal>
+                                </React.Fragment>
+                            }
                         </div>
                     </div> 
                 : null}

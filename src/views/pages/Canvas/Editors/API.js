b7a0e10bba18f55088018380b3dcbd72bf91a7e1
Jason <jason@getvoiceflow.com>
2018-12-18 15:34:47 -0500

    [Core-84] Added test API endpoint button to API block editor

--- src/views/pages/Canvas/Editors/API.js
+++ src/views/pages/Canvas/Editors/API.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { Nav, NavItem, NavLink, InputGroupButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, InputGroup } from 'reactstrap';
+import update from 'immutability-helper';
+import * as _ from 'lodash';
+import axios from 'axios';
+import stringifyObject from 'stringify-object';
+import isVarName from 'is-var-name';
+import { Button, Modal, ModalHeader, ModalBody, Nav, NavItem, NavLink, InputGroupAddon, Input, InputGroupButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, InputGroup } from 'reactstrap';
 import APIInputs from './components/APIInputs.js';
 import APIMapping from './components/APIMapping.js';
 import VariableInput from './components/VariableInput';
@@ -18,7 +23,7 @@ class API extends Component {
         super(props);
 
         let node = props.node;
-        
+
         // DEPRECATE turning from string to draftjs for SUPER old api blocks
         if(node.extras.mapping){
             node.extras.mapping = node.extras.mapping.map((choice) => {
@@ -54,14 +59,20 @@ class API extends Component {
             node: node,
             modal: false,
             body_state: true,
+            modalContent: null,
+            variables: [],
+            innerVariables: {},
             dropdownOpen: false,
             type: 'headers',
             popoverOpen: false
         };
 
         this.onChangeAce = this.onChangeAce.bind(this)
+        this.getEndpoint = this.getEndpoint.bind(this);
+        this.renderAPITest = this.renderAPITest.bind(this);
         this.toggle = this.toggle.bind(this);
         this.togglePopover = this.togglePopover.bind(this)
+        this.handleVariableChange = this.handleVariableChange.bind(this);
         this.handleUpdate = this.handleUpdate.bind(this)
         this.handleSelection = this.handleSelection.bind(this)
         this.handleAddPair = this.handleAddPair.bind(this)
@@ -72,12 +83,98 @@ class API extends Component {
         this.handleKVMappingChange = this.handleKVMappingChange.bind(this)
     }
 
+    componentDidMount() {
+      let variable_map = {}
+      this.props.variables.forEach(val =>
+        variable_map[val] = ''
+      );
+      this.setState({innerVariables: variable_map});
+    }
     onChangeAce(content){
         let node = this.state.node
         node.extras.content = content
         this.setState({
             node: node
         }, this.props.onUpdate)
+    }
+
+    getEndpoint(){
+      let regex = /\{([^{}]*)\}/g;
+      let variables = [];
+      let url = draftToMarkdown(this.state.node.extras.url);
+      let method = this.state.node.extras.method;
+      const replacer = (match, inner, variables_map) => {
+        if(inner in variables_map){
+          return variables_map[inner];
+        }else{
+          return match;
+        }
+      }
+
+      if (!_.isEmpty(this.state.variables)){
+        url = url.replace(/\{([^{}]*)\}/g, (match, inner) => replacer(match, inner, this.state.innerVariables))
+      }
+      if(!_.isNull(url)){
+    		let match = regex.exec(url);
+    		while (match != null) {
+    			if(isVarName(match[1])){
+    		    	variables.push(match[1]);
+    		    }
+    		    match = regex.exec(url);
+    		}
+        if (!_.isEmpty(variables))  {
+          this.setState({variables: variables})
+        } else {
+          axios({ method: method, url: url })
+          .then(res => {
+            console.log(JSON.stringify(res, null, 4))
+            this.setState({
+              modalContent: JSON.stringify(res, null, 4)
+            })
+          })
+          .catch(err => {
+            this.setState({
+              modalContent: JSON.stringify(err, null, 4)
+            })
+          })
+        }
+    	}
+      this.setState({ modal: true });
+    }
+
+    renderAPITest() {
+      if (!this.state.modalContent && _.isEmpty(this.state.variables)) {
+        return null;
+      }
+      return (
+      <div className='projects-menu'>
+          {
+            !_.isEmpty(this.state.variables) ?
+              <React.Fragment>
+                <label>We've detected you are using variables, please set variables and run again</label>
+                <Button color="primary" onClick={this.getEndpoint} size="sm" block><i className="fas fa-play"></i>&nbsp;&nbsp;&nbsp; Run</Button>
+                <br />
+              </React.Fragment> :
+              null
+          }
+          {_.map(this.state.variables, (val, key) => (
+            <React.Fragment key={key}>
+              <InputGroup>
+                <InputGroupAddon addonType='prepend'>{val}</InputGroupAddon>
+                <Input name={val} placeholder='set variable' onChange={this.handleVariableChange} />
+              </InputGroup>
+            </React.Fragment>
+          ))}
+        <div>
+          <pre>{this.state.modalContent}</pre>
+        </div>
+      </div>);
+    }
+
+    handleVariableChange = event => {
+      this.setState({
+        innerVariables: update(this.state.innerVariables, {[event.target.name]: {$set:event.target.value}})
+      })
     }
 
     handleUpdate(name, value) {
@@ -129,7 +226,7 @@ class API extends Component {
     handleRemovePair(type, i) {
         let node = this.state.node;
         node.extras[type].splice(i, 1);
-        
+
         this.setState({
             node: node
         }, this.props.onUpdate);
@@ -187,9 +284,20 @@ class API extends Component {
 
         return (
             <React.Fragment>
+              <Modal
+                isOpen={this.state.modal}
+                toggle={()=>this.setState({modalContent: null, modal: false, variables: []})}
+              >
+                <ModalHeader toggle={()=>this.setState({modalContent:null, modal: false, variables: []})}>API Test</ModalHeader>
+                <ModalBody>
+                  {this.renderAPITest()}
+                </ModalBody>
+              </Modal>
                 <label>
                     URL Endpoint
                 </label>
+                <Button color="primary" onClick={this.getEndpoint} size="sm" block><i className="fas fa-play"></i>&nbsp;&nbsp;&nbsp; Test Endpoint</Button>
+                <br />
                 <InputGroup>
                     <InputGroupButtonDropdown addonType="prepend" isOpen={this.state.dropdownOpen} toggle={this.toggle}>
                         <DropdownToggle caret>
@@ -210,7 +318,7 @@ class API extends Component {
                         raw={this.state.node.extras.url}
                         variables={this.props.variables}
                         updateRaw={(raw) => {
-                            let node = this.state.node; 
+                            let node = this.state.node;
                             node.extras.url = raw
                             this.setState({
                                 node: node
@@ -226,10 +334,10 @@ class API extends Component {
                             Headers
                         </NavLink>
                     </NavItem>
-                    <NavItem className="mr-2" onClick={() => {if (this.state.node.extras.method !== 'GET') this.setState({type: 'body'})}}> 
+                    <NavItem className="mr-2" onClick={() => {if (this.state.node.extras.method !== 'GET') this.setState({type: 'body'})}}>
                         <NavLink href="#" active={this.state.type === 'body'} disabled={this.state.node.extras.method === 'GET'}>Body</NavLink>
                     </NavItem>
-                    <NavItem className="mr-2" onClick={() => this.setState({type: 'params'})}> 
+                    <NavItem className="mr-2" onClick={() => this.setState({type: 'params'})}>
                         <NavLink href="#" active={this.state.type === 'params'}>Params</NavLink>
                     </NavItem>
                 </Nav>
@@ -249,13 +357,13 @@ class API extends Component {
                                 const node = this.state.node;
                                 node.extras.bodyInputType = 'rawInput';
                                 this.setState({ node: node })
-                                }}> 
+                                }}>
                             <NavLink href="#" active={this.state.node.extras.bodyInputType === 'rawInput'}>
                                 Raw Input
                             </NavLink>
                         </NavItem>
                     </Nav>
-                    { (this.state.node.extras.bodyInputType === 'rawInput') ? 
+                    { (this.state.node.extras.bodyInputType === 'rawInput') ?
                     <AceEditor
                         height='300px'
                         width='100%'
@@ -293,7 +401,7 @@ class API extends Component {
         )
 
         // return <React.Fragment>
-        //     {this.state.body_state ? 
+        //     {this.state.body_state ?
         //         <React.Fragment>
         //             <Button color='clear' block onClick={
         //                 ()=>this.setState({
@@ -303,7 +411,7 @@ class API extends Component {
         //             }><i className="far fa-expand-arrows-alt"/> Expand</Button>
         //             {content}
         //         </React.Fragment>
-        //         : 
+        //         :
         //         <React.Fragment>
         //             <Button color='clear' block disabled>{ this.state.modal ?
         //                 <React.Fragment>
@@ -312,8 +420,8 @@ class API extends Component {
         //                 <span className="loader"/>
         //             }</Button>
 
-        //             <Modal 
-        //                 isOpen={this.state.modal} 
+        //             <Modal
+        //                 isOpen={this.state.modal}
         //                 toggle={()=>this.setState({modal: false})}
         //                 onClosed={()=>this.setState({body_state: true})}
         //                 size="lg"

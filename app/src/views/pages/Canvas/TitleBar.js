23b93d9decfaf574d8801fd9f0af4019a4878f87
DecathectZero <tylerhan97@gmail.com>
2018-12-20 17:02:04 -0500

    slot values

--- app/src/views/pages/Canvas/TitleBar.js
+++ app/src/views/pages/Canvas/TitleBar.js
@@ -9,7 +9,7 @@ class TitleBar extends Component {
                     <div className="skill-name">
                         {this.props.skill.name}
                     </div>
-                    <MUIButton id="test" variant="extendedFab" className="white-btn play" onClick={this.props.onTest}><span className="words">Test</span><span className="button-circle"><i className="fas fa-play"/></span></MUIButton>
+                    <MUIButton id="test" className="white-btn play" onClick={this.props.onTest}><span className="words">Test</span><span className="button-circle"><i className="fas fa-play"/></span></MUIButton>
                 </div>
             </div>
         );

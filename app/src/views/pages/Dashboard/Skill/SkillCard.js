95e703cf3ab6b754e9e68c3dfafbe3caf3ff703c
DecathectZero <tylerhan97@gmail.com>
2018-12-20 16:41:54 -0500

    fixed nested buttons

--- app/src/views/pages/Dashboard/Skill/SkillCard.js
+++ app/src/views/pages/Dashboard/Skill/SkillCard.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import axios from 'axios'
 
-import { ButtonDropdown, DropdownMenu, DropdownToggle, DropdownItem } from 'reactstrap'
+import { Dropdown, DropdownMenu, DropdownToggle, DropdownItem } from 'reactstrap'
 
 import {Link} from 'react-router-dom'; 
 
@@ -55,19 +53,19 @@ class SkillCard extends React.Component {
         <Card className='skill-card'>
           <CardActionArea className="card-action">
             <div className="d-flex justify-content-end">
-              <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} direction="left">
-                <DropdownToggle>
+              <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} direction="left">
+                <DropdownToggle tag="div" className="btn btn-primary">
                   <i className="far fa-ellipsis-v mr-2"></i>
                 </DropdownToggle>
                 <DropdownMenu>
-                  <DropdownItem onClick={()=>{this.props.history.push('/publish/amzn/' + this.props.skill.skill_id)}}>
+                  <DropdownItem tag="div" onClick={()=>{this.props.history.push('/publish/amzn/' + this.props.skill.skill_id)}}>
                     Publish <span className="button-circle"><i className="fab fa-amazon"/></span>
                   </DropdownItem>
-                  <DropdownItem onClick={this.copySkill}>
+                  <DropdownItem tag="div" onClick={this.copySkill}>
                     Copy Skill
                   </DropdownItem>
                 </DropdownMenu>
-              </ButtonDropdown>
+              </Dropdown>
             </div>
             { image ?
               <div style={{backgroundImage: `url(${image})`}} className='card-image' onClick={()=>this.props.open(this.props.skill.skill_id, this.props.skill.diagram)}/> :

4acc4d25f3b7396897d3341204b0b9dadd074446
Peter Stakoun <peter@stakoun.com>
2018-12-27 15:43:30 -0500

    Admin panel

---
+++ src/views/pages/Admin/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import './Admin.css';
+import Home from './Home';
+
+import {ButtonGroup} from 'reactstrap'
+
+const tabs = [
+    {
+        display: <React.Fragment><i className="fal fa-home"/> Home</React.Fragment>,
+        match: ['default'],
+        link: '/admin'
+    }
+]
+
+class Admin extends Component {
+
+    render() {
+        let page;
+        switch(this.props.page){
+            default:
+                page = <Home user={this.props.user}/>
+        }
+
+        return (
+            <div className="admin Window">
+                <div md="3" className="sidebar">
+                    <div className="title">
+                        Tools
+                    </div>
+                    <ButtonGroup vertical>
+                        {tabs.map((tab, i) => {
+                            if(tab.match.includes(this.props.page)){
+                                return <div key={i} className="active-btn">
+                                    {tab.display}
+                                </div>
+                            }else{
+                                return <Link key={i} to={tab.link} className="inactive-btn">
+                                    {tab.display}
+                                </Link>
+                            }
+                        })}
+                    </ButtonGroup>
+                </div>
+                <div md="9" className="admin-page">
+                    {page}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Admin;

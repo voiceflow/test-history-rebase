12c93ea403e2eb29f0c2f7b0f682c1cfcc683fb7
Peter Stakoun <peter@stakoun.com>
2018-12-27 17:25:28 -0500

    Done

--- src/views/pages/Admin/index.js
+++ src/views/pages/Admin/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import './Admin.css';
 import Home from './Home';
+import Copy from './Copy';
 
 import {ButtonGroup} from 'reactstrap'
 
@@ -11,6 +12,11 @@ const tabs = [
         display: <React.Fragment><i className="fal fa-home"/> Home</React.Fragment>,
         match: ['default'],
         link: '/admin'
+    },
+    {
+        display: <React.Fragment><i className="fal fa-copy"/> Copy</React.Fragment>,
+        match: ['copy'],
+        link: '/admin/copy'
     }
 ]
 
@@ -19,6 +25,9 @@ class Admin extends Component {
     render() {
         let page;
         switch(this.props.page){
+            case 'copy':
+                page = <Copy {...this.props}/>
+                break;
             default:
                 page = <Home user={this.props.user}/>
         }

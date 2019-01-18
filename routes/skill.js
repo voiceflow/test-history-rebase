86acd3a8a420514a684ee5b16b5a0aa8425e0111
Jeremy Chinsen <chinsenjeremy@gmail.com>
2019-01-18 16:30:59 -0500

    Revert "Revert "Added different env vars for segment""

--- routes/skill.js
+++ routes/skill.js
@@ -1,21 +1,10 @@
 const axios = require('axios')
 const _ = require('lodash')
-const {
-  docClient,
-  pool,
-  hashids,
-  intercom,
-  jwt
-} = require('./../services')
-const {
-  AccessToken
-} = require('./authentication')
+const { docClient, pool, hashids, intercom, jwt } = require('./../services')
+const { AccessToken } = require('./authentication')
 const JSONs = require('./../config/amazon_json')
-const {
-  getEnvVariable
-} = require('../util')
-const Analytics = require('analytics-node')
-const analytics = new Analytics(process.env.SEGMENT_WRITE_KEY_DEVELOPMENT)
+const { getEnvVariable } = require('../util')
+const analytics = new (require('analytics-node'))(getEnvVariable('SEGMENT_WRITE_KEY'))
 
 const generateID = () => {
   return "xxxxxxxxxxxxxxxxyxxxxxxxxxxxxxxx".replace(/[xy]/g, c => {

6d4eb188bba2f5708bbe082c3b3343f4f39ec9b6
Jason <jason@getvoiceflow.com>
2019-01-18 13:45:00 -0500

    hot fix account_linking null

--- routes/skill.js
+++ routes/skill.js
@@ -797,9 +797,11 @@ exports.buildSkill = async (req,res) => {
                         });
                     }
                     let account_linking = r.account_linking;
-                    account_linking.domains=_.flattenDeep(account_linking.domains)
-                    account_linking.scopes = _.flattenDeep(account_linking.scopes)
-                    account_linking.clientSecret = jwt.verify(account_linking.clientSecret, getEnvVariable('ACCOUNT_SECRET_SIGNATURE'));
+                    if (!_.isNull(accountLinking)) {
+                      account_linking.domains=_.flattenDeep(account_linking.domains)
+                      account_linking.scopes = _.flattenDeep(account_linking.scopes)
+                      account_linking.clientSecret = jwt.verify(account_linking.clientSecret, getEnvVariable('ACCOUNT_SECRET_SIGNATURE'));
+                    }
 
                     if (account_linking) {
                         axios.request({

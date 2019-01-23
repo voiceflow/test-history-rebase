419bbff52695d8bd00051ca7174add4feb4915a3
DecathectZero <tylerhan97@gmail.com>
2019-01-22 21:33:51 -0500

    I GOTCHU FRANKIE

--- src/views/pages/Canvas/ActionGroup.js
+++ src/views/pages/Canvas/ActionGroup.js
@@ -155,7 +155,6 @@ class ActionGroup extends PureComponent {
         axios.post(`/diagram/${this.props.skill.diagram}/${this.props.skill.skill_id}/publish`)
         .then(res => {
             let new_version_data = res.data
-            // this.props.addVersion(new_version_data)
             this.setState({stage: 11}, () => {
                 axios.post(`/skill/${new_version_data.new_skill.skill_id}/publish`)
                 .then(res => {

75ff0602d2af819f8c01a97d572401046669f4f4
groovy-tunes <chinsenjeremy@gmail.com>
2019-01-24 18:14:06 -0500

    SAVED

---
+++ src/views/components/LineBar.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { Line } from 'react-chartjs-2'
+
+class LineBar extends Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    const data={
+      labels: this.props.dates,
+      datasets: [
+        {
+          label: 'Daily Active Users',
+          backgroundColor: 'rgba(96, 146, 255,0.2)',
+          borderColor: 'rgba(96, 146, 255, 1)',
+          borderWidth: 1,
+          hoverBackgroundColor: 'rgba(96, 146, 255,0.4)',
+          hoverBorderColor: 'rgba(96, 146, 255, 1)',
+          data: this.props.dau
+        }
+      ]
+    }
+    return(
+      <Line data={data}/>
+    )
+  }
+}
+
+export default LineBar

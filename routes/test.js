22485bb1c88ccd304d1d34277657afc56ed31790
groovy-tunes <chinsenjeremy@gmail.com>
2019-01-27 15:58:24 -0500

    Api bug fix

--- routes/test.js
+++ routes/test.js
@@ -2,6 +2,11 @@ const axios = require('axios')
 
 module.exports.api = (req, res) => {
     if(typeof req.body.api === 'object' && req.body.api.method && req.body.api.url){
+        if(req.body.api.method === 'POST'){
+            req.body.api.headers = { 'Content-Type': 'application/json' }
+            req.body.api.data = req.body.api.body
+            delete req.body.api.body
+        }
         axios(req.body.api)
         .then(result => {
             res.status(result.status).send(result.data)

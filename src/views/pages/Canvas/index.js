f3c346e99aa8bac69aa4f558d28b0ab556c2cb2c
Jason Zhao <zhaojason98@gmail.com>
2019-01-27 20:58:55 -0500

    [CORE-298] update skill from publish, better validation (#91)
    * [CORE-298] update skill from publish, better error handling
    
    * [CORE-298] put back axios request
    
    * [CORE-298] extra syntax

--- src/views/pages/Canvas/index.js
+++ src/views/pages/Canvas/index.js
@@ -223,6 +223,13 @@ class Canvas extends Component {
         })
     }
 
+    static getDerivedStateFromProps(props, state){
+        if (props.skill !== state.skill){
+            return {
+                skill: props.skill
+            }
+        }
+    }
     componentWillUnmount() {
         Mousetrap.reset()
         if(!this.props.preview && this.state.skill && this.state.skill.skill_id && this.props.diagram_id && !window.error){

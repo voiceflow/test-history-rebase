c39d09cc45203379690d63b38962bf1ac44ca957
DecathectZero <tylerhan97@gmail.com>
2019-02-12 11:34:49 -0500

    better synatx

--- src/views/pages/Canvas/index.jsx
+++ src/views/pages/Canvas/index.jsx
@@ -413,12 +413,12 @@ class Canvas extends Component {
     }
 
     paste() {
-        let event = {
-            clientX: this.mouseX,
-            clientY: this.mouseY
-        }
-        var point = this.state.engine.getRelativeMousePoint(event)
         if(this.state.copy){
+            let event = {
+                clientX: this.mouseX,
+                clientY: this.mouseY
+            }
+            var point = this.state.engine.getRelativeMousePoint(event)
             this.copyNode(this.state.copy, { x: point.x - (this.state.copy.name.length * 4.5 + 40), y: point.y -30})
         }
     }

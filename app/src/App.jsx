1070cb8c3cf55b33cc0fb35a71845651871b4034
Jason <jason@getvoiceflow.com>
2019-02-13 17:32:46 -0500

    call chmln when page loads

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -187,6 +187,12 @@ class App extends Component {
   }
 
   componentDidMount() {
+    if (window.chmln){
+      window.chmln.identify(window.user_detail.id, {
+        email: window.user_detail.email,
+        name: window.user_detail.name
+      })
+    }
     if (window.Stripe) {
       this.setState({stripe: window.Stripe(STRIPE_KEY)});
     } else {

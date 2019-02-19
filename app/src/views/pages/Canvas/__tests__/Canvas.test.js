8b178284b5106968d1d7462cb60e5ff1d6e2a71e
Jason <jason@getvoiceflow.com>
2019-02-19 16:07:33 -0500

    [CORE-406] added call tests

--- app/src/views/pages/Canvas/__tests__/Canvas.test.js
+++ app/src/views/pages/Canvas/__tests__/Canvas.test.js
@@ -17,5 +17,15 @@ describe('Canvas', () => {
         const component = shallow(<Canvas skill={skill}/>);
         expect(component.state().skill).toEqual(skill);
     })
+    it('auto save test', () => {
+        let skill = testSkill
+        const spy = jest.spyOn(Canvas.prototype, "onSave");
+        const unmount = jest.spyOn(Canvas.prototype, "componentWillUnmount");
+        const diagram_id = "e9f52b0622f08ff1b21137bae05a242b"
+        const component = shallow(<Canvas skill={skill} diagram_id={diagram_id} onError={() => {}}/>);
+        component.unmount();
+        expect(unmount).toHaveBeenCalled()
+        expect(spy).toHaveBeenCalled()
+    })
 })
 

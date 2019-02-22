ec5b97f74ebd5aa088f8223097a3e48449e51c64
DecathectZero <tylerhan97@gmail.com>
2019-02-21 19:27:26 -0500

    apparently there was never a aws endpoint

--- services.js
+++ services.js
@@ -20,10 +20,8 @@ const AWS = require('aws-sdk');
 AWS.config = new AWS.Config({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
-    endpoint: process.env.AWS_ENDPOINT
-});
-
+    region: process.env.AWS_REGION
+})
 
 const docClient = new AWS.DynamoDB.DocumentClient({
     convertEmptyValues: true

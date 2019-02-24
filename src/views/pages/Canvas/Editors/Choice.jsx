540856a0ed36f0f1fb1bdef955d056b5fe689a1a
DecathectZero <tylerhan97@gmail.com>
2019-02-24 12:32:06 -0500

    choice block stuff

--- src/views/pages/Canvas/Editors/Choice.jsx
+++ src/views/pages/Canvas/Editors/Choice.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import ChoiceInputs from './components/ChoiceInputs';
+import randomstring from 'randomstring'
 
 class Choice extends Component {
     constructor(props) {
         super(props);
 
+        // ensure choices/inputs maintain consistency (this is done for backwards compatiability)
+        if(!Array.isArray(props.node.extras.choices) || props.node.extras.choices.length !== props.node.extras.inputs.length){
+            props.node.extras.choices = new Array(props.node.extras.inputs.length).fill(null)
+        }
+
+        props.node.extras.choices = props.node.extras.choices.map(c => (c && c.key) ? c : {
+            open: false,
+            key: randomstring.generate(5)
+        })
+
         this.state = {
-            node: this.props.node,
+            node: props.node,
             voices: Array.isArray(this.props.voices) ? this.props.voices : []
         };
 
@@ -16,17 +27,9 @@ class Choice extends Component {
         this.handleRemoveChoice = this.handleRemoveChoice.bind(this);
     }
 
-    handleChange(e, key = undefined) {
-        var node = this.state.node;
-        var name = e.target.getAttribute('name');
-        var value = e.target.value;
-        if (name === 'name') {
-            node[name] = value;
-        } else if (key !== undefined && (typeof key === 'string' || typeof key === 'number')) {
-            node.extras[name][key] = value;
-        } else {
-            node.extras[name] = value;
-        }
+    handleChange(text, key) {
+        var node = this.state.node
+        node.extras.inputs[key] = text
         if (node.parentCombine) {
             let bestNode = _.findIndex(
                 node.parentCombine.combines,
@@ -40,14 +43,17 @@ class Choice extends Component {
         this.props.repaint();
     }
 
-    handleAddChoice(e) {
+    handleAddChoice() {
         // console.log(this.state.node.extras.choices);
         var node = this.state.node;
         this.props.clearRedo();
-        this.props.updateEvents(_.cloneDeep(node).extras, _.cloneDeep(node).ports);
-        node.extras.choices.push('New Choice');
+        this.props.updateEvents(_.cloneDeep(node).extras, _.cloneDeep(node).ports)
         node.extras.inputs.push('');
-        let test = node.addOutPort(node.extras.choices.length);
+        node.extras.choices.push({
+            open: true,
+            key: randomstring.generate(5)
+        })
+        let test = node.addOutPort(node.extras.inputs.length);
         test.setMaximumLinks(1);
         if (node.parentCombine) {
             let isLast = _.last(node.parentCombine.combines).id === node.id
@@ -62,13 +68,12 @@ class Choice extends Component {
         }
         this.setState({
             node: node
-        }, this.props.onUpdate);
+        }, this.props.onUpdate)
         // this.props.diagramEngine.setSuperSelect(node.parentCombine);
-        this.props.repaint();
-        e.preventDefault();
+        this.props.repaint()
     }
 
-    handleRemoveChoice(e, i) {
+    handleRemoveChoice(i) {
         var node = this.state.node;
         this.props.clearRedo();
         this.props.updateEvents(_.cloneDeep(node).extras, _.cloneDeep(node).ports);
@@ -79,20 +84,18 @@ class Choice extends Component {
         for (var name in node.getPorts()) {
             var port = node.getPort(name);
 
-            if (port.label === node.extras.choices.length) {
+            if (port.label === node.extras.inputs.length) {
                 node.removePort(port);
                 if (node.parentCombine) {
                     node.parentCombine.removePort(port);
                     // eslint-disable-next-line
                     node.parentCombine.combines[bestNode].ports = _.filter(node.parentCombine.combines[bestNode].ports, p => p.id !== port.id)
-                    // node.parentCombine.combines[bestNode].extras.choices.splice(i, 1);
-                    // node.parentCombine.combines[bestNode].extras.inputs.splice(i, 1);
                 }
                 break;
             }
         }
-        node.extras.choices.splice(i, 1);
-        node.extras.inputs.splice(i, 1);
+        node.extras.choices.splice(i, 1)
+        node.extras.inputs.splice(i, 1)
         if (node.parentCombine){
             node.parentCombine.combines[bestNode] = node.serialize()
         }
@@ -100,27 +103,19 @@ class Choice extends Component {
             node: node
         }, this.props.onUpdate);
         this.props.repaint();
-        e.preventDefault();
     }
 
     render() {
         return (
-            <div>
-                <div className="d-flex justify-content-between">
-                    <label>
-                        Choices
-                    </label>
-                </div>
-                <div className={this.props.live_mode ? 'disabled-overlay' : null}>
-                    <ChoiceInputs
-                        choices={this.state.node.extras.choices}
-                        inputs={this.state.node.extras.inputs}
-                        onAdd={this.handleAddChoice}
-                        onRemove={this.handleRemoveChoice}
-                        onChange={this.handleChange}
-                        live_mode={this.props.live_mode}
-                    />
-                </div>
+            <div className={this.props.live_mode ? 'disabled-overlay' : null}>
+                <ChoiceInputs
+                    choices={this.state.node.extras.choices}
+                    inputs={this.state.node.extras.inputs}
+                    onAdd={this.handleAddChoice}
+                    onRemove={this.handleRemoveChoice}
+                    onChange={this.handleChange}
+                    live_mode={this.props.live_mode}
+                />
             </div>
         );
     }

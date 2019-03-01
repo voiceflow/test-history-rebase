27c47299bdc50105f268860c1db3635dc59d8f29
DecathectZero <tylerhan97@gmail.com>
2019-03-01 18:30:43 -0500

    removed redundant websocket

--- src/services/Authentication.js
+++ src/services/Authentication.js
@@ -65,14 +65,9 @@ const login = () => new Promise((resolve, reject) => {
 })
 
 const initalizeLogin = (response, cb) => {
-	cookies.set('auth', response.data.token, {path: '/'});
-	cookies.remove('last_session');
-	window.user_detail = response.data.user;
-	let user = response.data.user;
-	window.CreatorSocket.emit('handshake', {
-		auth: response.data.token,
-		device: getDevice()
-	}, () => {window.CreatorSocket.status = 'HANDSHAKE'})
+	cookies.set('auth', response.data.token, {path: '/'})
+	cookies.remove('last_session')
+	window.user_detail = response.data.user
 	cb(null, response.data.user)
 }
 

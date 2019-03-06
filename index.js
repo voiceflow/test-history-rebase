fa634f732f265e2f952465536a078530cf49f18c
DecathectZero <tylerhan97@gmail.com>
2019-03-06 15:04:55 -0500

    master gucci

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 const fs = require('fs')
 
 if(process.env.NODE_ENV && fs.existsSync(`./.env.${process.env.NODE_ENV}`)){
-    if (process.env.NODE_ENV !== 'test') console.log(`Running in ${process.env.NODE_ENV} environment`)
-    require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}` })
+  if (process.env.NODE_ENV !== 'test') console.log(`Running in ${process.env.NODE_ENV} environment`)
+  require('dotenv').config({path:`./.env.${process.env.NODE_ENV}`})
+}else if(fs.existsSync(`./.env`)){
+  console.log(`No Environment Set/Not Found! Running default .env file`)
+  require('dotenv').config()
 }else{
-    console.log(`No Environment Set/Not Found! Running default .env file`)
-    require('dotenv').config()
+  console.log(`No Environment Set/Not Found! Hope you have your environment declared :O`)
 }
-console.log(`Process Environment`, process.env)
 
 const app = require('./app')
 const npmPackage = require('./package.json')

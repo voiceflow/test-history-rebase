ea8b788fbcac4d6be7d608208bb21bd5dac544ab
Jason <jason@getvoiceflow.com>
2019-03-09 19:46:13 -0500

    fix snapshot

--- src/views/pages/Canvas/__tests__/Canvas.test.js
+++ src/views/pages/Canvas/__tests__/Canvas.test.js
@@ -12,7 +12,7 @@ const error = jest.fn()
 describe('Canvas', () => {
     it('render canvas', () => {
         let skill = testSkill
-        const component = shallow(<Canvas skill={skill} setOnSave={setOnSave} />);
+        const component = shallow(<Canvas skill={skill} setOnSave={setOnSave} resetSkill={jest.fn()} />);
         expect(toJson(component)).toMatchSnapshot()
         component.unmount()
     });
@@ -21,7 +21,7 @@ describe('Canvas', () => {
         const spy = jest.spyOn(Canvas.prototype, "onSave");
         const unmount = jest.spyOn(Canvas.prototype, "componentWillUnmount");
         const diagram_id = "e9f52b0622f08ff1b21137bae05a242b"
-        const component = shallow(<Canvas skill={skill} diagram_id={diagram_id} onError={() => {}} updateSkill={() => {}} setOnSave={setOnSave} />);
+        const component = shallow(<Canvas skill={skill} resetSkill={jest.fn()} diagram_id={diagram_id} onError={() => {}} updateSkill={() => {}} setOnSave={setOnSave} />);
         component.unmount();
         expect(unmount).toHaveBeenCalled()
         expect(spy).toHaveBeenCalled()

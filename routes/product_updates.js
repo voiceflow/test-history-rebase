e872ec05fc30354a6e3e6b6b3452a0b1897952e0
DecathectZero <tylerhan97@gmail.com>
2019-03-13 14:21:51 -0400

    forgot to get rid of milliseconds

--- routes/product_updates.js
+++ routes/product_updates.js
@@ -4,7 +4,7 @@ exports.getUpdates = async (req, res) => {
   let ts = parseInt(parseInt(req.params.ts) / 1000)
 
   try{
-    let update_data = (await pool.query('SELECT * FROM product_updates WHERE created > to_timestamp($1)', [ts])).rows
+    let update_data = (await pool.query('SELECT * FROM product_updates WHERE created > to_timestamp($1) AND created < NOW()', [ts])).rows
     res.status(200).send(update_data)
   } catch (err) {
     res.sendStatus(500)
@@ -14,7 +14,7 @@ exports.getUpdates = async (req, res) => {
 
 exports.createUpdate = async (req, res) => {
   try{
-    await pool.query('INSERT INTO product_updates (type, details, created) VALUES ($1, $2, to_timestamp($3))', [req.body.type, req.body.details, parseInt(req.body.ts)])
+    await pool.query('INSERT INTO product_updates (type, details, created) VALUES ($1, $2, to_timestamp($3))', [req.body.type, req.body.details, (parseInt(req.body.ts)/1000)])
     res.sendStatus(200)
   } catch (err){
     res.sendStatus(500)

2627a792a36f02bf66a6517379313b542e4de572
Jason <jason@getvoiceflow.com>
2019-03-18 19:11:37 -0400

    clean-up warnings

--- app/src/views/pages/Skill/Publish.jsx
+++ app/src/views/pages/Skill/Publish.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
-import { compose } from 'recompose'
 
 import PublishAmazon from './PublishAmazon'
 import PublishMarket from '../PublishMarket/PublishMarket'

5c888e2a2aeee0780e3b854c03e9b9410cd8e545
Jason Zhao <zhaojason98@gmail.com>
2019-03-20 12:22:01 -0400

    [CORE-515] context menu on combine block elements (#284)
    * [CORE-515] copy and paste combine block elements
    
    * [CORE-515] update snapshots

--- src/views/components/SRD/widgets/BlockNodeWidget.js
+++ src/views/components/SRD/widgets/BlockNodeWidget.js
@@ -266,6 +266,13 @@ export class BlockNodeWidget extends BaseWidget {
 						this.forceUpdate()
 					}
 				}}
+				onContextMenu={(e) => {
+					if (this.props.node.parentCombine){
+						this.props.nodeProps.generateBlockMenu(e, this.props.node)
+						e.preventDefault()
+						e.stopPropagation()
+					}
+				}}
 				key={this.props.node.id}
 			>
 				{this.props.node.linter && this.props.node.linter.length > 0 &&

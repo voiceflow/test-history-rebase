3a32295d6923683d56b1f5d627aacacdc72f780d
Jason Zhao <zhaojason98@gmail.com>
2019-03-29 13:40:45 -0400

    Fix rearrange (#314) (#315)
    * [HOTFIX] no setstate
    
    * [HOTFIX] ondrop fix
    
    * cleanup

--- app/src/services/DragDropBackend/DragDropBackend.js
+++ app/src/services/DragDropBackend/DragDropBackend.js
@@ -452,7 +452,7 @@ export default class DragDropBackend {
       // Don't show a nice cursor but still prevent default
       // "drop and blow away the whole document" action.
       e.preventDefault();
-      e.dataTransfer.dropEffect = 'none';
+      // e.dataTransfer.dropEffect = 'none';
     } else if (this.checkIfCurrentDragSourceRectChanged()) {
       // Prevent animating to incorrect position.
       // Drop effect must be other than 'none' to prevent animation.

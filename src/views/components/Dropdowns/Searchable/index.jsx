16a7416d0cf2ab8e7d02443e3d8c0a9a47ab418c
Jason <jason@getvoiceflow.com>
2019-04-12 17:56:08 -0400

    Use bounding rect for dropdown

--- src/views/components/Dropdowns/Searchable/index.jsx
+++ src/views/components/Dropdowns/Searchable/index.jsx
@@ -19,7 +19,7 @@ export default class SearchableDropdown extends Component {
         return (
             <div class="search__dropdown">
                 <div id="myDropdown" class="search__dropdown_content">
-                    <input type="text" placeholder="Search.." id="myInput" onChange={this.onSearchChange} onKeyUp={e => e.stopPropagation()} />
+                    <input type="text" placeholder="Search..." id="myInput" onChange={this.onSearchChange} onKeyUp={e => e.stopPropagation()} />
                     
                     {this.state.searchFilter !== '' ?
                         _.filter(this.props.options, option => 

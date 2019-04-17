f100698205ceab96921f5185e943b421690d8279
Winston Chen <wx.chen@mail.utoronto.ca>
2019-04-17 12:33:58 -0400

    [CORE-521] Email Variable Mapping Fix (#373)

--- src/views/pages/Canvas/Editors/Mail.jsx
+++ src/views/pages/Canvas/Editors/Mail.jsx
@@ -11,24 +11,16 @@ export class Mail extends Component {
         super(props);
 
         let selected;
-        if(props.node.extras.template_id){
+        if (props.node.extras.template_id) {
             let find = props.templates.find(t => t.template_id === props.node.extras.template_id);
-            if(find){
+            if (find) {
                 selected = {
                     label: find.title,
                     value: find.template_id
                 }
 
-                if(props.node.extras.to && props.node.extras.to.length !== 0){
-                    props.node.extras.mapping = find.variables.map(v => {
-                        let existing = props.node.extras.mapping.find(kv => kv.key === v);
-                        let val;
-                        if(existing) val = existing.val;
-                        return {
-                            key: v,
-                            val: val
-                        }
-                    });
+                if (props.node.extras.to && props.node.extras.to.length !== 0) {
+                    this.updateMapping()
                 }
             }
         }
@@ -47,7 +39,26 @@ export class Mail extends Component {
         this.selectVariable = this.selectVariable.bind(this);
     }
 
-    onChange(e){
+    componentDidMount() {
+        this.updateMapping()
+    }
+
+    updateMapping = () => {
+        if (this.props.node.extras.template_id) {
+            let find = this.props.templates.find(t => t.template_id === this.props.node.extras.template_id);
+            this.props.node.extras.mapping = find.variables.map(v => {
+                let existing = this.props.node.extras.mapping.find(kv => kv.key === v);
+                let val;
+                if (existing) val = existing.val;
+                return {
+                    key: v,
+                    val: val
+                }
+            });
+        }
+    }
+
+    onChange(e) {
         let node = this.state.node;
         node.extras[e.target.name] = e.target.value;
 
@@ -58,7 +69,7 @@ export class Mail extends Component {
 
     selectVariable(selected, index) {
         let node = this.state.node;
-        if(!node.extras.mapping[index]) return;
+        if (!node.extras.mapping[index]) return;
         node.extras.mapping[index].val = selected.value;
 
         this.setState({
@@ -67,21 +78,21 @@ export class Mail extends Component {
     }
 
     selectTemplate(selected) {
-        
-        if(selected.template_id === this.state.node.extras.template_id) return;
+
+        if (selected.template_id === this.state.node.extras.template_id) return;
 
         let find = this.props.templates.find(t => t.template_id === selected.value);
         let node = this.state.node;
         node.extras.template_id = find.template_id;
 
-        if(Array.isArray(find.variables) && find.variables.length !== 0){
+        if (Array.isArray(find.variables) && find.variables.length !== 0) {
             node.extras.mapping = find.variables.map(v => {
                 return {
                     val: null,
                     key: v
                 }
             })
-        }else{
+        } else {
             node.extras.mapping = []
         }
 
@@ -94,11 +105,11 @@ export class Mail extends Component {
 
 
     render() {
-        if(this.props.templates.length === 0){
+        if (this.props.templates.length === 0) {
             return <div className="text-center">
-                <img className="mb-3 mt-5" src={'/images/email_2.svg'} alt="user" width="80"/><br/>
+                <img className="mb-3 mt-5" src={'/images/email_2.svg'} alt="user" width="80" /><br />
                 <span className="text-muted">You currently have no Email Templates</span>
-                <Link className="btn btn-secondary mt-3" to={`/business/${this.props.skill_id}/emails`}>Add Templates</Link> 
+                <Link className="btn btn-secondary mt-3" to={`/business/${this.props.skill_id}/emails`}>Add Templates</Link>
             </div>
         }
 
@@ -112,25 +123,27 @@ export class Mail extends Component {
                     value={this.state.selected}
                     onChange={this.selectTemplate}
                     placeholder='Select Email Template'
-                    options={this.props.templates.map(t => {return {
-                        value: t.template_id,
-                        label: t.title
-                    }})}
+                    options={this.props.templates.map(t => {
+                        return {
+                            value: t.template_id,
+                            label: t.title
+                        }
+                    })}
                 />
-                <hr/>
+                <hr />
                 <div className="label-btns">
                     <label>To</label>
-                    <Button outline={!user} disabled={user} color="primary" onClick={()=>{
+                    <Button outline={!user} disabled={user} color="primary" onClick={() => {
                         let node = this.state.node;
-                        if(node.extras.to === '_USER') return;
+                        if (node.extras.to === '_USER') return;
                         node.extras.to = '_USER';
-                        this.setState({node: node});
+                        this.setState({ node: node });
                     }}>User Email</Button>
-                    <Button outline={user} disabled={!user} color="primary" onClick={()=>{
+                    <Button outline={user} disabled={!user} color="primary" onClick={() => {
                         let node = this.state.node;
-                        if(node.extras.to !== '_USER') return;
+                        if (node.extras.to !== '_USER') return;
                         node.extras.to = '';
-                        this.setState({node: node});
+                        this.setState({ node: node });
                     }}>Defined</Button>
                 </div>
                 {
@@ -155,30 +168,30 @@ export class Mail extends Component {
                         This Message Will Only Be Sent If the User Consents to Sharing Their Email
                     </span>
                 }
-                <hr/>
+                <hr />
                 <label>Email Variable Map</label>
                 <div>
-                {
-                    this.state.node.extras.mapping.length !== 0 ?
-                        <React.Fragment> 
-                            {this.state.node.extras.mapping.map((v, i) => {
-                                return <div key={i} className="variable_map mb-2">
-                                    <Select
-                                        className="map-box"
-                                        classNamePrefix="variable-box"
-                                        placeholder="Variable"
-                                        value={v.val ? {label: '{' + v.val + '}', value: v.val} : null}
-                                        onChange={(select) => this.selectVariable(select, i)}
-                                        options={Array.isArray(this.props.variables) ? this.props.variables.map(variable => {
-                                            return {label: '{' + variable + '}', value: variable }
-                                        }) : null}
-                                    />
-                                    <i className="far fa-arrow-right"/>
-                                    <input readOnly className="map-box form-control" value={`{${v.key}}`}/>
-                                </div>
-                            })}
-                        </React.Fragment> : <i className="text-muted">No Variables Exist For This Email</i>
-                }
+                    {
+                        this.state.node.extras.mapping.length !== 0 ?
+                            <React.Fragment>
+                                {this.state.node.extras.mapping.map((v, i) => {
+                                    return <div key={i} className="variable_map mb-2">
+                                        <Select
+                                            className="map-box"
+                                            classNamePrefix="variable-box"
+                                            placeholder="Variable"
+                                            value={v.val ? { label: '{' + v.val + '}', value: v.val } : null}
+                                            onChange={(select) => this.selectVariable(select, i)}
+                                            options={Array.isArray(this.props.variables) ? this.props.variables.map(variable => {
+                                                return { label: '{' + variable + '}', value: variable }
+                                            }) : null}
+                                        />
+                                        <i className="far fa-arrow-right" />
+                                        <input readOnly className="map-box form-control" value={`{${v.key}}`} />
+                                    </div>
+                                })}
+                            </React.Fragment> : <i className="text-muted">No Variables Exist For This Email</i>
+                    }
                 </div>
             </div>
         );
--- src/views/pages/Canvas/index.jsx
+++ src/views/pages/Canvas/index.jsx
@@ -22,11 +22,12 @@ import CanvasWarning from './components/CanvasWarning'
 //Helpers
 import { combineAppendValidation, appendValidator } from './../../helpers/combineHelper'
 
-import { updateVersion, updateIntents, setCanFulfill } from "./../../../actions/versionActions";
-import { setVariables } from './../../../actions/variableActions'
+import { updateVersion, updateIntents, setCanFulfill } from "actions/versionActions";
+import { setVariables } from 'actions/variableActions'
 import { setCanvasError } from 'actions/userActions'
 import { renameDiagram } from 'actions/diagramActions'
 import { setError, setConfirm } from 'actions/modalActions'
+import { fetchEmails } from "actions/emailActions";
 
 import ActionGroup from './ActionGroup'
 import HelpModal from './HelpModal'
@@ -200,7 +201,15 @@ export class Canvas extends Component {
                 this.props.setError(this.props.integration_users_error)
             }
         })
-    }
+        if (window.user_detail && window.user_detail.admin > 0 && this.props.skill) {
+            // Re-load templates in case of change
+            try {
+                this.props.getEmails(this.props.skill.skill_id)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+}
 
     componentWillUnmount() {
         if(!this.props.preview && this.props.skill && this.props.skill.skill_id && this.props.diagram_id && !window.error){
@@ -1676,7 +1685,8 @@ const mapDispatchToProps = dispatch => {
     setCanvasError: (err) => dispatch(setCanvasError(err)),
     setError: (err) => dispatch(setError(err)),
     setConfirm: (confirm) => dispatch(setConfirm(confirm)),
-    getIntegrationsUsers: () => dispatch(fetchIntegrationUsers())
+    getIntegrationsUsers: () => dispatch(fetchIntegrationUsers()),
+    getEmails: (skill_id) => dispatch(fetchEmails(skill_id))
   }
 }
 

25d5d452bb9b779c9cd1f84fb460f629bf4cb8a3
DecathectZero <tylerhan97@gmail.com>
2019-04-17 18:31:49 -0400

    collab style updates

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -106,7 +106,6 @@ window.CreatorSocket.on("connect_error", socketFail);
 window.CreatorSocket.on("connect_failed", socketFail);
 // to catch connection events
 window.CreatorSocket.on("connect", () => {
-  console.log("CONNECTED")
   window.CreatorSocket.status = "CONNECTED";
   // queued up events after reconnection
   for (var cb in window.CreatorSocket.connectedCB) {

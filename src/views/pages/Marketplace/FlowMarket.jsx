afb3a51dd8d8241ed76d16d12996f53a6a3e56f5
groovy-tunes <chinsenjeremy@gmail.com>
2019-04-20 14:07:42 -0400

    Add debugging

--- src/views/pages/Marketplace/FlowMarket.jsx
+++ src/views/pages/Marketplace/FlowMarket.jsx
@@ -16,7 +16,7 @@ if(process.NODE_ENV === 'development'){
 } else {
   ESURL = 'http://localhost:8080/elasticsearch'
 }
-console.log(ESURL)
+console.log(process.NODE_ENV)
 class FlowMarket extends Component {
   constructor(props){
     super(props)

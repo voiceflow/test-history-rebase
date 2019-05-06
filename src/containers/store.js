9f68c058bc1d7f74259d7f5a977e2aa2d7836169
gwszeto <sszeto@uwaterloo.ca>
2019-05-06 16:26:56 -0400

    feat: added functionality to flow bar now able to go back and forth from node/diagram

--- src/containers/store.js
+++ src/containers/store.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { routerMiddleware } from 'connected-react-router'
 import thunk from "redux-thunk";
 import { createBrowserHistory } from 'history';
@@ -8,7 +9,7 @@ export const history = createBrowserHistory()
 
 export const store = createStore(
   rootReducer(history),
-  compose(
+  composeWithDevTools(
     applyMiddleware(
       routerMiddleware(history),
         thunk

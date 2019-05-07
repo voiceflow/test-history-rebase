658b376befaa8e234c386987708e1c815fbcc4fd
Winston Chen <wenxinchen11@hotmail.com>
2019-05-07 13:26:59 -0400

    Fix dotenv config setup

---
+++ envSetup.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV && fs.existsSync(`./.env.${process.env.NODE_ENV}`)) {
+  if (process.env.NODE_ENV !== 'test') console.log(`Running in ${process.env.NODE_ENV} environment`);
+  dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
+} else if (fs.existsSync('./.env')) {
+  console.log('No Environment Set/Not Found! Running default .env file');
+  dotenv.config();
+} else {
+  console.log('No Environment Set/Not Found! Hope you have your environment declared :O');
+}

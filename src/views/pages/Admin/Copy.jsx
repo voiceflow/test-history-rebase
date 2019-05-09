373e2150f23c99e766b02f65089e1aac067b4f11
DecathectZero <tylerhan97@gmail.com>
2019-05-08 17:18:31 -0700

    chore: admin skill lookup

--- src/views/pages/Admin/Copy.jsx
+++ src/views/pages/Admin/Copy.jsx
@@ -3,6 +3,7 @@ import Select from 'react-select';
 import { Button } from 'reactstrap';
 import axios from 'axios';
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 class Copy extends Component {
 
@@ -105,6 +106,10 @@ class Copy extends Component {
                         onChange={t => this.setState({skill: t})}
                         options={this.state.creator_skills}
                     />
+                    {this.state.skill && <>
+                      <i className="far fa-search text-dull mr-1"/>
+                      <Link to={`/admin/version/${this.state.skill.value}`}>{this.state.skill.value}</Link>
+                    </>}
                     <hr/>
                     <label>TO</label>
                     <div className="super-center mb-2">

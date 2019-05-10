b97ef0836326709238e60b493b756fc7ede819cf
GWSzeto <sszeto@uwaterloo.ca>
2019-05-10 15:47:28 -0400

    test: wip createing test suite for flowbar

--- app/jest.config.js
+++ app/jest.config.js
@@ -31,7 +31,7 @@ module.exports = {
   ],
   "moduleNameMapper": {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy"
+    "\\.(css|less)$": "identity-obj-proxy"
   },
   "moduleFileExtensions": [
     "web.js",
--- app/src/views/pages/Canvas/__tests__/FlowBar.test.js
+++ app/src/views/pages/Canvas/__tests__/FlowBar.test.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import { mount, shallow, render } from 'enzyme';
-import FlowBar from '../FlowBar';
+import { FlowBar } from '../FlowBar';
 import toJson from 'enzyme-to-json';
 
 const clickFn = jest.fn()
-
+const diagram = {
+    sub_diagrams: ["1", "2", "3"]
+}
+const diagrams = [
+    {
+        id: "1",
+        name: "diagram_1",
+        sub_diagrams: ["2", "3"]
+    },
+    {
+        id: "2",
+        name: "diagram_2",
+        sub_diagrams: ["1", "3"]
+    },
+    {
+        id: "3",
+        name: "diagram_3",
+        sub_diagrams: ["1", "2"]
+    }
+]
 describe('FlowBar', () => {
     it('render flow bar', () => {
-        const component = shallow(<FlowBar />);
+        const component = shallow(<FlowBar diagram={diagram} diagrams={diagrams} />);
         expect(toJson(component)).toMatchSnapshot()
     });
+    it('renders with the correct childDiagrams', () => {
+        const component = mount(<FlowBar diagram={diagram} diagrams={diagrams} />);
+        console.log(component.debug())
+    })
 })

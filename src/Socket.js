a08efc39e07a250beafe07d2355e00c24be37069
William Huang <william2958@gmail.com>
2019-05-14 13:47:00 -0400

    refactor: moved socket into independant js file

--- src/Socket.js
+++ src/Socket.js
@@ -1,59 +1,43 @@
-import { Component } from 'react';
 import socket from 'socket.io-client';
 
-class Socket extends Component {
+const getEndpoint = () => {
+	let port = '';
+	let protocol = 'https';
+	if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+		port = ':8080';
+		protocol = 'http';
+	}
+	return `${protocol}://${window.location.hostname}${port}`;
+};
 
-	getEndpoint = () => {
-		let port = '';
-		let protocol = 'https';
-		if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-			port = ':8080';
-			protocol = 'http';
+const socketFail = () => {
+	window.CreatorSocket.status = 'FAIL';
+};
+
+window.CreatorSocket = socket(getEndpoint());
+window.CreatorSocket.connectedCB = {};
+// catch error events
+window.CreatorSocket.on('fail', socketFail);
+window.CreatorSocket.on('error', socketFail);
+// to catch if the server is offline
+window.CreatorSocket.on('connect_error', socketFail);
+// catch failed connection attempts
+window.CreatorSocket.on('connect_failed', socketFail);
+// to catch connection events
+window.CreatorSocket.on('connect', () => {
+	window.CreatorSocket.status = 'CONNECTED';
+	// queued up events after reconnection
+	for (var cb in window.CreatorSocket.connectedCB) {
+		if (typeof window.CreatorSocket.connectedCB[cb] === 'function') {
+			window.CreatorSocket.connectedCB[cb]();
 		}
-		return `${protocol}://${window.location.hostname}${port}`;
-	};
-
-	socketFail = () => {
-		window.CreatorSocket.status = 'FAIL';
-	};
-
-	componentDidMount() {
-		window.CreatorSocket = socket(this.getEndpoint());
-		window.CreatorSocket.connectedCB = {};
-		// catch error events
-		window.CreatorSocket.on('fail', this.socketFail);
-		window.CreatorSocket.on('error', this.socketFail);
-		// to catch if the server is offline
-		window.CreatorSocket.on('connect_error', this.socketFail);
-		// catch failed connection attempts
-		window.CreatorSocket.on('connect_failed', this.socketFail);
-		// to catch connection events
-		window.CreatorSocket.on('connect', () => {
-			window.CreatorSocket.status = 'CONNECTED';
-			// queued up events after reconnection
-			for (var cb in window.CreatorSocket.connectedCB) {
-				if (typeof window.CreatorSocket.connectedCB[cb] === 'function') {
-					window.CreatorSocket.connectedCB[cb]();
-				}
-			}
-		});
-
-		window.addEventListener('beforeunload', function () {
-			if (window.CreatorSocket && window.CreatorSocket.disconnect) {
-				window.CreatorSocket.disconnect();
-			}
-		});
 	}
+});
 
-	render() {
-		return null;
+window.addEventListener('beforeunload', function () {
+	if (window.CreatorSocket && window.CreatorSocket.disconnect) {
+		window.CreatorSocket.disconnect();
 	}
+});
 
-
-}
-
-export default Socket;
-
-
-
-
+console.log("Socket ran");

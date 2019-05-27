0443456ab9f1ddb4c65dccce6b4055d5bb7261f6
DecathectZero <tylerhan97@gmail.com>
2019-05-26 21:34:24 -0400

    test: updated unit tests

--- tests/lib/services/accountManager.unit.js
+++ tests/lib/services/accountManager.unit.js
@@ -50,6 +50,10 @@ const defaultServices = {
   googleClient: {},
 };
 
+// DUMMY GLOBAL CONFIG VALUES
+const CONFIG_CLIENT_ID = 'CONFIG_CLIENT_ID';
+const CONFIG_CLIENT_SECRET = 'CONFIG_CLIENT_SECRET';
+
 describe('accountManager unit tests', () => {
   it('generates valid userHash', async () => {
     const randomstring = new DeterministicRandomString();
@@ -111,7 +115,7 @@ describe('accountManager unit tests', () => {
     const accountManager = new AccountManager(services, {});
 
     const userObject = {
-      creator_id: mockAccount.id,
+      id: mockAccount.creator_id,
       name: mockAccount.name,
       email: mockAccount.email,
       admin: mockAccount.admin,
@@ -167,7 +171,7 @@ describe('accountManager unit tests', () => {
           });
           return {
             data: {
-              expire: 0,
+              expires_in: 0,
               access_token: 'ACCESS_TOKEN_1',
               refresh_token: 'REFRESH_TOKEN_1',
             },
@@ -182,17 +186,167 @@ describe('accountManager unit tests', () => {
       expire: Date.now() + 1000,
     });
 
-    const accountManager = new AccountManager(services, {});
+    const accountManager = new AccountManager(services, {
+      CONFIG_CLIENT_ID,
+      CONFIG_CLIENT_SECRET,
+    });
 
     // should be able to consistently fetch access token
     expect(await accountManager.AmazonAccessToken(1)).to.eq('ACCESS_TOKEN');
     expect(await accountManager.AmazonAccessToken(1)).to.eq('ACCESS_TOKEN');
 
     redisStore[`${AccountManager.CONSTANTS.AMAZON_TOKEN_PREFIX}1`] = JSON.stringify({
+      expire: Date.now() - 1000,
       access_token: 'ACCESS_TOKEN',
       refresh_token: 'REFRESH_TOKEN',
-      expire: Date.now() - 1000,
     });
     expect(await accountManager.AmazonAccessToken(1)).to.eq('ACCESS_TOKEN_1');
+    expect(requests.length).to.eq(1);
+    expect(requests[0]).to.deep.eq({
+      url: 'https://api.amazon.com/auth/o2/token',
+      data: {
+        grant_type: 'refresh_token',
+        client_id: CONFIG_CLIENT_ID,
+        client_secret: CONFIG_CLIENT_SECRET,
+        refresh_token: 'REFRESH_TOKEN',
+      },
+    });
+
+    const newSession = JSON.parse(redisStore[`${AccountManager.CONSTANTS.AMAZON_TOKEN_PREFIX}1`]);
+    expect(newSession.access_token).to.eq('ACCESS_TOKEN_1');
+    expect(newSession.refresh_token).to.eq('REFRESH_TOKEN_1');
+  });
+
+  it('deletes amazon token', async () => {
+    const redisStore = {
+      [`${AccountManager.CONSTANTS.AMAZON_TOKEN_PREFIX}1`]: 'test',
+      [`${AccountManager.CONSTANTS.AMAZON_TOKEN_PREFIX}2`]: 'test',
+    };
+
+    const services = {
+      ...defaultServices,
+      redis: {
+        del: (key) => delete redisStore[key],
+      },
+    };
+
+    const accountManager = new AccountManager(services, {});
+
+    await accountManager.deleteAmazonAccessToken(1);
+    // one item should be left in store
+    expect(Object.keys(redisStore).length).to.eq(1);
+
+    await accountManager.deleteAmazonAccessToken(2);
+    // store should be cleared
+    expect(Object.keys(redisStore).length).to.eq(0);
+  });
+
+  it('creates amazon token', async () => {
+    const requests = [];
+    const redisStore = {};
+
+    const services = {
+      ...defaultServices,
+      redis: {
+        set: (key, value) => {
+          redisStore[key] = value;
+        },
+      },
+      axios: {
+        post: (url, data) => {
+          requests.push({
+            url,
+            data,
+          });
+          return {
+            data: {
+              expires_in: 0,
+              access_token: 'ACCESS_TOKEN',
+              refresh_token: 'REFRESH_TOKEN',
+            },
+          };
+        },
+      },
+    };
+
+    const accountManager = new AccountManager(services, {
+      CONFIG_CLIENT_ID,
+      CONFIG_CLIENT_SECRET,
+    });
+
+    // should be able to consistently fetch access token
+    expect(await accountManager.createAmazonSession(1, 'test_code')).to.eq('ACCESS_TOKEN');
+
+    const newSession = JSON.parse(redisStore[`${AccountManager.CONSTANTS.AMAZON_TOKEN_PREFIX}1`]);
+
+    expect(newSession.access_token).to.eq('ACCESS_TOKEN');
+    expect(newSession.refresh_token).to.eq('REFRESH_TOKEN');
+
+    expect(requests.length).to.be.eql(1);
+    expect(requests[0].url).to.eq('https://api.amazon.com/auth/o2/token');
+    expect(requests[0].data.code).to.eq('test_code');
+    expect(requests[0].data.grant_type).to.eq('authorization_code');
+    expect(requests[0].data.client_id).to.eq(CONFIG_CLIENT_ID);
+    expect(requests[0].data.client_secret).to.eq(CONFIG_CLIENT_SECRET);
+  });
+
+  it('checks for login', async () => {
+    const fakeRow = {
+      accountStuff: 'stuff',
+      password: 'password2',
+    };
+    const queries = [];
+    const services = {
+      ...defaultServices,
+      pool: {
+        query: (query, params) => {
+          queries.push({
+            query,
+            params,
+          });
+          return {
+            rows: [{ ...fakeRow }],
+          };
+        },
+      },
+      bcrypt: {
+        compare: (a, b) => {
+          if (a !== b) throw new Error();
+          return true;
+        },
+      },
+    };
+
+    const accountManager = new AccountManager(services, {});
+
+    expect(await accountManager.checkLogin({ email: 'joe@vf.com', password: 'password2' })).to.deep.eq({
+      ...fakeRow,
+    });
+  });
+
+  it('checks incorrect password', async () => {
+    const services = {
+      ...defaultServices,
+      pool: {
+        query: () => ({
+          rows: [
+            {
+              accountStuff: 'stuff',
+              password: 'password2',
+            },
+          ],
+        }),
+      },
+      bcrypt: {
+        compare: (a, b) => {
+          if (a !== b) throw new Error();
+          return true;
+        },
+      },
+    };
+
+    const accountManager = new AccountManager(services, {});
+
+    expect(accountManager.checkLogin({ email: 'joe@vf.com', password: 'password1' })).to.eventually.be.rejectedWith('Username or Password Incorrect');
   });
 });

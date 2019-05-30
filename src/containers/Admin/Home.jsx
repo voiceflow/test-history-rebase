56920a6e5de75c9727af80104526a7856a5ce2aa
Jason <jason@getvoiceflow.com>
2019-05-30 16:15:38 -0400

    fix: remove console error

--- src/containers/Admin/Home.jsx
+++ src/containers/Admin/Home.jsx
@@ -1,44 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import InternalLookup from "./components/InternalLookup/InternalLookup";
 
 
-const Home = (props) => {
-  return (
-    <div className="admin-page-inner">
-      <div className="subheader">
-        <div className="space-between">
-          <span className="subheader-title">
-            <b>Home</b>
-            <div className="hr-label">
-              <small><i className="far fa-user mr-1"></i></small>
-              {' '}
-              {props.user.name}{' '}
-              <small><i className="far fa-chevron-right"/></small>
-              {' '}
-              <span className="text-secondary">Admin</span>
-            </div>
-          </span>
-        </div>
-      </div>
-      <div className="p-5">
+class Home extends Component {
 
-        {/*Admin Header*/}
-        <div>
-          <h3><span className={'crossed_out'}>Tyler's</span>
-            &nbsp; Will's Lookup Emporium <span className={'admin_highlight'}>New and improved!</span>
-          </h3>
-        </div>
+	render() {
 
-        <InternalLookup user={props.user}/>
+		return (
+			<div className="admin-page-inner">
+				<div className="subheader">
+					<div className="space-between">
+			            <span className="subheader-title">
+			              <b>Home</b>
+			              <div className="hr-label">
+			                <small><i className="far fa-user mr-1"></i></small>
+				              {' '}
+				              {this.props.user.name}{' '}
+				              <small><i className="far fa-chevron-right"/></small>
+				              {' '}
+				              <span className="text-secondary">Admin</span>
+			              </div>
+			            </span>
+					</div>
+				</div>
+				<div className="p-5">
 
-      </div>
-    </div>
-  );
+					 {/*Admin Header*/}
+					<div>
+						<h3><span className={'crossed_out'}>Tyler's</span>
+							&nbsp; Will's Lookup Emporium <span className={'admin_highlight'}>New and improved!</span>
+						</h3>
+					</div>
+
+					<InternalLookup />
+
+				</div>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => ({
-  user: state.account
+	user: state.account
 });
 
 export default connect(mapStateToProps)(Home);

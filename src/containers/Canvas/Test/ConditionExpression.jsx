65642bb0659cb9e789cd276578796993d6830758
Jason <jason@getvoiceflow.com>
2019-06-06 16:56:20 -0400

    fix: remove collapses

--- src/containers/Canvas/Test/ConditionExpression.jsx
+++ src/containers/Canvas/Test/ConditionExpression.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import cn from 'classnames'
-import { Collapse } from 'reactstrap'
-
-import { useToggle } from 'hooks/toggle'
+import cn from 'classnames';
 
 import { Input } from 'reactstrap'
 
@@ -12,26 +9,15 @@ const ConditionExpression = props => {
         onSelection,
     } = props
 
-    const [show, toggleShow] = useToggle(true)
-
     return (
         <div className="mb-2 px-3">
             <div className="break" />
-            <div className="condition-label" onClick={() => toggleShow()}>
-                <div className="text-left w-100">{`${variable}`}</div>
-                <i className={cn("fas","light-grey", "d-flex", "align-items-center", {
-                    "fa-chevron-up": show,
-                    "fa-chevron-down": !show
-                })} />
+            <div className="variable-group">
+                <span>Set</span>
+                <span className="action-visible light-blue">{`{${variable}}`}</span>
+                <span>to:</span>
             </div>
-            <Collapse isOpen={show}>
-                <div className="variable-group">
-                    <span>Set</span>
-                    <span className="action-visible light-blue">{`{${variable}}`}</span>
-                    <span>to:</span>
-                </div>
-                <Input type="text" onChange={e => onSelection(variable, e.target.value)} />
-            </Collapse>
+            <Input type="text" placeholder={`{${variable}}`} onChange={e => onSelection(variable, e.target.value)} />
         </div>
     );
 }
--- src/containers/Canvas/Test/index.jsx
+++ src/containers/Canvas/Test/index.jsx
@@ -96,15 +96,11 @@ class Test extends Component {
               section = null
           }
 
-          return <div key={i} className="mt-3 mb-3 sidebar_container">
+          return <div key={i} className="mt-3 sidebar_container">
             <div className="condition-label" onClick={() => {this.toggleSection(s)}}>
             <label id={s} className='ml-3 mt-2 text-left'>{s}</label>
-              <i className={cn("fas", "light-grey", "d-flex", "align-items-center", {
-                "fa-chevron-up": s === C.CONDITIONS ? conditionsOpen : timelineOpen,
-                "fa-chevron-down": s === C.CONDITIONS ? !conditionsOpen: !timelineOpen,
-              })} />
             </div>
-            <Collapse isOpen={s === C.CONDITIONS ? !this.props.testing_info && this.state.conditionsOpen: this.state.timelineOpen}>
+            <Collapse isOpen={s === C.CONDITIONS ? !this.props.testing_info: this.props.testing_info}>
               {section}
             </Collapse>
             {s === C.CONDITIONS && <div className="break" />}

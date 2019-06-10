f6661cb74a47063f9a2bed5af46a2d0f12a76b13
Frank Gu <frank@voiceflow.com>
2019-06-10 17:51:54 -0400

    fix(oauth): changed oauth ids

--- src/containers/Register/social-id.js
+++ src/containers/Register/social-id.js
@@ -1,3 +1,3 @@
 exports.googleClient = "792099969137-uf81ar579b5ea2ll96a37r3ruucfbrmv.apps.googleusercontent.com";
-exports.stagingGoogleClient = "60536514156-b0rhdgh006vmo07p1as9s236jmqurugj.apps.googleusercontent.com";
+exports.devGoogleClient = "60536514156-b0rhdgh006vmo07p1as9s236jmqurugj.apps.googleusercontent.com";
 exports.fbId = "382723665627980";

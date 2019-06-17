27694a9be2d250c7458b4e9ed4739f97f4aba26a
Eric Hacke <ehacke@gmail.com>
2019-06-17 17:28:35 -0400

    fix: handle non-string errors

--- src/containers/Register/ErrorWidget.jsx
+++ src/containers/Register/ErrorWidget.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Alert } from 'reactstrap';
+import { isString } from 'lodash';
 
 // eslint-disable-next-line react/display-name
-export default ({ error, color }) => (error ? <Alert color={color}>{error}</Alert> : null);
+export default ({ error, color }) => (error ? <Alert color={color}>{isString(error) ? error : JSON.stringify(error)}</Alert> : null);

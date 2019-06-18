35e45af37e463b57cb5dbf165aa4f5e1e565811f
Will <william2958@gmail.com>
2019-06-18 12:08:16 -0400

    style: fixed linting issues

--- src/containers/Canvas/Editors/components/VariableInput.jsx
+++ src/containers/Canvas/Editors/components/VariableInput.jsx
@@ -9,22 +9,6 @@ import React, { Component } from 'react';
 
 import './VariableInput.css';
 
-const isOutOfViewport = function(elem) {
-  // Get element's bounding
-  const bounding = elem.getBoundingClientRect();
-
-  // Check if it's out of the viewport on each side
-  const out = {};
-  out.top = bounding.top < 0;
-  out.left = bounding.left < 0;
-  out.bottom = bounding.bottom > (window.innerHeight || document.documentElement.clientHeight);
-  out.right = bounding.right > (window.innerWidth || document.documentElement.clientWidth);
-  out.any = out.top || out.left || out.bottom || out.right;
-  out.all = out.top && out.left && out.bottom && out.right;
-
-  return out;
-};
-
 class VariableInput extends Component {
   constructor(props) {
     super(props);

e2589dd75c2343c6573b99db6dcdcd78e0eb5027
Frank Gu <frank@voiceflow.com>
2019-06-21 11:12:03 -0400

    fix(cookie): changed cookie domain options

--- src/cookies.js
+++ src/cookies.js
@@ -4,21 +4,27 @@ export const AUTH_COOKIE = 'auth';
 
 const cookies = new Cookies();
 
-export const removeAuthCookie = () => cookies.remove(AUTH_COOKIE, { path: '/' });
+const creatorUrl = 'creator.voiceflow.com';
+const legacyUrl = 'creator.getvoiceflow.com';
+const cookieDomain = window.location.host === creatorUrl ? '.voiceflow.com' : window.location.host;
+const cookieOptions = { path: '/', domain: cookieDomain };
 
-export const setAuthCookie = (token) => cookies.set(AUTH_COOKIE, token, { path: '/' });
+export const removeAuthCookie = () => cookies.remove(AUTH_COOKIE, cookieOptions);
+
+export const setAuthCookie = (token) => cookies.set(AUTH_COOKIE, token, cookieOptions);
 
 export function getAuthCookie() {
   switch (window.location.host) {
-    case 'creator.voiceflow.com':
-      cookies.remove(AUTH_COOKIE, { path: '/', domain: 'creator.getvoiceflow.com' });
+    case creatorUrl:
+      cookies.remove(AUTH_COOKIE, { path: '/', domain: legacyUrl });
       break;
-    case 'creator.getvoiceflow.com':
-      cookies.remove(AUTH_COOKIE, { path: '/', domain: 'creator.voiceflow.com' });
+    case legacyUrl:
+      cookies.remove(AUTH_COOKIE, cookieOptions);
       break;
+    // no default
   }
 
-  return cookies.get(AUTH_COOKIE);
+  return cookies.get(AUTH_COOKIE, cookieOptions);
 }
 
 export const removeLastSessionCookie = () => cookies.remove('last_session');

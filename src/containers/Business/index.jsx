8b76309bbee59a74c137b78a4c7a788f7487abe8
Will <william2958@gmail.com>
2019-06-27 15:35:26 -0400

    style: fixed style

--- src/containers/Business/index.jsx
+++ src/containers/Business/index.jsx
@@ -2,6 +2,7 @@ import './Business.css';
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
 import Home from './Home';
 import EditProduct from './Products/EditProduct';
 import Products from './Products/Products';

01a12ec03ad25e047fbb8d5ae548afe57434d148
Will <william2958@gmail.com>
2019-07-02 10:35:49 -0400

    style: styling changes

--- src/containers/Canvas/Editors/components/integrations/googleSheets/addUserModal.jsx
+++ src/containers/Canvas/Editors/components/integrations/googleSheets/addUserModal.jsx
@@ -40,7 +40,7 @@ class GoogleAddUserModal extends Component {
         </div>
         <div className="d-flex justify-content-center mx-5 my-3">
           <GoogleLogin
-            clientId={process.env.REACT_APP_BUILD_ENV === 'production' ? googleClient : devGoogleClient }
+            clientId={process.env.REACT_APP_BUILD_ENV === 'production' ? googleClient : devGoogleClient}
             className="social-button class-ggl mb-4"
             buttonText="Login with Google"
             onSuccess={this.googleLogin}

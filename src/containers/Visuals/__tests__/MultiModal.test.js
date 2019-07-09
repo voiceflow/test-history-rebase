03ba1025e99e5657ad4a042749a7def1ed2e26c4
spacevash <nidhi.gaday@gmail.com>
2019-07-09 15:45:54 -0400

    fix: visual template can delete now

---
+++ src/containers/Visuals/__tests__/MultiModal.test.js
@@ -0,0 +1,11 @@
+// import toJson from 'enzyme-to-json';
+// import { shallow } from 'enzyme/build';
+// import React from 'react';
+
+// import MultiModal from '../Multimodal';
+
+// describe('MultiModal', () => {
+//   it('renders MultiModal', () => {
+//     const component = shallow(<MultiModal />);
+//   });
+// });

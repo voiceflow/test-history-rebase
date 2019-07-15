0a57e95ab6d170c154b58b2d374a2500a625286f
Edwin Zhang <omegalink12@gmail.com>
2019-07-15 16:56:23 -0400

    feat: use references instead of templates

---
+++ src/components/Reference.jsx
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import React, { useEffect } from 'react';
+
+function Reference(props) {
+  const project_id = props.match.params.project_id;
+  const history = props.history;
+  useEffect(() => {
+    axios.post(`/project/${project_id}/use_reference`).then((res) => history.push(`/canvas/${res.data.skill_id}`));
+  }, [project_id]);
+  return (
+    <div id="loading-diagram">
+      <div className="text-center">
+        <h5 className="text-muted mb-2">Loading Project...</h5>
+        <span className="loader" />
+      </div>
+    </div>
+  );
+}
+export default Reference;

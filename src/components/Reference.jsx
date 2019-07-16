792d29d6b207528944a38f1463269c296e5a7f06
Edwin Zhang <omegalink12@gmail.com>
2019-07-16 12:42:20 -0400

    fix: replace history

--- src/components/Reference.jsx
+++ src/components/Reference.jsx
@@ -5,7 +5,7 @@ function Reference(props) {
   const project_id = props.match.params.project_id;
   const history = props.history;
   useEffect(() => {
-    axios.post(`/project/${project_id}/use_reference`).then((res) => history.push(`/canvas/${res.data.skill_id}`));
+    axios.post(`/project/${project_id}/use_reference`).then((res) => history.replace(`/canvas/${res.data.skill_id}`));
   }, [project_id]);
   return (
     <div id="loading-diagram">

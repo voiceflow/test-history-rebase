7b2b8b391a0aad3654cc315ecdc502f14d301364
Jason <zhaojason98@gmail.com>
2019-07-18 13:06:40 -0400

    chore: move to styled component

--- src/components/Spinner/FullPageSpinner.jsx
+++ src/components/Spinner/FullPageSpinner.jsx
@@ -1,14 +1,25 @@
-import cn from 'classnames';
 import React from 'react';
+import styled from 'styled-components';
 
 import Spinner from './Spinner';
 
+const Diagram = styled.div`
+  position: ${(props) => (props.isAbs ? 'absolute' : 'fixed')};
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 6;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const FullSpinner = (props) => {
-  const { transparent } = props;
   return (
-    <div id="loading-diagram" className={cn({ transparent })}>
+    <Diagram>
       <Spinner {...props} />
-    </div>
+    </Diagram>
   );
 };
 

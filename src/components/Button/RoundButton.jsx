fd5ff5d6d59a5ca71aaae4cb82c7811815f1eea3
Jason <zhaojason98@gmail.com>
2019-07-24 17:26:13 -0400

    chore: remove hover if controlled

--- src/components/Button/RoundButton.jsx
+++ src/components/Button/RoundButton.jsx
@@ -86,7 +86,7 @@ const Button = styled.button`
   background-image: ${({ color }) => (color ? `linear-gradient(${color}15, ${color}30)` : 'none')};
 
   &:hover {
-    ${hoverStyles}
+    ${({ active }) => !active && hoverStyles}
   }
   ${({ active }) => active && activeStyles}
   &:active {

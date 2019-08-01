b7938807a1acb5cb5d38a8ff9288b445bc6c04f6
Jason Zhao <zhaojason98@gmail.com>
2019-08-01 11:09:21 -0400

    feat: new success modal for submitting to amazon (#200)

--- src/components/Modals/ModalHeader.jsx
+++ src/components/Modals/ModalHeader.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import Button from '@/components/Button';
 
-export const ModalHeader = ({ header, toggle }) => (
+export const ModalHeader = ({ header, toggle, children }) => (
   <div className="modal-header">
-    <h5 className="modal-title">{header}</h5>
+    <h5 className="modal-title">{header || children}</h5>
     <Button className="close" aria-label="Close" onClick={toggle} />
   </div>
 );

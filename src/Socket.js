7a533816fe0784f5368008cd3eac4a300175aca4
omegalink12 <omegalink12@users.noreply.github.com>
2019-08-28 14:45:40 -0400

    feat: push messages (#279)

--- src/Socket.js
+++ src/Socket.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 import LogRocket from 'logrocket';
 import setupLogRocketReact from 'logrocket-react';
+import Markdown from 'markdown-to-jsx';
 import randomstring from 'randomstring';
+import React from 'react';
+import { Alert } from 'reactstrap';
 import socket from 'socket.io-client';
 
 import { getDevice } from '@/Helper';
 import { API_HOST, IS_DEVELOPMENT, LOGROCKET_ENABLED, LOGROCKET_PROJECT } from '@/config';
 import { getAuthCookie } from '@/cookies';
+import { setConfirm } from '@/ducks/modal';
 
 // setup LogRocket
 if (LOGROCKET_ENABLED) {
@@ -78,4 +82,24 @@ window.addEventListener('beforeunload', () => {
 
 window.CreatorSocket.on('force_refresh', () => {
   window.location.reload(true);
+});
+
+window.CreatorSocket.on('show_message', (data) => {
+  if (data.redirect) {
+    window.location.replace(data.redirect);
+    // eslint-disable-next-line xss/no-location-href-assign
+    window.location.href = data.redirect;
+    throw new Error('REDIRECT');
+  } else if (data.message && window.store) {
+    window.store.dispatch(
+      setConfirm({
+        size: 'rg',
+        text: (
+          <Alert className="mb-0" color={data.type}>
+            <Markdown>{data.message}</Markdown>
+          </Alert>
+        ),
+      })
+    );
+  }
 });

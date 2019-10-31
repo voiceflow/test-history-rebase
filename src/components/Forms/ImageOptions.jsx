be6d44d3933f2c43f6358cf873ef50db8e0a82ca
Joshua <31071869+JoshTheDeveloper@users.noreply.github.com>
2019-10-31 16:11:43 -0400

    fix: wrapped the buttons in the correct component (#728)

--- src/components/Forms/ImageOptions.jsx
+++ src/components/Forms/ImageOptions.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Button from '@/components/Button';
+import { ButtonCard } from '@/containers/Onboarding/container';
 
 export default (props) => {
   const option_map = props.options.map((o) => o.type);
@@ -10,12 +11,10 @@ export default (props) => {
       <p className="text-muted text-center mb-3 mt-4">{props.question}</p>
       <div className="row justify-content-center mb-4">
         {props.options.map((o) => (
-          <div className="selector-card col-s ml-2 mr-2" key={o.type}>
-            <Button isTransparent className="mb-2" onClick={() => props.update(o.type)}>
-              <img className="image-selector" alt="intermediate" src={props.state === o.type ? o.selected : o.unselected} />
-            </Button>
+          <ButtonCard className="selector-card col-s ml-2 mr-2" key={o.type} onClick={() => props.update(o.type)}>
+            <img className="image-selector" alt="intermediate" src={props.state === o.type ? o.selected : o.unselected} />
             <p className={props.state === o.type ? '' : 'text-muted'}>{o.text}</p>
-          </div>
+          </ButtonCard>
         ))}
       </div>
       <div className="justify-content-center">

59cf36312c3d4dc724157948e410de8bf98e5b72
Tyler Han <tylerhan97@gmail.com>
2019-10-31 09:51:50 -0400

    fix: account linking not showing up for permission card (#722)

--- src/client/adapters/creator/block/permission.js
+++ src/client/adapters/creator/block/permission.js
@@ -1,15 +1,17 @@
 import { createBlockAdapter } from './utils';
 
 const permissionBlockAdapter = createBlockAdapter(
-  ({ settings, custom, permissions }) => ({
+  ({ settings, custom, permissions, a_l }) => ({
     custom: custom || false,
     permissions,
     settingsOpen: settings,
+    accountLinking: !!a_l,
   }),
-  ({ settingsOpen, custom, permissions }) => ({
+  ({ settingsOpen, custom, permissions, accountLinking }) => ({
     custom,
     permissions,
     settings: settingsOpen,
+    a_l: !!accountLinking,
   })
 );
 

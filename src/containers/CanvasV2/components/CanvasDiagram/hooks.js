7e7ae4b9fa2341fc9708c4a61fd741e08b4c2b82
Lauren Greenway <lagreenway@gmail.com>
2019-12-09 13:44:39 -0500

    fix: transform NewLink mouse coordinates on pan (#972)
    * chore: lint error
    
    * refactor: use existing canvas transform method (#986)

--- src/containers/CanvasV2/components/CanvasDiagram/hooks.js
+++ src/containers/CanvasV2/components/CanvasDiagram/hooks.js
@@ -2,35 +2,45 @@ import mouseEventOffset from 'mouse-event-offset';
 import React from 'react';
 
 import { DEBUG_REALTIME } from '@/config';
-import { EngineContext } from '@/containers/CanvasV2/contexts';
+import { EngineContext, LinkCreationContext } from '@/containers/CanvasV2/contexts';
 import * as Realtime from '@/ducks/realtime';
 
 // eslint-disable-next-line import/prefer-default-export
 export const useCursorControls = () => {
   const mousePosition = React.useRef(null);
   const engine = React.useContext(EngineContext);
-
+  const linkCreation = React.useContext(LinkCreationContext);
   const moveMouse = React.useCallback((location) => {
     if (DEBUG_REALTIME) return;
 
     engine.realtime.sendVolatileUpdate(Realtime.moveMouse(location));
   }, []);
 
-  const panViewport = React.useCallback((moveX, moveY) => {
-    engine.realtime.panViewport(moveX, moveY);
+  const panViewport = React.useCallback(
+    (moveX, moveY) => {
+      engine.realtime.panViewport(moveX, moveY);
 
-    if (mousePosition.current !== null && engine.canvas.isTrackpadPanning()) {
-      const zoom = engine.canvas.getZoom();
-      const [currX, currY] = mousePosition.current;
-      const nextMousePosition = [currX - moveX / zoom, currY - moveY / zoom];
+      if (mousePosition.current !== null && engine.canvas.isTrackpadPanning()) {
+        const zoom = engine.canvas.getZoom();
+        const [currX, currY] = mousePosition.current;
+        const nextMousePosition = [currX - moveX / zoom, currY - moveY / zoom];
 
-      mousePosition.current = nextMousePosition;
+        if (linkCreation.isDrawing) {
+          const transformedPosition = engine.canvas.reverseTransformPoint(nextMousePosition);
 
-      moveMouse(nextMousePosition);
-    }
+          linkCreation.onAbort();
+          linkCreation.onStart(linkCreation.sourcePortID, transformedPosition);
+        }
+        mousePosition.current = nextMousePosition;
+        moveMouse(nextMousePosition);
+      }
+    },
+    [linkCreation, engine]
+  );
+
+  const zoomViewport = React.useCallback((calculateMovement) => {
+    engine.realtime.zoomViewport(calculateMovement);
   }, []);
-
-  const zoomViewport = React.useCallback((calculateMovement) => engine.realtime.zoomViewport(calculateMovement), []);
 
   const updateViewport = React.useCallback(({ x, y, zoom }) => engine.updateViewport(x, y, zoom), []);
 

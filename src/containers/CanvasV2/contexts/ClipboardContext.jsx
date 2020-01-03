0841cd762d6ceff64a1a1c452538d06a3aa4f2c8
Evgeny Zaytsev <overhawlin@gmail.com>
2020-01-03 19:40:56 +0300

    fix: paste text in the test tool (#1035)

--- src/containers/CanvasV2/contexts/ClipboardContext.jsx
+++ src/containers/CanvasV2/contexts/ClipboardContext.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { MousePositionContext } from '@/contexts';
 import { withContext } from '@/hocs';
-import { preventDefault } from '@/utils/dom';
 
 import { EngineContext } from './EngineContext';
 
@@ -13,23 +12,31 @@ export const BufferType = {
 export const ClipboardContext = React.createContext(null);
 export const { Consumer: ClipboardConsumer } = ClipboardContext;
 
+const IGNORED_TAGS = ['TEXTAREA', 'INPUT'];
+
 export const ClipboardProvider = ({ children }) => {
   const engine = React.useContext(EngineContext);
   const mousePosition = React.useContext(MousePositionContext);
 
-  const copy = React.useCallback((nodeID) => engine.copyActive(nodeID), []);
+  const copy = React.useCallback((nodeID) => engine.copyActive(nodeID), [engine]);
 
   React.useEffect(() => {
-    const handlePaste = preventDefault((event) => {
+    const handlePaste = (event) => {
+      if (IGNORED_TAGS.includes(event.target.nodeName)) {
+        return;
+      }
+
+      event.preventDefault();
+
       const position = engine.canvas.transformPoint(mousePosition.current);
 
       engine.paste(event.clipboardData.getData('text'), position);
-    });
+    };
 
     document.addEventListener('paste', handlePaste);
 
     return () => document.removeEventListener('paste', handlePaste);
-  }, []);
+  }, [engine, mousePosition]);
 
   return <ClipboardContext.Provider value={{ copy }}>{children}</ClipboardContext.Provider>;
 };

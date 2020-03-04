2151e2c8118781fb70ea268aca49bb30b797cb1a
Ben Teichman <ben@voiceflow.com>
2020-03-04 15:08:34 -0500

    chore: add basic typescript duck example (#1329)

---
+++ src/client/diagram.ts
@@ -0,0 +1,42 @@
+import { PlatformType } from '@/constants';
+
+import creatorAdapter from './adapters/creator';
+import diagramAdapter from './adapters/diagram';
+import fetch, { NetworkError, StatusCode } from './fetch';
+
+const DIAGRAM_PATH = 'diagram';
+const VARIABLES_PATH = 'variables';
+
+const diagramClient = {
+  getData: (diagramID: string, platform: PlatformType, isBlockRedesignEnabled: boolean): Promise<unknown> =>
+    fetch(`${DIAGRAM_PATH}/${diagramID}`).then(({ diagram, timestamp = [] }) => ({
+      data: creatorAdapter.fromDB(JSON.parse(diagram.data), platform, isBlockRedesignEnabled),
+      variables: diagram.variables || [],
+      timestamp,
+    })),
+
+  get: (diagramID: string): Promise<unknown> => fetch(`${DIAGRAM_PATH}/${diagramID}`).then(({ diagram }) => diagramAdapter.fromDB(diagram)),
+
+  create: (diagram: unknown) => fetch.post(`${DIAGRAM_PATH}?new=1`, diagram),
+
+  copy: (diagramID: string, name: string) => fetch(`${DIAGRAM_PATH}/copy/${diagramID}?name=${encodeURI(name)}`),
+
+  delete: (diagramID: string) =>
+    fetch.delete(`${DIAGRAM_PATH}/${diagramID}`).catch((err) => {
+      if (err instanceof NetworkError && err.statusCode === StatusCode.FORBIDDEN) {
+        throw new NetworkError(err.statusCode, 'Flow has active users and cannot be deleted at the moment');
+      } else {
+        throw new NetworkError(err.statusCode || StatusCode.SERVER_ERROR, 'Error Deleting Flow');
+      }
+    }),
+
+  rename: (diagramID: string, name: string) => fetch.post(`${DIAGRAM_PATH}/${diagramID}/name`, { name }),
+
+  update: (diagram: unknown) => fetch.post(`${DIAGRAM_PATH}`, diagram),
+
+  findVariables: (diagramID: string): Promise<string[]> => fetch(`${DIAGRAM_PATH}/${diagramID}/${VARIABLES_PATH}`),
+
+  updateVariables: (diagramID: string, variables: string[]) => fetch.patch(`${DIAGRAM_PATH}/${diagramID}/${VARIABLES_PATH}`, { variables }),
+};
+
+export default diagramClient;

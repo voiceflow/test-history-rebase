b8415cb73fd2402442ce72c530433a385401b1ae
Ben Teichman <ben@voiceflow.com>
2020-03-31 15:53:02 -0400

    fix: correct left sidebar height (CORE-2909) (#1403)

--- src/pages/Canvas/components/LeftSidebar/components/Container.js
+++ src/pages/Canvas/components/LeftSidebar/components/Container.js
@@ -6,8 +6,8 @@ const Container = styled.aside`
 
   position: absolute;
   left: 0;
-  top: 60px;
-  bottom: 60px;
+  top: 50px;
+  bottom: 90px;
 
   transform: translateX(-${({ theme }) => theme.components.leftSidebar.contentWidth}px) translateZ(0);
   transition: transform 350ms cubic-bezier(0.075, 0.82, 0.165, 1);

8eace60312c03d3c62f1a48512276343ea64dbc6
Evgeny Zaytsev <overhawlin@gmail.com>
2020-04-07 21:47:07 +0300

    fix: improve the precision of linking to blocks (#1447)

--- src/pages/Canvas/engine/linkCreationEngine.ts
+++ src/pages/Canvas/engine/linkCreationEngine.ts
@@ -21,6 +21,8 @@ class LinkCreationEngine extends EngineConsumer {
 
   isCompleting = false;
 
+  unpinTimeout = 0;
+
   get isDrawing() {
     return !!this.sourcePortID;
   }
@@ -75,13 +77,19 @@ class LinkCreationEngine extends EngineConsumer {
   }
 
   pin(targetPortID: string, position: [number, number]) {
+    clearTimeout(this.unpinTimeout);
+
     this.activeTargetPortID = targetPortID;
     this.newLink?.pin(position);
   }
 
   unpin() {
-    this.activeTargetPortID = null;
-    this.newLink?.unpin();
+    clearTimeout(this.unpinTimeout);
+
+    this.unpinTimeout = setTimeout(() => {
+      this.activeTargetPortID = null;
+      this.newLink?.unpin();
+    }, 24);
   }
 
   getLinkPoints() {
@@ -94,6 +102,8 @@ class LinkCreationEngine extends EngineConsumer {
   }
 
   reset() {
+    clearTimeout(this.unpinTimeout);
+
     this.sourcePortID = null;
     this.activeTargetPortID = null;
     this.mouseOrigin = null;

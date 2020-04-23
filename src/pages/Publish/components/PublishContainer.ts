9b8884324d89695d67046966599b7bf8eea60c5a
Tyler Han <tylerhan97@gmail.com>
2020-04-23 16:33:52 -0400

    fix: publish page styling

---
+++ src/pages/Publish/components/PublishContainer.ts
@@ -0,0 +1,12 @@
+import { styled } from '@/hocs';
+
+const PublishContainer = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+`;
+
+export default PublishContainer;
---
+++ src/pages/Publish/components/PublishPlatformContainer.ts
@@ -0,0 +1,9 @@
+import { styled } from '@/hocs';
+
+const PublishPlatformContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+`;
+
+export default PublishPlatformContainer;

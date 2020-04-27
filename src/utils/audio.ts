f81ca67bc29ae978e8c2c388ec6fdeff00213c61
Tyler Han <tylerhan97@gmail.com>
2020-04-27 10:05:29 -0400

    fix: audio title null values

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -3,4 +3,4 @@ import { AUDIO_FILE_BUCKET_NAME } from '@/constants';
 const AUDIO_REGEX = new RegExp(`${AUDIO_FILE_BUCKET_NAME}/\\d+-`);
 
 // eslint-disable-next-line import/prefer-default-export
-export const getAudioTitle = (url = '') => url.replace(AUDIO_REGEX, '');
+export const getAudioTitle = (url = '') => url?.replace(AUDIO_REGEX, '');

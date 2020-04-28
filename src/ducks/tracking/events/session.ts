afd37f8e1dcf618b206e7aab518772c46862a5de
Ben Teichman <ben@voiceflow.com>
2020-04-28 15:13:11 -0400

    feat: identify user at the beginning of each session (CORE-3079) (#1534)

--- src/ducks/tracking/events/session.ts
+++ src/ducks/tracking/events/session.ts
@@ -2,7 +2,10 @@ import client from '@/client';
 
 import { EventName } from '../constants';
 
-export const trackSessionBegin = () => () => client.analytics.track(EventName.SESSION_BEGIN);
+export const trackSessionBegin = () => () => {
+  client.analytics.track(EventName.SESSION_BEGIN);
+  client.analytics.identify({});
+};
 
 export const trackSessionDuration = (duration: number) => () =>
   client.analytics.track(EventName.SESSION_DURATION, { properties: { duration: Math.floor(duration / 1000) } });

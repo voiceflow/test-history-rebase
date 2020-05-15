4dd50f589db76e1166eebdf62b6612bd48885d6d
Evgeny Zaytsev <overhawlin@gmail.com>
2020-05-15 16:52:39 +0300

    fix: prototype input autofocus, secondary button icon style (#1601)

--- src/components/Button/components/SecondaryButton/components/SecondaryButtonIcon.js
+++ src/components/Button/components/SecondaryButton/components/SecondaryButtonIcon.js
@@ -1,13 +1,15 @@
-import styled from 'styled-components';
+import { css, styled } from '@/hocs';
 
 const SecondaryButtonIcon = styled.span`
   height: 16px;
   width: 16px;
   color: rgba(110, 132, 154, 0.85);
 
-  &:not(:last-child) {
-    margin-right: 16px;
-  }
+  ${({ withoutChildren }) =>
+    !withoutChildren &&
+    css`
+      margin-right: 16px;
+    `}
 `;
 
 export default SecondaryButtonIcon;

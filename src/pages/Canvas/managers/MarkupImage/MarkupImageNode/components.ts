698bd2d6e5e7a38e5e3735aa20c1c5196612d3a3
Josh <31071869+JoshTheDeveloper@users.noreply.github.com>
2020-06-15 10:57:03 -0400

    fix: image hover outline (#1745)

--- src/pages/Canvas/managers/MarkupImage/MarkupImageNode/components.ts
+++ src/pages/Canvas/managers/MarkupImage/MarkupImageNode/components.ts
@@ -1,4 +1,5 @@
 import { styled } from '@/hocs';
+import { CANVAS_DRAGGING_CLASSNAME, CANVAS_MARKUP_ENABLED_CLASSNAME } from '@/pages/Canvas/constants';
 
 type ContainerProps = {
   url: string;
@@ -16,6 +17,13 @@ export const Container = styled.div.attrs<ContainerProps>(({ width, height }) =>
   background-position: center;
   background-image: ${({ url }) => `url(${url})`};
   z-index: -1;
+  border: solid 1px transparent;
+  background-repeat: no-repeat;
+
+  .${CANVAS_MARKUP_ENABLED_CLASSNAME}:not(.${CANVAS_DRAGGING_CLASSNAME}) &:hover {
+    border: solid 1px #5d9df5;
+  }
+
 `;
 
 export default Container;

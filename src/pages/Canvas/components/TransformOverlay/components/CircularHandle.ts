4236437a00867349b17d4e66bb54f3564324a14b
Evgeny Zaytsev <overhawlin@gmail.com>
2020-06-15 16:10:26 +0300

    fix: rotate toggle styles (#1746)

--- src/pages/Canvas/components/TransformOverlay/components/CircularHandle.ts
+++ src/pages/Canvas/components/TransformOverlay/components/CircularHandle.ts
@@ -7,7 +7,7 @@ const CircularHandle = styled.div`
   width: 7px;
   height: 7px;
   background: #fff;
-  z-index: 110;
+  z-index: 109;
   pointer-events: all;
   border-radius: 50%;
   box-shadow: 0 1px 3px 0 rgba(19, 33, 68, 0.08), 0 0 1px 1px rgba(17, 49, 96, 0.16);
@@ -16,6 +16,18 @@ const CircularHandle = styled.div`
 
   :active {
     cursor: grabbing;
+  }
+
+  &:before {
+    display: block;
+    position: absolute;
+    top: 7px;
+    left: 3px;
+    width: 1px;
+    height: 13px;
+    background-color: rgba(98, 119, 140, 0.5);
+
+    content: '';
   }
 `;
 

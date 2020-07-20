28d1395f60b029c05041c075e8c28ae51f6f52d5
Ben Teichman <ben@voiceflow.com>
2020-07-20 16:55:10 -0400

    test(e2e): set source map type for instrumented tests (#1887)

--- config/webpack/creator/build.ts
+++ config/webpack/creator/build.ts
@@ -1,5 +1,6 @@
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import merge from 'webpack-merge';
+import { instrument } from 'webpack-nano/argv';
 
 import commonBuildConfig from '../common/build';
 import { IS_SERVING } from '../config';
@@ -7,4 +8,8 @@ import commonConfig from './common';
 
 export default merge(commonConfig, commonBuildConfig, {
   plugins: IS_SERVING ? [] : [new BundleAnalyzerPlugin({ analyzerMode: 'static' })],
+
+  ...(instrument && {
+    devtool: 'source-map',
+  }),
 });

7a687d4ff06789ca88de841633d6b7d21b59f459
Josh <josh@voiceflow.com>
2020-08-04 23:50:48 -0400

    chore: removed demo code

--- src/pages/Onboarding/Steps/AddCollaborators/components/index.ts
+++ src/pages/Onboarding/Steps/AddCollaborators/components/index.ts
@@ -1,7 +1,6 @@
 export { default as Container } from './AddCollaboratorPageContainer';
 export { default as HeaderLabel } from './HeaderLabel';
 export { default as Text } from './HeaderText';
-export { default as BookDemo } from './BookDemo';
 export { default as AddTeamMember } from './AddTeamMember';
 export { default as DropdownInput } from './DropdownInput';
 export { default as InvalidEmailError } from './InvalidEmailError';

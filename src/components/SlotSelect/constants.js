d65a0fedd7025e016208e25e1ae827767c93df06
Jenna Choi <jena.choi1106@gmail.com>
2020-09-22 11:12:04 -0400

    fix: remove both platform icons in Capture Input (CORE-3982) (#2244)
    * fix: remove both platform icons in Capture Input (CORE-3982)
    
    * fix: delete unused files

--- src/components/SlotSelect/constants.js
+++
@@ -1,3 +0,0 @@
-export const ALEXA_SLOT_PATTERN = /AMAZON/;
-export const GOOGLE_SLOT_PATTERN = /^@sys\./;
-export const CUSTOM_SLOT_PLACEHOLDER = 'Custom';
--- src/components/SlotSelect/index.jsx
+++ src/components/SlotSelect/index.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
 
-import Flex, { FlexApart } from '@/components/Flex';
+import { FlexApart } from '@/components/Flex';
 import Select from '@/components/Select';
-import SvgIcon from '@/components/SvgIcon';
 import { connect } from '@/hocs';
 import { activeSlotTypes } from '@/store/selectors';
 
-import { ALEXA_SLOT_PATTERN, GOOGLE_SLOT_PATTERN } from './constants';
-
 const slotOptionRenderer = (option) => {
-  const isAlexa = option?.value.match(ALEXA_SLOT_PATTERN);
-  const isGoogle = option?.value.match(GOOGLE_SLOT_PATTERN);
-  const isGlobal = !isAlexa && !isGoogle;
-
-  return (
-    <FlexApart fullWidth>
-      {option.label}
-      <Flex>
-        {(isAlexa || isGlobal) && <SvgIcon icon="amazon" color="#BECEDC" />}
-        {(isGoogle || isGlobal) && <SvgIcon icon="google" color="#BECEDC" />}
-      </Flex>
-    </FlexApart>
-  );
+  return <FlexApart fullWidth>{option.label}</FlexApart>;
 };
 
 const SlotSelect = ({ value, onChange, className, slotTypes, filter, ...props }) => {

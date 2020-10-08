d8793ad150814eb8cbf2554afa02f1e21c046af0
Josh <31071869+JoshTheDeveloper@users.noreply.github.com>
2020-10-08 13:24:50 -0400

    feat: track hostname in workspace session being (CORE-4107) (#2343)

--- src/ducks/tracking/events/workspace.ts
+++ src/ducks/tracking/events/workspace.ts
@@ -2,12 +2,14 @@
 import client from '@/client';
 import { EventName } from '@/ducks/tracking/constants';
 import { Workspace } from '@/models';
+import { getHostName } from '@/utils/window';
 
 export const trackWorkspace = (workspace: Workspace) => () => {
   client.analytics.track(EventName.WORKSPACE_SESSION_BEGIN, {
     teamhashed: ['workspace_id'],
     properties: {
       workspace_id: workspace.id,
+      creator_version: getHostName(),
     },
   });
   client.analytics.identifyWorkspace(workspace);

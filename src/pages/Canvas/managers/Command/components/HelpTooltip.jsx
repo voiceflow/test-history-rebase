54ebe0a1262daf484568ddbc90fcdacae49a080b
Tyler Han <tyler@voiceflow.com>
2020-11-29 23:14:41 -0500

    chore: fix typo

--- src/pages/Canvas/managers/Command/components/HelpTooltip.jsx
+++ src/pages/Canvas/managers/Command/components/HelpTooltip.jsx
@@ -7,7 +7,7 @@ export default function HelpTooltip() {
     <>
       <Paragraph>
         Commands can be accessed by the user from anywhere in your project. For example, if a user says “Help”, they will activate the help flow.
-        Since the help intent is housed in a Command, once the user is done the flow they will retiurn to where they were previously in your project.
+        Since the help intent is housed in a Command, once the user is done the flow they will return to where they were previously in your project.
       </Paragraph>
       <Paragraph>We can think of Commands as a jump and return function inside our project</Paragraph>
     </>

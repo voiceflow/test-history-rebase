6052de28c34aac917c20be7d682805b505c8df25
Tyler Han <tylerhan97@gmail.com>
2020-11-30 18:52:36 -0500

    fix: invalid intent slots (#2650)

--- src/client/adapters/intent.ts
+++ src/client/adapters/intent.ts
@@ -2,22 +2,42 @@ import { Intent as DBIntent } from '@voiceflow/general-types';
 
 import { createAdapter } from '@/client/adapters/utils';
 import { PlatformType } from '@/constants';
-import { Intent } from '@/models';
+import { Intent, IntentInput, IntentSlot } from '@/models';
 import { denormalize, normalize } from '@/utils/normalized';
+
+const IntentInputSanitizer = ({ text, slots }: IntentInput): IntentInput => ({
+  text: text || '',
+  slots: slots || [],
+});
+
+const IntentSlotSanitizer = ({
+  id,
+  dialog: { prompt = [], confirm = [], utterances = [], confirmEnabled = false },
+  required = false,
+}: IntentSlot): IntentSlot => ({
+  id,
+  dialog: {
+    prompt: prompt.map(IntentInputSanitizer),
+    confirm: confirm.map(IntentInputSanitizer),
+    utterances: utterances.map(IntentInputSanitizer),
+    confirmEnabled,
+  },
+  required,
+});
 
 const intentAdapter = (platform: PlatformType) =>
   createAdapter<DBIntent, Intent>(
     ({ key, name, inputs, slots = [] }) => ({
       id: key,
       name,
-      slots: normalize(slots),
+      slots: normalize(slots.map(IntentSlotSanitizer)),
       inputs: inputs.map(({ text = '', slots = [] }) => ({ text, slots })),
       platform,
     }),
     ({ id, name, slots, inputs }) => ({
       key: id,
       name,
-      slots: denormalize(slots),
+      slots: denormalize(slots).map(IntentSlotSanitizer),
       inputs,
     })
   );

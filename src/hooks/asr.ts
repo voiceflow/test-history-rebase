98b8aa23d23d1b0e27470c11a83c21a21af7ce9e
Josh <31071869+JoshTheDeveloper@users.noreply.github.com>
2021-02-23 13:39:13 -0500

    fix:update asr conditional for desktop app (CORE-5296) (#3035)

--- src/hooks/asr.ts
+++ src/hooks/asr.ts
@@ -2,11 +2,14 @@ import { isChrome, isMobile, isTablet } from '@/config';
 import { FeatureFlag } from '@/config/features';
 import { useFeature } from '@/hooks/feature';
 
+const DESKTOP_APP_USERAGENT = 'Chrome Desktop App';
+
 // eslint-disable-next-line import/prefer-default-export
 export const useCanASR = () => {
   const googleASR = useFeature(FeatureFlag.GOOGLE_STT);
   const asrBypass = useFeature(FeatureFlag.ASR_BYPASS);
-  const canUseASR = (!isMobile && !isTablet && !isChrome && googleASR.isEnabled) || asrBypass.isEnabled;
+  const isDesktopApp = window.navigator.userAgent === DESKTOP_APP_USERAGENT;
+  const canUseASR = (!isMobile && !isTablet && !isChrome && googleASR.isEnabled) || asrBypass.isEnabled || (googleASR.isEnabled && isDesktopApp);
 
   return [canUseASR];
 };

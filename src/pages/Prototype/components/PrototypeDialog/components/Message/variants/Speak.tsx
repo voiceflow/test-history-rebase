2fa47c3a29b39e2365ba60530e98f528807a7d0d
Josh <31071869+JoshTheDeveloper@users.noreply.github.com>
2021-02-23 15:21:34 -0500

    fix: remove voice name in proto (CORE-5168) (#3043)

--- src/pages/Prototype/components/PrototypeDialog/components/Message/variants/Speak.tsx
+++ src/pages/Prototype/components/PrototypeDialog/components/Message/variants/Speak.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 
-import { prettifyVoice } from '@/components/SSML/utils';
 import { SSML_TAG_REGEX } from '@/constants';
-import { styled } from '@/hocs';
 
 import { Message } from '../components';
 import { MessageProps } from '../components/Message';
-
-const Voice = styled.div`
-  width: 100%;
-  color: #62778c;
-`;
 
 type SpeakProps = Omit<MessageProps, 'iconProps'> & {
   voice?: string;
@@ -19,12 +12,7 @@ type SpeakProps = Omit<MessageProps, 'iconProps'> & {
 
 const Speak: React.FC<SpeakProps> = ({ voice, message, ...props }) => {
   const noSSMLMessage = message.replace(SSML_TAG_REGEX, '');
-  return noSSMLMessage ? (
-    <Message {...props}>
-      {voice && <Voice>{prettifyVoice(voice)}</Voice>}
-      {noSSMLMessage}
-    </Message>
-  ) : null;
+  return noSSMLMessage ? <Message {...props}>{noSSMLMessage}</Message> : null;
 };
 
 export default Speak;

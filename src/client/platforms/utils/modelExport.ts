6a28f5a005a43afbbdd85b26ba0ed0814396e086
Justin Goping <32006038+jgoping@users.noreply.github.com>
2021-02-23 18:32:15 -0500

    feat: Remove model wrapper from model export JSONs (CORE-5303) (#3046)
    * feat: remove model wrapper from model export JSONs
    
    * chore: avoid parsing JSON

--- src/client/platforms/utils/modelExport.ts
+++ src/client/platforms/utils/modelExport.ts
@@ -1,10 +1,10 @@
 import { Fetch } from '@/client/fetch';
-import { MessageFormat } from '@/client/fetch/types';
 
 export const RESOURCE_ENDPOINT = 'model-export';
 
 const createModelExportService = (client: Fetch) => ({
-  export: (versionID: string, type: string) => client.get<string>(`${RESOURCE_ENDPOINT}/${type}/${versionID}`, { returns: MessageFormat.TEXT }),
+  export: (versionID: string, type: string) =>
+    client.get<Record<string, unknown>>(`${RESOURCE_ENDPOINT}/${type}/${versionID}`).then(({ model }) => JSON.stringify(model, null, 2)),
 });
 
 export default createModelExportService;

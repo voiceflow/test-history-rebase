355c554a18f82050a7491a1197653f8c556696a8
Tyler Han <tyler@voiceflow.com>
2021-03-12 00:16:09 -0500

    fix: use params for tracking (CORE-000)

--- src/client/platforms/utils/project.ts
+++ src/client/platforms/utils/project.ts
@@ -4,10 +4,8 @@ import axios from 'axios';
 export const RESOURCE_ENDPOINT = 'project';
 
 const createProjectService = <P extends Project<any, any>>(serviceEndpoint: string) => ({
-  copy: (projectID: string, data?: Partial<P>, extra?: { channel?: string }) =>
-    axios
-      .post<P>(`${serviceEndpoint}/${RESOURCE_ENDPOINT}/${projectID}/copy`, { ...data, ...extra })
-      .then((res) => res.data),
+  copy: (projectID: string, data?: Partial<P>, params?: { channel?: string }) =>
+    axios.post<P>(`${serviceEndpoint}/${RESOURCE_ENDPOINT}/${projectID}/copy`, { ...data }, { params }).then((res) => res.data),
 });
 
 export default createProjectService;

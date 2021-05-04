2be0221a474fc284404e0341e72a8e70f1a576c4
(Frank) Yu Cheng Gu <frank@voiceflow.com>
2021-05-04 09:03:35 -0700

    fix: unify nginx configs on e2e and prod (VF-840) (#3309)
    * refactor: move locations to separate file
    
    * ci: fix e2e start script
    
    * ci: run nginx in background
    
    * chore: prototype endpoint iframe
    
    * chore: test iframe policy
    
    * chore: fix headers
    
    * chore: fix headers
    
    * ci: fix build process
    
    * chore: empty commit
    
    * refactor: move doc duplication into webpack process
    
    Co-authored-by: Tyler Han <tyler@voiceflow.com>
    Co-authored-by: Ben Teichman <ben@voiceflow.com>

--- config/webpack/creator/build.ts
+++ config/webpack/creator/build.ts
@@ -1,23 +1,38 @@
+import FileManagerPlugin from 'filemanager-webpack-plugin';
 import path from 'path';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import merge from 'webpack-merge';
 import { analyze } from 'webpack-nano/argv';
 
+import paths from '../../paths';
 // import { instrument } from 'webpack-nano/argv';
 import commonBuildConfig from '../common/build';
 import { IS_SERVING } from '../config';
 import commonConfig from './common';
 
 export default merge(commonConfig, commonBuildConfig, {
-  plugins:
-    IS_SERVING || !analyze
+  plugins: [
+    new FileManagerPlugin({
+      events: {
+        onEnd: {
+          copy: [
+            {
+              source: `${paths.buildDir}/index.html`,
+              destination: `${paths.buildDir}/prototype.html`,
+            },
+          ],
+        },
+      },
+    }),
+    ...(IS_SERVING || !analyze
       ? []
       : [
           new BundleAnalyzerPlugin({
             analyzerMode: 'static',
             reportFilename: path.resolve(__dirname, '../../../bundle-report.html'),
           }),
-        ],
+        ]),
+  ],
 
   // ...(instrument && {
   //   devtool: 'source-map',

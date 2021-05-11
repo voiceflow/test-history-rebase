217a7766755782ff8a50c24b3a30068a3f6fc7ee
Hanlin Cheng <h62cheng@uwaterloo.ca>
2021-05-10 23:06:14 -0400

    fix(ui): allow full height for dropzone image preview (VF-851) (#3316)

--- src/components/Upload/ImageUpload/FullImage/components/Image.js
+++ src/components/Upload/ImageUpload/FullImage/components/Image.js
@@ -1,20 +1,22 @@
 import { css, styled } from '@/hocs';
 
+const calculateRatio = (ratio) =>
+  ratio
+    ? css`
+        padding-bottom: ${ratio}%;
+      `
+    : css`
+        height: 100%;
+      `;
+
 const Image = styled.div`
   width: 100%;
   border-radius: 5px;
   background-size: cover;
   background-position: top center;
-
   min-height: 30px;
-
   background-image: url(${({ src }) => src});
 
-  ${({ ratio }) =>
-    ratio &&
-    css`
-      padding-bottom: ${ratio}%;
-    `};
+  ${({ ratio }) => calculateRatio(ratio)}
 `;
-
 export default Image;

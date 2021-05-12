49b08ace6fb49086478bf547023cd2e4cb184e04
Evgeny Zaytsev <overhawlin@gmail.com>
2021-05-12 10:40:36 +0300

    fix: line breakpoints position on canvas export page (VF-854) (#3314)

--- src/pages/Export/index.tsx
+++ src/pages/Export/index.tsx
@@ -9,7 +9,7 @@ import * as Workspace from '@/ducks/workspace';
 import { ProjectLoadingGate, WorkspaceFeatureLoadingGate, WorkspaceLoadingGate } from '@/gates';
 import { connect, withBatchLoadingGate } from '@/hocs';
 import removeIntercom from '@/hocs/removeIntercom';
-import { Node } from '@/models';
+import { Link, LinkData, Node, Port } from '@/models';
 import LinkLayer from '@/pages/Canvas/components/LinkLayer';
 import MarkupLayer from '@/pages/Canvas/components/MarkupLayer';
 import NodeLayer from '@/pages/Canvas/components/NodeLayer';
@@ -55,23 +55,50 @@ const ExportCanvas: React.FC<{ isOnPaidPlan: boolean; diagramID: string; initial
   );
 };
 
-const findCanvasExportOffsets = (nodes: Node[]): Point => {
+const findCanvasExportOffsets = (nodes: Node[], ports: Port[]): Point => {
   const rootNodes = nodes.filter(({ type }) => isRootOrMarkupBlockType(type));
 
   const xValues = rootNodes.map((node) => node.x);
   const yValues = rootNodes.map((node) => node.y);
+  const portXValues = ports.flatMap((port) => port.linkData!.points!.map((point) => point.point[0]));
+  const portYValues = ports.flatMap((port) => port.linkData!.points!.map((point) => point.point[1]));
 
-  const minX = Math.min(...xValues);
-  const minY = Math.min(...yValues);
+  const minX = Math.min(...xValues, ...portXValues);
+  const minY = Math.min(...yValues, ...portYValues);
 
   return [0 - minX, 0 - minY];
 };
 
-const applyCanvasExportOffsets = (nodes: Node[], [offsetX, offsetY]: Point) =>
+const applyOffsetsToNodes = (nodes: Node[], [offsetX, offsetY]: Point) =>
   nodes.map((node) => ({
     ...node,
     x: node.x + offsetX + (isMarkupBlockType(node.type) ? BLOCK_WIDTH / 2 : 0),
     y: node.y + offsetY,
+  }));
+
+const applyOffsetsToLinkData = (data: LinkData | undefined, [offsetX, offsetY]: Point): LinkData | undefined =>
+  !data
+    ? undefined
+    : {
+        ...data,
+        points: !data.points
+          ? undefined
+          : data.points.map((point) => ({
+              ...point,
+              point: [point.point[0] + offsetX + BLOCK_WIDTH / 2, point.point[1] + offsetY],
+            })),
+      };
+
+const applyOffsetsToPorts = (ports: Port[], offsets: Point) =>
+  ports.map((port) => ({
+    ...port,
+    linkData: applyOffsetsToLinkData(port.linkData, offsets),
+  }));
+
+const applyOffsetsToLinks = (links: Link[], offsets: Point) =>
+  links.map((link) => ({
+    ...link,
+    data: applyOffsetsToLinkData(link.data, offsets),
   }));
 
 const initialize = (diagramID: string) => async (dispatch: any, getState: any) => {
@@ -81,9 +108,12 @@ const initialize = (diagramID: string) => async (dispatch: any, getState: any) =
   const { viewport, ...creator } = creatorAdapter.fromDB(await client.api.diagram.get(diagramID), { platform });
 
   const nodesWithCoordinates = creator.nodes.filter((node) => _isNumber(node.x) && _isNumber(node.y));
-  const offsets = findCanvasExportOffsets(nodesWithCoordinates);
+  const portsWithPoints = creator.ports.filter((port) => !!port.nodeID && !!port.linkData?.points?.length);
+  const offsets = findCanvasExportOffsets(nodesWithCoordinates, portsWithPoints);
 
-  creator.nodes = applyCanvasExportOffsets(nodesWithCoordinates, offsets);
+  creator.nodes = applyOffsetsToNodes(nodesWithCoordinates, offsets);
+  creator.ports = applyOffsetsToPorts(creator.ports, offsets);
+  creator.links = applyOffsetsToLinks(creator.links, offsets);
 
   dispatch(Creator.initializeCreator({ ...creator, diagramID }));
 };

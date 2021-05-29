3b7bfc07b0bc742702b30335a993dd1d46838d07
Hanlin Cheng <h62cheng@uwaterloo.ca>
2021-05-29 11:54:15 -0400

    style: update test setting font size to 13px (VF-906) (#3440)
    * style: set body font to 13px (VF-906)
    
    * style: revert and add inline instead (VF-906)

--- src/pages/Prototype/components/PrototypeSettings/components/SettingsSection.tsx
+++ src/pages/Prototype/components/PrototypeSettings/components/SettingsSection.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import Box from '@/components/Box';
 import { SectionProps, UncontrolledSection } from '@/components/Section';
 import Toggle from '@/components/Toggle';
+import THEME from '@/styles/theme';
 
 const SettingsSection: React.FC<{ toggle: () => void; value: boolean } & SectionProps> = ({ value, toggle, children, ...props }) => (
   <UncontrolledSection onClick={toggle} suffix={<Toggle small checked={value} onChange={toggle} />} {...props}>
-    <Box color="#62778c" mb={20} mt={-6}>
+    <Box color={THEME.colors.secondary} fontSize="13px" mb={20} mt={-6}>
       {children}
     </Box>
   </UncontrolledSection>

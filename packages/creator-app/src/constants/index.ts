34ec0787b83cc47a54f6a8af9a00722f2f39c29a
Jonah Snider <jonah.snider@voiceflow.com>
2021-07-12 13:18:43 -0700

    feat: request `firebase` scope in Google OAuth connect (VF-873) (#3656)

--- packages/creator-app/src/constants/index.ts
+++ packages/creator-app/src/constants/index.ts
@@ -8,7 +8,8 @@ export { IntegrationType } from '@voiceflow/general-types';
 export const USER_INFO_SCOPE = 'https://www.googleapis.com/auth/userinfo.profile';
 export const ACTION_BUILDER_SCOPE = 'https://www.googleapis.com/auth/actions.builder';
 export const CLOUD_RESOURCE_SCOPE = 'https://www.googleapis.com/auth/cloudplatformprojects.readonly';
-export const GOOGLE_OAUTH_SCOPES = [CLOUD_RESOURCE_SCOPE, ACTION_BUILDER_SCOPE, USER_INFO_SCOPE];
+export const FIREBASE_SCOPE = 'https://www.googleapis.com/auth/firebase';
+export const GOOGLE_OAUTH_SCOPES = [CLOUD_RESOURCE_SCOPE, ACTION_BUILDER_SCOPE, USER_INFO_SCOPE, FIREBASE_SCOPE];
 
 export const CUSTOM_SLOT_TYPE = 'Custom';
 export const LEGACY_CUSTOM_SLOT_TYPE = 'CUSTOM';

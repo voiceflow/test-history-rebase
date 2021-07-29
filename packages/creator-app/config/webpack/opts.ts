ef5d6dd48a294079bedcfd3d5c17aa26a441f2b4
Matheus Poleza <matheus.poleza@gmail.com>
2021-07-29 16:19:37 -0300

    chore: updates webpack local url adding https instead of http (VF-000) (#3787)
    Co-authored-by: Matheus Poleza <matheuspoleza@Matheuss-MacBook-Pro.local>

--- packages/creator-app/config/webpack/opts.ts
+++ packages/creator-app/config/webpack/opts.ts
@@ -28,7 +28,7 @@ const opts: Options = {
 
   serve: {
     port: 3000,
-    publicURL: 'creator-local.development.voiceflow.com:3002',
+    publicURL: 'https://creator-local.development.voiceflow.com:3002',
 
     ...(E2E && {
       port: 3002,

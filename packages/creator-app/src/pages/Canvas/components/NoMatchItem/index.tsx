294d76823cc7f78e63b9a27a97dd90d251265a19
Hanlin Cheng <h62cheng@uwaterloo.ca>
2021-08-16 16:36:42 -0400

    fix: use numbered reprompts instead of words (VF-1443) (#3914)

--- packages/creator-app/src/pages/Canvas/components/NoMatchItem/index.tsx
+++ packages/creator-app/src/pages/Canvas/components/NoMatchItem/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
 import SpeakAudioItem, { SpeakAudioItemProps } from '@/pages/Canvas/components/SpeakAudioItem';
-import { convertToWord } from '@/utils/number';
-import { capitalizeFirstLetter } from '@/utils/string';
 
 export type NoMatchItemProps = Omit<SpeakAudioItemProps, 'header' | 'formControlProps'>;
 
 const NoMatchItem: React.ForwardRefRenderFunction<HTMLElement, NoMatchItemProps> = (props, ref) => {
-  const number = capitalizeFirstLetter(convertToWord(props.index + 1));
-
-  return <SpeakAudioItem {...props} header={`Reprompt ${number}`} ref={ref} formControlProps={{ contentBottomUnits: 2.5 }} />;
+  return <SpeakAudioItem {...props} header={`Reprompt ${props.index + 1}`} ref={ref} formControlProps={{ contentBottomUnits: 2.5 }} />;
 };
 
 export default React.forwardRef<HTMLElement, NoMatchItemProps>(NoMatchItem);

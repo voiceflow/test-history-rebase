ee0581ff6d27b2ead0da9de72a4a8310174cb67c
Josh <31071869+JoshTheDeveloper@users.noreply.github.com>
2021-08-19 17:16:43 -0400

    fix: use substring check (VF-1569) (#3961)

--- packages/creator-app/src/hooks/asr.ts
+++ packages/creator-app/src/hooks/asr.ts
@@ -8,7 +8,7 @@ const DESKTOP_APP_USERAGENT = 'Chrome Desktop App';
 // eslint-disable-next-line import/prefer-default-export
 export const useCanASR = () => {
   const asrBypass = useFeature(FeatureFlag.ASR_BYPASS);
-  const isDesktopApp = window.navigator.userAgent === DESKTOP_APP_USERAGENT;
+  const isDesktopApp = window.navigator.userAgent.includes(DESKTOP_APP_USERAGENT);
   const canUseASR = !IS_CHROME || asrBypass.isEnabled || isDesktopApp || (IS_CHROME && (IS_MOBILE || IS_TABLET));
 
   return [canUseASR];

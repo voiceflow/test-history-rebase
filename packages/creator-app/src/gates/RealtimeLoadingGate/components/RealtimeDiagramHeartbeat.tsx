9827c4550744be2a0215c9a02749aec68e15cf4d
Josh <31071869+JoshTheDeveloper@users.noreply.github.com>
2021-09-03 08:53:53 -0400

    fix: dont use setInterval for heartbeat (VF-993) (#4036)

--- packages/creator-app/src/gates/RealtimeLoadingGate/components/RealtimeDiagramHeartbeat.tsx
+++ packages/creator-app/src/gates/RealtimeLoadingGate/components/RealtimeDiagramHeartbeat.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 import client from '@/client';
-import { useInterval } from '@/hooks';
 
 const HEARTBEAT_TIMEOUT = 1000;
 
 const RealtimeDiagramHeartbeat: React.FC = () => {
-  useInterval(() => client.socket.diagram.sendHeartbeat(), HEARTBEAT_TIMEOUT);
+  const [timeoutCounter, setTimeoutCounter] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setTimeout(() => {
+      client.socket.diagram.sendHeartbeat();
+      setTimeoutCounter(timeoutCounter + 1);
+    }, HEARTBEAT_TIMEOUT);
+    return () => clearTimeout(interval);
+  }, [timeoutCounter]);
 
   return null;
 };

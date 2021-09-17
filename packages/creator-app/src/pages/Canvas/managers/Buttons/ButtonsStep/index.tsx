afae3014011200e570443cbe951392b23b403732
Josh <31071869+JoshTheDeveloper@users.noreply.github.com>
2021-09-17 11:07:48 -0400

    fix: button label fallback (VF-1767) (#4139)

--- packages/creator-app/src/pages/Canvas/managers/Buttons/ButtonsStep/index.tsx
+++ packages/creator-app/src/pages/Canvas/managers/Buttons/ButtonsStep/index.tsx
@@ -1,9 +1,14 @@
 import { Node } from '@voiceflow/base-types';
+import { PlatformType } from '@voiceflow/internal';
+import { Nullable } from '@voiceflow/ui';
 import React from 'react';
 
 import { StepLabelVariant } from '@/constants/canvas';
-import { NodeData } from '@/models';
+import { applySingleIntentNameFormatting } from '@/ducks/intent/utils';
+import { Intent, NodeData } from '@/models';
 import Step, { ConnectedStepProps, ElseItem, Item, Section } from '@/pages/Canvas/components/Step';
+import { CustomIntentMapContext } from '@/pages/Canvas/contexts';
+import { PlatformContext } from '@/pages/Skill/contexts';
 
 import { NODE_CONFIG } from '../constants';
 
@@ -15,25 +20,36 @@ export interface ButtonsStepProps {
   elsePathName: string;
 }
 
+const getIntentName = (intentMap: Record<string, Intent>, actions: string[], platform: PlatformType, intent: Nullable<string> | undefined) => {
+  const intentEnabled = actions.includes(Node.Buttons.ButtonAction.INTENT);
+  const intentName = intent && intentEnabled && applySingleIntentNameFormatting(platform, intentMap[intent])?.name;
+  return intentName || '';
+};
+
 export const ButtonsStep: React.FC<ButtonsStepProps> = ({ ports, nodeID, buttons, elsePathName, withElsePath }) => {
+  const platform = React.useContext(PlatformContext)!;
+  const intentsMap = React.useContext(CustomIntentMapContext)!;
+
   return (
     <Step nodeID={nodeID}>
       <Section>
         {buttons.length ? (
-          buttons.map(({ id, name, actions }, index) => (
-            <Item
-              key={id}
-              icon={index === 0 ? NODE_CONFIG.icon : null}
-              label={name}
-              portID={actions.includes(Node.Buttons.ButtonAction.PATH) ? ports[index + 1] : null}
-              iconColor={NODE_CONFIG.iconColor}
-              placeholder="Add button text"
-              withNewLines
-              labelVariant={StepLabelVariant.PRIMARY}
-              multilineLabel
-              labelLineClamp={100}
-            />
-          ))
+          buttons.map(({ id, name, actions, intent }, index) => {
+            return (
+              <Item
+                key={id}
+                icon={index === 0 ? NODE_CONFIG.icon : null}
+                label={name || getIntentName(intentsMap, actions, platform, intent)}
+                portID={actions.includes(Node.Buttons.ButtonAction.PATH) ? ports[index + 1] : null}
+                iconColor={NODE_CONFIG.iconColor}
+                placeholder="Add button text"
+                withNewLines
+                labelVariant={StepLabelVariant.PRIMARY}
+                multilineLabel
+                labelLineClamp={100}
+              />
+            );
+          })
         ) : (
           <Item placeholder="Add buttons" icon={NODE_CONFIG.icon} iconColor={NODE_CONFIG.iconColor} />
         )}

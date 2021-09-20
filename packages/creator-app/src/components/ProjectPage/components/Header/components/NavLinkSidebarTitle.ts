0ce8cc643bf7bb12c767b7c5f29a2e73aa40560b
Kerry Liu <kerry.liu@voiceflow.com>
2021-09-20 12:30:32 -0400

    feat: update section nav dimensions (#4138)

--- packages/creator-app/src/components/ProjectPage/components/Header/components/NavLinkSidebarTitle.ts
+++ packages/creator-app/src/components/ProjectPage/components/Header/components/NavLinkSidebarTitle.ts
@@ -3,6 +3,7 @@ import { styled } from '@/hocs';
 import Title from './Title';
 
 const NavLinkSidebarTitle = styled(Title)`
+  height: 65px;
   width: ${({ theme }) => theme.components.navLinkSidebar.width - 18}px;
   border-right: 1px solid ${({ theme }) => theme.colors.borders};
   padding-left: 14px;
--- packages/creator-app/src/components/ProjectPage/components/Header/components/Title.ts
+++ packages/creator-app/src/components/ProjectPage/components/Header/components/Title.ts
@@ -3,7 +3,7 @@ import { BoxFlex } from '@voiceflow/ui';
 import { styled } from '@/hocs';
 
 const Title = styled(BoxFlex)<{ leftOffset?: boolean }>`
-  height: 100%;
+  height: 65px;
   font-weight: 600;
   color: #132144;
   padding-left: ${({ leftOffset }) => (leftOffset ? 32 : 0)}px;

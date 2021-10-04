346ca399677699f259d54343406af78c33238b4f
Matheus Poleza <matheus.poleza@gmail.com>
2021-10-04 15:08:45 -0300

    fix: fixes model export endpoint (VF-1915) (#4263)
    * fix: fixes model export endpoint (VF-1915)
    
    * fix: separate post body and options from model export and changed it to POST (VF-1915)
    
    Co-authored-by: Matheus Poleza <matheus.poleza@voiceflow.com>

--- packages/creator-app/src/client/platforms/utils/modelExport.ts
+++ packages/creator-app/src/client/platforms/utils/modelExport.ts
@@ -5,17 +5,12 @@ import { Fetch } from '@/client/fetch';
 export const RESOURCE_ENDPOINT = 'model-export';
 
 const createModelExportService = (client: Fetch) => ({
-  exportBlob: (versionID: string, type: string, intents?: string[]) =>
-    client
-      .post<string>(
-        `${RESOURCE_ENDPOINT}/${type}/${versionID}`,
-        intents && intents.length > 0 ? { returns: FetchMessageFormat.BLOB, intents } : { returns: FetchMessageFormat.BLOB }
-      )
-      .then(async (data) => {
-        const binaryData = [];
-        binaryData.push(data);
-        return URL.createObjectURL(new Blob(binaryData, { type: 'application/zip' }));
-      }),
+  exportBlob: (versionID: string, type: string, intents?: string[]) => {
+    const exportEndpoint = `${RESOURCE_ENDPOINT}/${type}/${versionID}`;
+    const exportBody = intents && intents.length > 0 ? { intents } : {};
+
+    return client.post<string>(exportEndpoint, exportBody, { returns: FetchMessageFormat.BLOB }).then(async (blob) => URL.createObjectURL(blob));
+  },
 });
 
 export default createModelExportService;

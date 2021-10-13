9a5ee07700f818bd81677bdfdbbe3407b6a76760
Kerry Liu <kerry.liu@voiceflow.com>
2021-10-13 14:55:54 -0400

    fix: update dialogflow icon (#4301)

--- packages/creator-app/src/pages/Skill/components/Header/components/CanvasHeader/components/Upload/components/Button.tsx
+++ packages/creator-app/src/pages/Skill/components/Header/components/CanvasHeader/components/Upload/components/Button.tsx
@@ -14,8 +14,8 @@ const getPlatformIconProps = createPlatformSelector<HeaderIconButtonProps>(
   {
     [Constants.PlatformType.ALEXA]: { icon: 'amazonAlexa', iconProps: { color: '#5fcaf4' } },
     [Constants.PlatformType.GOOGLE]: { icon: 'googleAssistant' },
-    [Constants.PlatformType.DIALOGFLOW_ES_CHAT]: { icon: 'dialogflow' },
-    [Constants.PlatformType.DIALOGFLOW_ES_VOICE]: { icon: 'dialogflow' },
+    [Constants.PlatformType.DIALOGFLOW_ES_CHAT]: { icon: 'dialogflow', iconProps: { height: '18px', width: '18px' } },
+    [Constants.PlatformType.DIALOGFLOW_ES_VOICE]: { icon: 'dialogflow', iconProps: { height: '18px', width: '18px' } },
   },
   { icon: 'ban' }
 );

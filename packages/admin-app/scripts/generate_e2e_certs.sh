04889a84dc20d7b2c2f5e8a2dacefed0a81c80cb
xavidop <xavi_tb@hotmail.com>
2021-10-18 16:25:58 +0200

    ci: fix for the local e2e development

--- packages/admin-app/scripts/generate_e2e_certs.sh
+++ packages/admin-app/scripts/generate_e2e_certs.sh
@@ -4,8 +4,14 @@ mkcert -install
 
 rm -rf certs/
 mkdir -p certs/ && cd certs/
-mkcert localhost 127.0.0.1 ::1 $(echo "$(hostname)" | tr '[:upper:]' '[:lower:]') $(hostname) $LAN_IP 192.168.18.1 0.0.0.0
+LAN_IP=$(ifconfig en0 | grep "inet " | awk '{print $2}' | sed 's/addr://');
+echo "LAN IP: $LAN_IP"
+mkcert localhost 127.0.0.1 ::1 $(echo "$(hostname)" | tr '[:upper:]' '[:lower:]') $(hostname) $LAN_IP admin-local.development.voiceflow.com creator-local.development.voiceflow.com *.test.e2e;
 
 # Rename certificate and key
 mv *-key.pem localhost.key
 mv *.pem localhost.crt
+
+# Merge into a pem
+cp localhost.key server.test.pem
+cat localhost.crt >> server.test.pem

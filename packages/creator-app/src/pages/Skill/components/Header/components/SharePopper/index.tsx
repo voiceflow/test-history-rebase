ec48cfc0276bb1a851ea52f1f6e10e174a09429b
Josh <31071869+JoshTheDeveloper@users.noreply.github.com>
2021-10-25 11:20:05 -0400

    feat: persisted share tab session (VF-2038) (#4360)

--- packages/creator-app/src/pages/Skill/components/Header/components/SharePopper/index.tsx
+++ packages/creator-app/src/pages/Skill/components/Header/components/SharePopper/index.tsx
@@ -3,7 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 
 import Popper, { PopperContent, PopperFooter, PopperNav, PopperNavItem, PopperProps } from '@/components/Popper';
 import { Permission } from '@/config/permissions';
-import { usePermission } from '@/hooks';
+import * as Session from '@/ducks/session';
+import { usePermission, useSelector, useSessionStorageState } from '@/hooks';
 import { MenuContent as ShareContent, SharePrototype as ShareFooter } from '@/pages/Canvas/header/ActionGroup/components/ShareProject/components';
 import InviteContent from '@/pages/Collaborators';
 import InviteFooter from '@/pages/Collaborators/components/InviteByLink';
@@ -16,16 +17,41 @@ import { ExportProvider } from './contexts';
 interface SharePopperProps {
   children: PopperProps['children'];
 }
+const PERSISTED_SESSION_SHARE_TAB = 'persisted_session_share_tab';
 
 const SharePopper: React.FC<SharePopperProps> = ({ children }) => {
   const sharePopper = React.useContext(SharePopperContext);
+  const activeProjectID = useSelector(Session.activeProjectIDSelector);
 
   const [canSharePrototype] = usePermission(Permission.SHARE_PROTOTYPE);
   const [canAddCollaborators] = usePermission(Permission.ADD_COLLABORATORS);
 
-  const initialTab =
-    sharePopper?.data?.defaultTab ??
-    ((canAddCollaborators && ShareProjectTab.INVITE) || (canSharePrototype && ShareProjectTab.SHARE) || ShareProjectTab.EXPORT);
+  const initialTab = (canAddCollaborators && ShareProjectTab.INVITE) || (canSharePrototype && ShareProjectTab.SHARE) || ShareProjectTab.EXPORT;
+  const [persistedTab, setPersistedTab] = useSessionStorageState(`${PERSISTED_SESSION_SHARE_TAB}-${activeProjectID}`, initialTab);
+
+  React.useEffect(() => {
+    if (!sharePopper?.opened) {
+      return;
+    }
+
+    // In the case where the persisted tab is a feature they don't have anymore
+    const hasAccessToPersistedTab = (tab: ShareProjectTab) => {
+      switch (tab) {
+        case ShareProjectTab.SHARE:
+          return canSharePrototype;
+        case ShareProjectTab.INVITE:
+          return canAddCollaborators;
+        default:
+          return true;
+      }
+    };
+
+    if (sharePopper?.data?.defaultTab && hasAccessToPersistedTab(sharePopper.data.defaultTab)) {
+      setPersistedTab(sharePopper.data.defaultTab);
+    } else if (!hasAccessToPersistedTab(persistedTab)) {
+      setPersistedTab(initialTab);
+    }
+  }, [sharePopper?.opened]);
 
   return (
     <ExportProvider>
@@ -34,15 +60,25 @@ const SharePopper: React.FC<SharePopperProps> = ({ children }) => {
         height="510px"
         opened={sharePopper?.opened}
         onClose={sharePopper?.close}
-        initialTab={initialTab}
+        initialTab={sharePopper?.data?.defaultTab ?? persistedTab}
         preventOverflowPadding={24}
         renderNav={() => (
           <PopperNav>
-            {canAddCollaborators && <PopperNavItem to={ShareProjectTab.INVITE}>Invite collaborators</PopperNavItem>}
+            {canAddCollaborators && (
+              <PopperNavItem onClick={() => setPersistedTab(ShareProjectTab.INVITE)} to={ShareProjectTab.INVITE}>
+                Invite collaborators
+              </PopperNavItem>
+            )}
 
-            {canSharePrototype && <PopperNavItem to={ShareProjectTab.SHARE}>Share prototype</PopperNavItem>}
+            {canSharePrototype && (
+              <PopperNavItem onClick={() => setPersistedTab(ShareProjectTab.SHARE)} to={ShareProjectTab.SHARE}>
+                Share prototype
+              </PopperNavItem>
+            )}
 
-            <PopperNavItem to={ShareProjectTab.EXPORT}>Export as…</PopperNavItem>
+            <PopperNavItem onClick={() => setPersistedTab(ShareProjectTab.EXPORT)} to={ShareProjectTab.EXPORT}>
+              Export as…
+            </PopperNavItem>
           </PopperNav>
         )}
         renderContent={() => (

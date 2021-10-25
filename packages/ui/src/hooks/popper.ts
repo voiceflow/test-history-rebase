0219febf846854f38a981c8832f25f7d83e93c39
Evgeny Zaytsev <overhawlin@gmail.com>
2021-10-25 15:45:27 +0200

    fix: use popper types (VF-000) (#4362)

--- packages/ui/src/hooks/popper.ts
+++ packages/ui/src/hooks/popper.ts
@@ -1,41 +1,61 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import type { VirtualElement } from '@popperjs/core';
-import { usePopper as usePopperBase } from 'newpopper';
+import type * as PopperJS from '@popperjs/core';
+import { Nullable } from '@voiceflow/common';
+import { Modifier, usePopper as usePopperBase } from 'newpopper';
 import { useState } from 'react';
 
-import { Nullable } from '../types';
-
-type PopperParameters = Parameters<typeof usePopperBase>;
-type TriggerElement = PopperParameters[0];
-type PopperElement = PopperParameters[1];
-type PopperOptions = PopperParameters[2];
-
 export type { Placement as PopperPlacement } from '@popperjs/core';
+export interface PopperOptions<Modifiers> extends Omit<Partial<PopperJS.Options>, 'modifiers'> {
+  modifiers?: ReadonlyArray<Modifier<Modifiers>>;
+  createPopper?: typeof PopperJS.createPopper;
+}
 
-export const usePopper = <TTriggerRef extends TriggerElement, TPopperRef extends PopperElement>(popperOptions?: PopperOptions) => {
-  const [referenceElement, setReferenceElement] = useState<Nullable<TTriggerRef>>(null);
-  const [popperElement, setPopperElement] = useState<Nullable<TPopperRef>>(null);
+interface BasePopperAPI {
+  state: PopperJS.State | null;
+  update: PopperJS.Instance['update'] | null;
+  styles: Record<string, React.CSSProperties>;
+  attributes: Record<string, Record<string, string> | undefined>;
+  forceUpdate: PopperJS.Instance['forceUpdate'] | null;
+}
+
+interface PopperAPI<TriggerRef extends Nullable<Element | PopperJS.VirtualElement>, PopperRef extends Nullable<HTMLElement>> extends BasePopperAPI {
+  popperElement: Nullable<PopperRef>;
+  setPopperElement: React.Dispatch<React.SetStateAction<Nullable<PopperRef>>>;
+  referenceElement: Nullable<TriggerRef>;
+  setReferenceElement: React.Dispatch<React.SetStateAction<Nullable<TriggerRef>>>;
+}
+
+export const usePopper = <TriggerRef extends Nullable<Element | PopperJS.VirtualElement>, PopperRef extends Nullable<HTMLElement>, Modifiers>(
+  popperOptions?: PopperOptions<Modifiers>
+): PopperAPI<TriggerRef, PopperRef> => {
+  const [popperElement, setPopperElement] = useState<Nullable<PopperRef>>(null);
+  const [referenceElement, setReferenceElement] = useState<Nullable<TriggerRef>>(null);
+
   const popperProps = usePopperBase(referenceElement, popperElement, popperOptions);
 
   return {
-    referenceElement,
-    setReferenceElement,
+    ...popperProps,
     popperElement,
     setPopperElement,
-    ...popperProps,
+    referenceElement,
+    setReferenceElement,
   };
 };
 
-export const useVirtualElementPopper = <TPopperRef extends PopperElement>(
-  virtualElement: Nullable<VirtualElement>,
-  popperOptions?: PopperOptions
-) => {
-  const [popperElement, setPopperElement] = useState<Nullable<TPopperRef>>(null);
+interface VirtualPopperAPI<PopperRef extends Nullable<HTMLElement>> extends BasePopperAPI {
+  popperElement: Nullable<PopperRef>;
+  setPopperElement: React.Dispatch<React.SetStateAction<Nullable<PopperRef>>>;
+}
+
+export const useVirtualElementPopper = <VirtualElement extends Nullable<PopperJS.VirtualElement>, PopperRef extends Nullable<HTMLElement>, Modifiers>(
+  virtualElement: VirtualElement,
+  popperOptions?: PopperOptions<Modifiers>
+): VirtualPopperAPI<PopperRef> => {
+  const [popperElement, setPopperElement] = useState<Nullable<PopperRef>>(null);
   const popperProps = usePopperBase(virtualElement, popperElement, popperOptions);
 
   return {
+    ...popperProps,
     popperElement,
     setPopperElement,
-    ...popperProps,
   };
 };

3e887857c2cfa47ae2ddcb432916ea71eb227968
Tyler Han <tylerhan97@gmail.com>
2021-11-23 12:13:58 +0100

    fix: reload on chunk miss (VF-000) (#4662)
    * fix: reload on chunk miss (VF-000)
    
    * chore: more succinct logic
    
    * ci: empty commit to update the track
    
    * chore: empty commit to simulate changes and rebuild
    
    * chore: session storage
    
    Co-authored-by: vf-service-account <serviceaccount@voiceflow.com>

--- packages/creator-app/src/hocs/lazy.tsx
+++ packages/creator-app/src/hocs/lazy.tsx
@@ -1,16 +1,40 @@
-import { Spinner } from '@voiceflow/ui';
+import { FullSpinner, Spinner } from '@voiceflow/ui';
 import React from 'react';
+
+const ALREADY_FORCE_REFRESH_KEY = 'page-has-been-force-refreshed';
+
+const LazyLoadSpinner = () => {
+  React.useEffect(() => {
+    sessionStorage.setItem(ALREADY_FORCE_REFRESH_KEY, 'true');
+    window.location.reload();
+  }, []);
+  return <FullSpinner />;
+};
 
 const ComponentSpinner = () => <Spinner isMd />;
 
-// eslint-disable-next-line import/prefer-default-export
-export const lazy = <T extends React.ComponentType<any>>(factory: () => Promise<{ default: T }>) => React.lazy(() => factory());
+export const lazy = <T extends React.ComponentType<any>>(factory: () => Promise<{ default: T }>) =>
+  React.lazy(async () => {
+    try {
+      const component = await factory();
+      sessionStorage.removeItem(ALREADY_FORCE_REFRESH_KEY);
+      return component;
+    } catch (error) {
+      if (sessionStorage.getItem(ALREADY_FORCE_REFRESH_KEY)) {
+        // The page has already been reloaded
+        // Assuming that user is already using the latest version of the application.
+        // Let's let the application crash and raise the error.
+        throw error;
+      }
+      return { default: LazyLoadSpinner as React.ComponentType<any> };
+    }
+  });
 
 export const lazyComponent = <T extends React.ComponentType<any>, R>(
   factory: () => Promise<{ default: T }>,
   { fallback: Fallback = ComponentSpinner, forwardRef }: { fallback?: React.ComponentType; forwardRef?: boolean } = {}
 ) => {
-  const LazyComponent = React.lazy(() => factory());
+  const LazyComponent = lazy(factory);
 
   return React.forwardRef<R, React.ComponentProps<T>>((props, ref) => (
     <React.Suspense fallback={<Fallback />}>

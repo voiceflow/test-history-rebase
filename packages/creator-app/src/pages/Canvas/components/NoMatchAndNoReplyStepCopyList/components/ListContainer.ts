266adf7d52691386b7cddcf71642279d6dd80df9
Tyler Stewart <hello@tylerstewart.ca>
2022-01-05 11:39:33 -0700

    fix: increase no match and no reply popover width (VF-2539) (#4809)

--- packages/creator-app/src/pages/Canvas/components/NoMatchAndNoReplyStepCopyList/components/ListContainer.ts
+++ packages/creator-app/src/pages/Canvas/components/NoMatchAndNoReplyStepCopyList/components/ListContainer.ts
@@ -1,10 +1,10 @@
 import { styled } from '@/hocs';
 
 const ListContainer = styled.div`
-  width: 250px;
+  width: 300px;
   max-height: 400px;
   overflow-x: hidden;
-  overflow-y: scroll;
+  overflow-y: auto;
 `;
 
 export default ListContainer;

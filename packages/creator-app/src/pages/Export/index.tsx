1a37a26fd5945a77e5c4dba974dfd6a7232bd801
Tyler Han <tyler@voiceflow.com>
2022-03-14 16:39:02 +0000

    fix: canvas-export loading (VF-000) (#5186)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-000**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    
    
    Co-authored-by: Tyler Han <tylerhan97@gmail.com>

--- packages/creator-app/src/pages/Export/index.tsx
+++ packages/creator-app/src/pages/Export/index.tsx
@@ -1,16 +1,13 @@
-import * as Realtime from '@voiceflow/realtime-sdk';
-import _isNumber from 'lodash/isNumber';
 import React from 'react';
 
-import client from '@/client';
-import * as Creator from '@/ducks/creator';
+import LoadingGate from '@/components/LoadingGate';
 import * as ProjectV2 from '@/ducks/projectV2';
 import * as Session from '@/ducks/session';
 import * as WorkspaceV2 from '@/ducks/workspaceV2';
 import { ProjectLoadingGate, WorkspaceFeatureLoadingGate } from '@/gates';
-import { compose, connect, withBatchLoadingGate } from '@/hocs';
+import { compose, withBatchLoadingGate } from '@/hocs';
 import removeIntercom from '@/hocs/removeIntercom';
-import { useSelector } from '@/hooks';
+import { useDispatch, useSelector } from '@/hooks';
 import LinkLayer from '@/pages/Canvas/components/LinkLayer';
 import MarkupLayer from '@/pages/Canvas/components/MarkupLayer';
 import NodeLayer from '@/pages/Canvas/components/NodeLayer';
@@ -18,25 +15,19 @@ import { CanvasProviders, ManagerProvider, PresentationModeProvider } from '@/pa
 import useEngine from '@/pages/Canvas/engine';
 import { getManager } from '@/pages/Canvas/managers';
 import { MarkupProvider, ProjectProvider } from '@/pages/Project/contexts';
-import { Thunk } from '@/store/types';
-import { BLOCK_WIDTH } from '@/styles/theme';
-import { ConnectedProps, Point } from '@/types';
-import { isMarkupBlockType, isRootOrMarkupBlockType } from '@/utils/typeGuards';
 
 import { ExportCanvasDiagram, ExportGlobalStyle, ExportWatermark, MockRealtimeGate } from './components';
+import * as Utils from './utils';
 
-const ExportCanvas: React.FC<ConnectedExportProps> = ({ platform, platformV2, typeV2, diagramID, initialize }) => {
+const ExportCanvas: React.FC = () => {
   const isOnPaidPlan = useSelector(WorkspaceV2.active.isOnPaidPlanSelector);
+
+  const platform = useSelector(ProjectV2.active.platformSelector);
+  const typeV2 = useSelector(ProjectV2.active.typeV2Selector);
+  const platformV2 = useSelector(ProjectV2.active.platformV2Selector);
 
   const engine = useEngine();
   const registerCanvas = React.useCallback((api) => engine.registerCanvas(api), []);
-
-  React.useEffect(() => {
-    if (diagramID) {
-      initialize(diagramID);
-    }
-  }, [diagramID]);
-
   return (
     <ProjectProvider platform={platform} platformV2={platformV2} typeV2={typeV2}>
       <PresentationModeProvider>
@@ -58,105 +49,20 @@ const ExportCanvas: React.FC<ConnectedExportProps> = ({ platform, platformV2, ty
   );
 };
 
-const findCanvasExportOffsets = (nodes: Realtime.Node[], ports: Realtime.Port[]): { maxPoint: Point; offsets: Point } => {
-  const rootNodes = nodes.filter(({ type }) => isRootOrMarkupBlockType(type));
+const CanvasGate: React.FC = ({ children }) => {
+  const [loaded, setLoaded] = React.useState(false);
 
-  const xValues = rootNodes.map((node) => node.x);
-  const yValues = rootNodes.map((node) => node.y);
-  const portXValues = ports.flatMap((port) => port.linkData!.points!.map((point) => point.point[0]));
-  const portYValues = ports.flatMap((port) => port.linkData!.points!.map((point) => point.point[1]));
+  const initialize = useDispatch(Utils.initialize);
+  const diagramID = useSelector(Session.activeDiagramIDSelector);
 
-  const minX = Math.min(...xValues, ...portXValues);
-  const minY = Math.min(...yValues, ...portYValues);
-  const maxX = Math.max(...xValues, ...portXValues);
-  const maxY = Math.max(...yValues, ...portYValues);
+  React.useEffect(() => {
+    if (diagramID) initialize(diagramID).then(() => setLoaded(true));
+  }, [diagramID]);
 
-  const offsetX = 0 - minX;
-  const offsetY = 0 - minY;
-
-  return {
-    offsets: [offsetX, offsetY],
-    maxPoint: [maxX + offsetX, maxY + offsetY],
-  };
+  return <LoadingGate isLoaded={loaded}>{children}</LoadingGate>;
 };
-
-const applyOffsetsToNodes = (nodes: Realtime.Node[], [offsetX, offsetY]: Point) =>
-  nodes.map((node) => ({
-    ...node,
-    x: node.x + offsetX + (isMarkupBlockType(node.type) ? BLOCK_WIDTH / 2 : 0),
-    y: node.y + offsetY,
-  }));
-
-const applyOffsetsToLinkData = (data: Realtime.LinkData | undefined, [offsetX, offsetY]: Point): Realtime.LinkData | undefined =>
-  !data
-    ? undefined
-    : {
-        ...data,
-        points: !data.points
-          ? undefined
-          : data.points.map((point) => ({
-              ...point,
-              point: [point.point[0] + offsetX + BLOCK_WIDTH / 2, point.point[1] + offsetY],
-            })),
-      };
-
-const applyOffsetsToPorts = (ports: Realtime.Port[], offsets: Point) =>
-  ports.map((port) => ({
-    ...port,
-    linkData: applyOffsetsToLinkData(port.linkData, offsets),
-  }));
-
-const applyOffsetsToLinks = (links: Realtime.Link[], offsets: Point) =>
-  links.map((link) => ({
-    ...link,
-    data: applyOffsetsToLinkData(link.data, offsets),
-  }));
-
-const initialize =
-  (diagramID: string): Thunk =>
-  async (dispatch, getState) => {
-    const state = getState();
-    const platform = ProjectV2.active.platformSelector(state);
-
-    const { viewport, ...creator } = Realtime.Adapters.creatorAdapter.fromDB(await client.api.diagram.get(diagramID), { platform, context: {} });
-
-    const nodesWithCoordinates = creator.nodes.filter((node) => _isNumber(node.x) && _isNumber(node.y));
-    const portsWithPoints = creator.ports.filter((port) => !!port.nodeID && !!port.linkData?.points?.length);
-    const { offsets, maxPoint } = findCanvasExportOffsets(nodesWithCoordinates, portsWithPoints);
-
-    creator.nodes = applyOffsetsToNodes(nodesWithCoordinates, offsets);
-    creator.ports = applyOffsetsToPorts(creator.ports, offsets);
-    creator.links = applyOffsetsToLinks(creator.links, offsets);
-
-    dispatch(Creator.initializeCreator(creator));
-
-    const node = document.createElement('div');
-
-    node.style.top = `${maxPoint[1]}px`;
-    node.style.left = `${maxPoint[0]}px`;
-    node.style.width = '1px';
-    node.style.height = '1px';
-    node.style.position = 'absolute';
-
-    // inserting this node to adjust scroll
-    document.body.appendChild(node);
-  };
-
-const mapStateToProps = {
-  diagramID: Session.activeDiagramIDSelector,
-  platform: ProjectV2.active.platformSelector,
-  typeV2: ProjectV2.active.typeV2Selector,
-  platformV2: ProjectV2.active.platformV2Selector,
-};
-
-const mapDispatchToProps = {
-  initialize,
-};
-
-type ConnectedExportProps = ConnectedProps<typeof mapStateToProps, typeof mapDispatchToProps>;
 
 export default compose(
   removeIntercom,
-  connect(mapStateToProps, mapDispatchToProps),
-  withBatchLoadingGate(ProjectLoadingGate, WorkspaceFeatureLoadingGate, MockRealtimeGate)
+  withBatchLoadingGate(ProjectLoadingGate, WorkspaceFeatureLoadingGate, MockRealtimeGate, CanvasGate)
 )(ExportCanvas);
---
+++ packages/creator-app/src/pages/Export/utils.ts
@@ -0,0 +1,95 @@
+import * as Realtime from '@voiceflow/realtime-sdk';
+import _isNumber from 'lodash/isNumber';
+
+import client from '@/client';
+import * as Creator from '@/ducks/creator';
+import * as ProjectV2 from '@/ducks/projectV2';
+import { Thunk } from '@/store/types';
+import { BLOCK_WIDTH } from '@/styles/theme';
+import { Point } from '@/types';
+import { isMarkupBlockType, isRootOrMarkupBlockType } from '@/utils/typeGuards';
+
+const findCanvasExportOffsets = (nodes: Realtime.Node[], ports: Realtime.Port[]): { maxPoint: Point; offsets: Point } => {
+  const rootNodes = nodes.filter(({ type }) => isRootOrMarkupBlockType(type));
+
+  const xValues = rootNodes.map((node) => node.x);
+  const yValues = rootNodes.map((node) => node.y);
+  const portXValues = ports.flatMap((port) => port.linkData!.points!.map((point) => point.point[0]));
+  const portYValues = ports.flatMap((port) => port.linkData!.points!.map((point) => point.point[1]));
+
+  const minX = Math.min(...xValues, ...portXValues);
+  const minY = Math.min(...yValues, ...portYValues);
+  const maxX = Math.max(...xValues, ...portXValues);
+  const maxY = Math.max(...yValues, ...portYValues);
+
+  const offsetX = 0 - minX;
+  const offsetY = 0 - minY;
+
+  return {
+    offsets: [offsetX, offsetY],
+    maxPoint: [maxX + offsetX, maxY + offsetY],
+  };
+};
+
+const applyOffsetsToNodes = (nodes: Realtime.Node[], [offsetX, offsetY]: Point) =>
+  nodes.map((node) => ({
+    ...node,
+    x: node.x + offsetX + (isMarkupBlockType(node.type) ? BLOCK_WIDTH / 2 : 0),
+    y: node.y + offsetY,
+  }));
+
+const applyOffsetsToLinkData = (data: Realtime.LinkData | undefined, [offsetX, offsetY]: Point): Realtime.LinkData | undefined =>
+  !data
+    ? undefined
+    : {
+        ...data,
+        points: !data.points
+          ? undefined
+          : data.points.map((point) => ({
+              ...point,
+              point: [point.point[0] + offsetX + BLOCK_WIDTH / 2, point.point[1] + offsetY],
+            })),
+      };
+
+const applyOffsetsToPorts = (ports: Realtime.Port[], offsets: Point) =>
+  ports.map((port) => ({
+    ...port,
+    linkData: applyOffsetsToLinkData(port.linkData, offsets),
+  }));
+
+const applyOffsetsToLinks = (links: Realtime.Link[], offsets: Point) =>
+  links.map((link) => ({
+    ...link,
+    data: applyOffsetsToLinkData(link.data, offsets),
+  }));
+
+// eslint-disable-next-line import/prefer-default-export
+export const initialize =
+  (diagramID: string): Thunk =>
+  async (dispatch, getState) => {
+    const state = getState();
+    const platform = ProjectV2.active.platformSelector(state);
+
+    const { viewport, ...creator } = Realtime.Adapters.creatorAdapter.fromDB(await client.api.diagram.get(diagramID), { platform, context: {} });
+
+    const nodesWithCoordinates = creator.nodes.filter((node) => _isNumber(node.x) && _isNumber(node.y));
+    const portsWithPoints = creator.ports.filter((port) => !!port.nodeID && !!port.linkData?.points?.length);
+    const { offsets, maxPoint } = findCanvasExportOffsets(nodesWithCoordinates, portsWithPoints);
+
+    creator.nodes = applyOffsetsToNodes(nodesWithCoordinates, offsets);
+    creator.ports = applyOffsetsToPorts(creator.ports, offsets);
+    creator.links = applyOffsetsToLinks(creator.links, offsets);
+
+    dispatch(Creator.initializeCreator(creator));
+
+    const node = document.createElement('div');
+
+    node.style.top = `${maxPoint[1]}px`;
+    node.style.left = `${maxPoint[0]}px`;
+    node.style.width = '1px';
+    node.style.height = '1px';
+    node.style.position = 'absolute';
+
+    // inserting this node to adjust scroll
+    document.body.appendChild(node);
+  };

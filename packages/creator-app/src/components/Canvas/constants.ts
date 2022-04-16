0a2dc8bceff57ea2c25e8fadd0fdbe9a1ce40b7a
Josh <josh@voiceflow.com>
2022-04-16 15:10:14 +0000

    fix: updated message (VF-000) (#5337)
    nico pointed out we should update this message

--- packages/creator-app/src/components/Canvas/constants.ts
+++ packages/creator-app/src/components/Canvas/constants.ts
@@ -43,7 +43,7 @@ export const VARIABLE_DESCRIPTION: Record<string, string> = {
   [BuiltInVariable.SESSIONS]: 'The number of times a particular user has opened the app',
   [BuiltInVariable.USER_ID]: "The user's Amazon/Google unique ID",
   [BuiltInVariable.TIMESTAMP]: 'UNIX timestamp (number of seconds since January 1st, 1970 at UTC)',
-  [BuiltInVariable.PLATFORM]: 'The platform your skill is running on ("alexa" or "google")',
+  [BuiltInVariable.PLATFORM]: 'The platform your skill is running on ("voiceflow", "alexa" or "google")',
   [BuiltInVariable.LOCALE]: 'The locale of the user (eg. en-US, en-CA, it-IT, fr-FR, ...)',
   [BuiltInVariable.INTENT_CONFIDENCE]: 'The confidence interval (measured as a value from 0 to 100) for the most recently matched intent',
   [BuiltInVariable.LAST_UTTERANCE]: `The user's last utterance in a text string`,

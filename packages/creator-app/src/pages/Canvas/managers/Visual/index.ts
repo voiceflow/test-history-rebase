aec1e40841c0cefcb3131dab08d0f5497bdf45ea
Alex Nainer <me@alexnainer.io>
2022-04-19 20:18:47 +0000

    fix: add new NLU platforms to visual step (VF-3378)  (#5346)
    **Fixes or implements VF-3378**
    
    ### Brief description. What is this change?
    
    Only allow chat projects to be supported by visual step.
    
    
    
    Co-authored-by: Alex Nainer <alex.nainer@voiceflow.com>
    Co-authored-by: Tyler Han <tyler@voiceflow.com>

--- packages/creator-app/src/pages/Canvas/managers/Visual/index.ts
+++ packages/creator-app/src/pages/Canvas/managers/Visual/index.ts
@@ -1,5 +1,4 @@
 import * as Realtime from '@voiceflow/realtime-sdk';
-import { VoiceflowConstants } from '@voiceflow/voiceflow-types';
 
 import { NodeManagerConfig } from '../types';
 import { NODE_CONFIG } from './constants';
@@ -10,7 +9,6 @@ const VisualManager: NodeManagerConfig<Realtime.NodeData.Visual, Realtime.NodeDa
   ...NODE_CONFIG,
 
   label: 'Visuals',
-  platforms: [VoiceflowConstants.PlatformType.VOICEFLOW, VoiceflowConstants.PlatformType.DIALOGFLOW_ES],
 
   step: VisualStep,
   editor: VisualEditor,

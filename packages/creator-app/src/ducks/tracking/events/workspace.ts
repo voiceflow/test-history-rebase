d915d6df75e62bfc75b572d60377fee7bfc3664b
Victor Wei <victoryqwei@gmail.com>
2022-05-03 20:26:06 +0000

    fix: add logrocket to segment events (VF-3375) (#5379)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-3375**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    Attach Logrocket to `begin workspace session` Segment Events
    
    
    
    Co-authored-by: Victor Wei <victor.wei@voiceflow.com>

--- packages/creator-app/src/ducks/tracking/events/workspace.ts
+++ packages/creator-app/src/ducks/tracking/events/workspace.ts
@@ -1,4 +1,5 @@
 import * as Realtime from '@voiceflow/realtime-sdk';
+import LogRocket from 'logrocket';
 
 import client from '@/client';
 import { EventName } from '@/ducks/tracking/constants';
@@ -7,10 +8,12 @@ import { getHostName } from '@/utils/window';
 import { createWorkspaceEventPayload } from '../utils';
 
 export const trackWorkspace = (workspace: Realtime.Workspace) => () => {
-  client.api.analytics.track(
-    EventName.WORKSPACE_SESSION_BEGIN,
-    createWorkspaceEventPayload({ workspaceID: workspace.id }, { creator_version: getHostName() })
-  );
+  LogRocket.getSessionURL((sessionURL) => {
+    client.api.analytics.track(
+      EventName.WORKSPACE_SESSION_BEGIN,
+      createWorkspaceEventPayload({ workspaceID: workspace.id, sessionURL }, { creator_version: getHostName() })
+    );
+  });
 
   client.api.analytics.identifyWorkspace(workspace.id, { name: workspace.name });
 };

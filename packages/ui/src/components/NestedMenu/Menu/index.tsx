95b537a767b2676a5eae42fcc027ec5435d42b65
Josh <josh@voiceflow.com>
2022-05-06 19:15:37 +0000

    fix: allow selection on key press [bugfix] (#5399)
    introduced a weird issue where the enter keypress could trigger while a modal was opened, causing various issues
    
    reverted this one https://github.com/voiceflow/creator-app/pull/5382

--- packages/ui/src/components/NestedMenu/Menu/index.tsx
+++ packages/ui/src/components/NestedMenu/Menu/index.tsx
@@ -181,29 +181,17 @@ function BaseNestedMenu({
       case KeyCode.TAB:
       case KeyCode.ENTER:
         {
-          // if it's a direct match or the select is grouped and only 1 option is available, select it
-          if ((directSearchMatch || (grouped && !creatable && flatOptions.length === 1)) && (focusedOptionIndex ?? 0) === 0) {
-            swallowEvent(null, true)(event);
-
-            const option = flatOptions[firstOptionIndex];
-
-            if (isBaseMenuItem(option) && (option.disabled || option.vfUIOnly)) return;
-
-            onSelect(getOptionValue(option), optionsPath, dataRef.current.scheduleUpdate);
-
-            return;
-          }
+          if (focusedOptionIndex === null) return;
 
           const nextValue = searchable && creatable ? searchLabel : newOptionLabel;
 
-          if ((creatable || alwaysShowCreate) && (focusedOptionIndex ?? 0) === 0 && nextValue && !isButtonDisabled?.({ value: nextValue })) {
+          if (creatable && focusedOptionIndex === 0 && nextValue && !isButtonDisabled?.({ value: nextValue })) {
             swallowEvent(null, true)(event);
             onCreate?.(nextValue, dataRef.current.scheduleUpdate);
           } else if (
-            focusedOptionIndex !== null &&
             (!isInput ||
               (event.target && inputWrapperNode?.contains(event.target as Node)) ||
-              ((creatable || alwaysShowCreate) && searchable) ||
+              (creatable && searchable) ||
               (isDropdown && searchable)) &&
             (!creatable || focusedOptionIndex > 0)
           ) {

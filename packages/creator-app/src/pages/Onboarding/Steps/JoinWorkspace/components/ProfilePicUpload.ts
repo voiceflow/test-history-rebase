7514cad209f0805329e76cee4677a71a4f4087c6
Amanda <22350614+amandasteinhauer@users.noreply.github.com>
2022-05-13 15:01:30 +0000

    fix: profile pic size change (VF-000) (#5430)
    **Fixes or implements VF-000**
    
    ### Brief description. What is this change?
    
    Changes profile pic image size on invite join page to 42px.

--- packages/creator-app/src/pages/Onboarding/Steps/JoinWorkspace/components/ProfilePicUpload.ts
+++ packages/creator-app/src/pages/Onboarding/Steps/JoinWorkspace/components/ProfilePicUpload.ts
@@ -2,7 +2,7 @@ import { Upload, UploadIconVariant } from '@voiceflow/ui';
 
 import { styled } from '@/hocs';
 
-const ProfilePicUpload = styled(Upload.IconUpload).attrs({ size: UploadIconVariant.EXTRA_LARGE })`
+const ProfilePicUpload = styled(Upload.IconUpload).attrs({ size: UploadIconVariant.EXTRA_SMALL })`
   display: inline-block;
   margin-left: 10px;
   position: relative;

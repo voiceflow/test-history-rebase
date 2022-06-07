451d31120f1be9724e4c0d3d8f7259132986def2
Victor Wei <victoryqwei@gmail.com>
2022-06-07 18:54:14 +0000

    feat: update icon/label for set step v2 (VF-3370) (#5543)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-3370**
    
    ### Brief description. What is this change?
    
    Updates the label and icon for the set step.
    
    Co-authored-by: Victor Wei <victor.wei@voiceflow.com>

--- packages/creator-app/src/pages/Canvas/managers/SetV2/v2/index.ts
+++ packages/creator-app/src/pages/Canvas/managers/SetV2/v2/index.ts
@@ -2,9 +2,11 @@ import * as Realtime from '@voiceflow/realtime-sdk';
 
 import { NodeManagerConfigV2 } from '../../types';
 import SetEditor from './SetEditor';
+import ConnectedSetStep from './SetStep';
 
 const SetManagerV2: Partial<NodeManagerConfigV2<Realtime.NodeData.SetV2, Realtime.NodeData.SetV2BuiltInPorts>> = {
   editorV2: SetEditor,
+  step: ConnectedSetStep,
 };
 
 export default SetManagerV2;

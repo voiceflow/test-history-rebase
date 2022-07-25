2f2abcdbfd11c65a672ac82175221f9734a1f9e3
Tyler Han <tyler@voiceflow.com>
2022-07-25 22:30:44 +0000

    fix: invalidate dne entity (VF-4016) (#5723)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-4016**
    
    ### Brief description. What is this change?
    When an utterance has an entity that has been deleted or no longer exists, we sanitize it to no longer reference that entity. This is permanent
    
    BEFORE:
    <img width="812" alt="Screen Shot 2022-07-06 at 1 55 32 PM" src="https://user-images.githubusercontent.com/5643574/177613235-290d4745-3aa0-4c17-a8c1-38bbc04fe109.png">
    
    AFTER:
    <img width="928" alt="Screen Shot 2022-07-06 at 3 19 29 PM" src="https://user-images.githubusercontent.com/5643574/177626424-680f6584-90e4-4524-87cf-69121c84b538.png">
    
    
    This doesn't affect variables because ID and name are the same.
    
    Co-authored-by: Tyler Han <tylerhan97@gmail.com>

--- packages/realtime/src/services/migrate/migrations/index.ts
+++ packages/realtime/src/services/migrate/migrations/index.ts
@@ -5,12 +5,14 @@ import { Migration, Transform } from './types';
 import migrateToV2_0 from './v2_0';
 import migrateToV2_1 from './v2_1';
 import migrateToV2_2 from './v2_2';
+import migrateToV2_3 from './v2_3';
 
 const migrationsMap: Record<Realtime.SchemaVersion, Transform> = {
   [Realtime.SchemaVersion.V1]: Utils.functional.noop,
   [Realtime.SchemaVersion.V2]: migrateToV2_0,
   [Realtime.SchemaVersion.V2_1]: migrateToV2_1,
   [Realtime.SchemaVersion.V2_2]: migrateToV2_2,
+  [Realtime.SchemaVersion.V2_3]: migrateToV2_3,
 };
 
 const migrations = Object.entries(migrationsMap)

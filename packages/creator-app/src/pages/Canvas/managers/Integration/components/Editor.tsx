970b4b029cd03b22adb9a2fc574a7e28e8b61c9a
Vinicius Dacal <viniciusldacal@gmail.com>
2022-08-12 14:40:33 +0000

    fix: [bugfix] root api editor showing back button [bugfix] (CT-781) (#5898)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->

--- packages/creator-app/src/pages/Canvas/managers/Integration/components/Editor.tsx
+++ packages/creator-app/src/pages/Canvas/managers/Integration/components/Editor.tsx
@@ -1,8 +1,6 @@
 import * as Realtime from '@voiceflow/realtime-sdk';
 import React from 'react';
 
-import * as Documentation from '@/config/documentation';
-import EditorV2 from '@/pages/Canvas/components/EditorV2';
 import { NodeEditorV2 } from '@/pages/Canvas/managers/types';
 
 import APIForm from './APIForm';
@@ -10,14 +8,7 @@ import { isCustomAPIEditor } from './utils';
 import ZapierAndGoogleEditor from './ZapierAndGoogleEditor';
 
 const IntegrationEditorV2: NodeEditorV2<Realtime.NodeData.Integration, Realtime.NodeData.IntegrationBuiltInPorts> = (props) => {
-  if (isCustomAPIEditor(props))
-    return (
-      <APIForm
-        header={<EditorV2.DefaultHeader onBack={props.goBack} />}
-        editor={props}
-        footer={<APIForm.Footer tutorial={Documentation.ACTIONS_BACKEND} />}
-      />
-    );
+  if (isCustomAPIEditor(props)) return <APIForm editor={props} />;
 
   return <ZapierAndGoogleEditor {...props} />;
 };

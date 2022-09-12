c20aec7c93d0a8ad2ec5581af8c44d3fc058df61
Tyler Han <tylerhan97@gmail.com>
2022-09-12 17:01:35 -0400

    fix: add text editor classname [bugfix] (CT-000)

--- packages/creator-app/src/components/SlateInputs/components/SlateBaseInput.tsx
+++ packages/creator-app/src/components/SlateInputs/components/SlateBaseInput.tsx
@@ -4,13 +4,14 @@ import { Input } from '@voiceflow/ui';
 import React from 'react';
 
 import SlateEditable, { SlateEditableProps, SlateEditableRef, SlateValue } from '@/components/SlateEditable';
+import { ClassName } from '@/styles/constants';
 
 export interface SlateBaseInputProps extends Omit<SlateEditableProps, 'onBlur'> {
   onBlur?: Nullish<(value: SlateValue, event: React.FocusEvent) => void>;
 }
 
 const SlateBaseInput: React.ForwardRefRenderFunction<SlateEditableRef, SlateBaseInputProps> = ({ value, onBlur, children, ...props }, ref) => (
-  <Input>
+  <Input className={ClassName.TEXT_EDITOR}>
     {({ ref: inputRef }) => (
       <SlateEditable ref={composeRef(inputRef, ref)} value={value} onBlur={(event) => onBlur?.(value, event)} {...props}>
         {children}

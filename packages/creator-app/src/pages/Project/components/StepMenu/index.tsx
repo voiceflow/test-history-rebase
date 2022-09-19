3dc35534d3fc3a035ff97105cb15cfa65082e4e3
Josh <josh@voiceflow.com>
2022-09-19 20:13:26 +0000

    fix: collapse step menu on first login (CT-1005) (#6097)
    for activation
    
    https://voiceflow.atlassian.net/jira/software/c/projects/CT/boards/13?modal=detail&selectedIssue=CT-1005&assignee=5d2f70dc4d6e3c0c88a040b7

--- packages/creator-app/src/pages/Project/components/StepMenu/index.tsx
+++ packages/creator-app/src/pages/Project/components/StepMenu/index.tsx
@@ -1,22 +1,25 @@
-import { SvgIcon } from '@voiceflow/ui';
+import { SvgIcon, useLocalStorageState } from '@voiceflow/ui';
 import React from 'react';
 
 import { Permission } from '@/config/permissions';
 import * as CanvasTemplates from '@/ducks/canvasTemplate';
 import * as ProjectV2 from '@/ducks/projectV2';
-import { usePermission, useSelector, useToggle } from '@/hooks';
+import { usePermission, useSelector } from '@/hooks';
 import { Identifier } from '@/styles/constants';
 
 import { getAllSections } from './constants';
 import * as S from './styles';
 import TopLevelButton from './TopLevelButton';
 
+const STEP_MENU_EXPANDED_LOCAL_STORAGE_KEY = 'stepMenuExpanded';
+
 const StepMenu: React.FC<{ numCollapsedSteps?: number }> = ({ numCollapsedSteps = 3 }) => {
   const platform = useSelector(ProjectV2.active.platformSelector);
   const projectType = useSelector(ProjectV2.active.projectTypeSelector);
+  const [isExpanded, toggleIsExpanded] = useLocalStorageState(STEP_MENU_EXPANDED_LOCAL_STORAGE_KEY, false);
+
   const templates = useSelector(CanvasTemplates.allCanvasTemplatesSelector);
   const [canEditCanvas] = usePermission(Permission.EDIT_CANVAS);
-  const [isExpanded, toggleIsExpanded] = useToggle(true);
 
   const steps = getAllSections(platform, projectType, templates);
 
@@ -32,7 +35,7 @@ const StepMenu: React.FC<{ numCollapsedSteps?: number }> = ({ numCollapsedSteps 
             ))}
           </S.TopLevelInnerContainer>
 
-          <S.StepMenuExpandButton onClick={toggleIsExpanded}>
+          <S.StepMenuExpandButton onClick={() => toggleIsExpanded(!isExpanded)}>
             <SvgIcon icon="arrowToggleV2" size={20} color="#6e849a" inline rotation={isExpanded ? 270 : 90} />
           </S.StepMenuExpandButton>
         </S.TopLevelOuterContainer>

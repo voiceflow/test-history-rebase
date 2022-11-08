826354a5235e784481c38d437d362577635666f1
xavidop <xavi_tb@hotmail.com>
2022-11-08 13:00:18 +0100

    fix: audio test

--- packages/creator-app/src/utils/audio.ts
+++ packages/creator-app/src/utils/audio.ts
@@ -1,4 +1,4 @@
 const BUCKET_PREFIX_REGEX = /(.*?)\/\d+-/;
-const BUCKET_STRING = 'getstoryflow.audio';
+const BUCKET_STRING = 'voiceflow-audio';
 
 export const prettifyBucketURL = (url?: string | null): string => (url?.includes(BUCKET_STRING) ? url.replace(BUCKET_PREFIX_REGEX, '') : url ?? '');

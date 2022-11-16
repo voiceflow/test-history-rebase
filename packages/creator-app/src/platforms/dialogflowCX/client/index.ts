63fbd84e338562a0f85e69fa921666e059807561
Matheus Poleza <matheus.poleza@gmail.com>
2022-11-16 22:57:33 +0000

    fix: adds model import endpoint call to dfcx projects (NLU-440) (#6319)
    **Fixes or implements NLU-440**
    
    ### Brief description. What is this change?
    We added file extension import to DF CX but we forgot to add the endpoint call to model import. As we don't have a specific dfcx endpoint, added VF general endpoint for now.
    
    https://www.loom.com/share/987c24ddd3a44c03945e3247cd175cf1

--- packages/creator-app/src/platforms/dialogflowCX/client/index.ts
+++ packages/creator-app/src/platforms/dialogflowCX/client/index.ts
@@ -1,8 +1,11 @@
-import { googleService } from '@/client/fetch';
+import * as Platform from '@voiceflow/platform-config';
+
+import { generalService, googleService } from '@/client/fetch';
 import { createPublishService } from '@/client/services';
+import { RESOURCE_ENDPOINT } from '@/client/services/modelImport';
 import { GOOGLE_SERVICE_ENDPOINT } from '@/config';
 import { GoogleStageType } from '@/constants/platforms';
-import { DialogflowCXPublishJob } from '@/models';
+import { DialogflowCXPublishJob, NLUImportModel } from '@/models';
 
 import generalServiceClient from '../../general/client';
 import type { AgentName, GCPAgent } from '../types';
@@ -10,7 +13,13 @@ import type { AgentName, GCPAgent } from '../types';
 const DIALOGFLOW_CX_ENDPOINT = `${GOOGLE_SERVICE_ENDPOINT}/dialogflow/cx`;
 
 const dialogflowCXServiceClient = {
-  modelImport: null,
+  modelImport: {
+    // TO DO: Remove it once we have a proper endpoint for dfcx model import
+    import: (_type: string, formData?: FormData) => {
+      const importEndpoint = `${RESOURCE_ENDPOINT}/${Platform.Constants.PlatformType.VOICEFLOW}`;
+      return generalService.post<NLUImportModel>(importEndpoint, formData, { json: false }).then((res) => res);
+    },
+  },
   prototype: generalServiceClient.prototype,
   publish: {
     ...createPublishService<DialogflowCXPublishJob.AnyJob, GoogleStageType>(DIALOGFLOW_CX_ENDPOINT),

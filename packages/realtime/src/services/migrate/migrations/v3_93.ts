50323c5582895e098de4518276758f9426510fd5
Yauheni <overhawlin@gmail.com>
2022-12-07 14:25:45 +0100

    fix: migration [bugfix] (CT-000)

--- packages/realtime/src/services/migrate/migrations/v3_93.ts
+++ packages/realtime/src/services/migrate/migrations/v3_93.ts
@@ -7,7 +7,7 @@ import { Transform } from './types';
  * this migration fixes versions with duplicate intents/entities that have the same ID
  */
 const migrateToV3_93: Transform = ({ version }) => {
-  version.platformData.intents = uniqBy(version.platformData.intents, 'id');
+  version.platformData.intents = uniqBy(version.platformData.intents, 'key');
   version.platformData.slots = uniqBy(version.platformData.slots, 'key');
 };
 

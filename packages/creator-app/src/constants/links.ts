c292afce352cadb02bc19dda5d2a593689954a7b
(Frank) Yu Cheng Gu <frank@voiceflow.com>
2023-01-03 21:59:03 +0000

    fix: broken links for ai assist features (BUG-157) (#6505)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements [BUG-157](https://voiceflow.atlassian.net/browse/BUG-157)**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->

--- packages/creator-app/src/constants/links.ts
+++ packages/creator-app/src/constants/links.ts
@@ -11,7 +11,7 @@ export const DESKTOP_APP_LINK = 'https://www.voiceflow.com/desktop';
 export const PLAN_INFO_LINK = 'https://www.voiceflow.com/pricing';
 export const APL_TOOL_LINK = 'https://developer.amazon.com/alexa/console/ask/displays';
 
-export const AI_GENERAL_LINK = 'https://voiceflow.zendesk.com/hc/en-us/categories/11695656569229-AI-Assist-BETA-';
-export const LEARN_GENERATIVE_TASKS = 'https://voiceflow.zendesk.com/hc/en-us/sections/11696014193805-Generative-Tasks';
-export const LEARN_FREESTYLE = 'https://voiceflow.zendesk.com/hc/en-us/sections/11695912012045-Freestyle';
+export const AI_GENERAL_LINK = 'https://voiceflow.zendesk.com/hc/en-us/articles/11935569385997-What-is-AI-Assist-';
+export const LEARN_GENERATIVE_TASKS = 'https://voiceflow.zendesk.com/hc/en-us/articles/11935524043149-Learn-about-Generative-tasks';
+export const LEARN_FREESTYLE = 'https://voiceflow.zendesk.com/hc/en-us/articles/11935516192909-Learn-about-Freestyle';
 export const REQUEST_MORE_TOKENS = 'https://voiceflow.zendesk.com/hc/en-us/articles/11696284619533-Request-more-Tokens';

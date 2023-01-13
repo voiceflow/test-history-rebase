8eb8c1f47eef8a791f2377ec21d2f1ca0573c1cb
Bassem <bessem.merai@gmail.com>
2023-01-13 17:36:25 +0000

    fix: fix menu message in NLU Export (NLU-549) (#6559)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    https://voiceflow.atlassian.net/browse/NLU-549
    
    **Fixes or implements VF-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->

--- packages/creator-app/src/pages/Project/components/Header/components/SharePopper/components/Export/components/Model/index.tsx
+++ packages/creator-app/src/pages/Project/components/Header/components/SharePopper/components/Export/components/Model/index.tsx
@@ -1,29 +1,23 @@
 import { Utils } from '@voiceflow/common';
-import * as Realtime from '@voiceflow/realtime-sdk';
 import { BlockText, Box, createDividerMenuItemOption, isNotUIOnlyMenuItemOption, Select, SvgIcon } from '@voiceflow/ui';
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 import IntentsSelect from '@/components/IntentsSelect';
 import UpgradeOption from '@/components/UpgradeOption';
 import * as NLP from '@/config/nlp';
 import { Permission } from '@/config/permissions';
 import { getNLUExportLimitDetails, isGatedNLUExportType } from '@/config/planLimits/nluExport';
-import * as ProjectV2 from '@/ducks/projectV2';
 import { UpgradePrompt } from '@/ducks/tracking';
-import { useActiveProjectNLUConfig, usePermission } from '@/hooks';
+import { usePermission } from '@/hooks';
 
 import { ExportContext } from '../../Context';
 
 const ExportModel: React.OldFC<{
   selectedIntentsIds?: string[];
 }> = ({ selectedIntentsIds }) => {
-  const nluConfig = useActiveProjectNLUConfig();
-
   const { exportNLPType, setExportNLPType, setExportIntents, exportIntents, nlpTypes, setCanExport, setCheckedExportIntents } =
     React.useContext(ExportContext)!;
 
-  const platform = useSelector(ProjectV2.active.platformSelector);
   const [permissionToExport] = usePermission(Permission.NLU_EXPORT_ALL);
   const [permissionToExportCSV] = usePermission(Permission.NLU_EXPORT_CSV);
   const [selectedIntents, setSelectedIntents] = React.useState(exportIntents);
@@ -106,9 +100,7 @@ const ExportModel: React.OldFC<{
           {nluOptions.length === 1 && exportNLPType === NLP.Constants.NLPType.VOICEFLOW ? (
             'Export as .CSV'
           ) : (
-            <>
-              Export as .CSV, or as consumable file for {Realtime.Utils.typeGuards.isVoiceflowPlatform(platform) ? 'any NLU vendor' : nluConfig.name}.
-            </>
+            <>Export as .CSV, or as consumable file for any NLU vendor.</>
           )}
         </span>
       </BlockText>

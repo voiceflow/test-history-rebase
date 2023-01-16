3f04735a8abed646dab07a5584f025ab5d7dfa72
Bassem <bessem.merai@gmail.com>
2023-01-16 22:41:15 +0000

    fix: add loading state to Clarity and Confidence scores (CT-1432) (#6567)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->

--- packages/ui/src/components/StrengthGauge/index.tsx
+++ packages/ui/src/components/StrengthGauge/index.tsx
@@ -18,7 +18,7 @@ const LINE_MULTIPLIER_MAP: Record<Level, number> = {
 
 const StrengthGauge: React.FC<T.Props> = ({ level = Level.NOT_SET, width = 100, tooltipLabelMap }) => (
   <S.Container content={tooltipLabelMap?.[level] || TOOLTIP_LABEL_MAP[level]} offset={[0, 8]}>
-    <ProgressBar width={width} color={StrengthColor[level]} height={2} progress={LINE_MULTIPLIER_MAP[level]} />
+    <ProgressBar width={width} color={StrengthColor[level]} height={2} progress={LINE_MULTIPLIER_MAP[level]} loading={level === Level.LOADING} />
   </S.Container>
 );
 

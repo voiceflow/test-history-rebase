7fb8a39158558b62d3cdb6a2e19036e70aec8a5e
Tyler Han <tyler@voiceflow.com>
2023-01-19 17:13:26 +0000

    fix: sso tab endpoint [bugfix] (BUG-177) (#6592)
    the endpoint is wrong:
    https://github.com/voiceflow/platform/blob/ba740969e634b4cb9a200c172f04c9311195cb09/apps/identity/src/provider/saml2-provider.controller.ts#L11
    
    This was breaking SSO and causing an infinite load
    
    Co-authored-by: Tyler Han <tylerhan97@gmail.com>

--- packages/realtime-sdk/src/clients/identity/v1alpha1/provider.ts
+++ packages/realtime-sdk/src/clients/identity/v1alpha1/provider.ts
@@ -4,7 +4,7 @@ import { NestResource, NestResourceOptions } from '../../nest';
 
 export class Provider extends NestResource {
   constructor(options: NestResourceOptions) {
-    super({ ...options, path: '/provider' });
+    super({ ...options, path: '/saml2-provider' });
   }
 
   public async findOneByOrganizationDomain(domain: string): Promise<Models.Identity.SAMLProvider> {

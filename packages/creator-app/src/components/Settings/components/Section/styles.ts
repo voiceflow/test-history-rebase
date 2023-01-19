e8c4e3c2cf9a501b9da002f17274527ba7343f79
Tyler Han <tylerhan97@gmail.com>
2023-01-19 11:10:36 -0500

    fix: webchat section width failing [bugfix] (CT-000)

--- packages/creator-app/src/components/Settings/components/Section/styles.ts
+++ packages/creator-app/src/components/Settings/components/Section/styles.ts
@@ -20,8 +20,8 @@ export const Title = styled.div<{ withDescription?: boolean }>`
     `}
 `;
 
-export const Container = styled.div<{ mb?: number; w?: number }>`
-  width: ${({ w }) => `${w}px` || `100%`};
+export const Container = styled.div<{ mb?: number; w?: number | string }>`
+  width: ${({ w = '100%' }) => (typeof w === 'number' ? `${w}px` : w)};
 
   &:not(:last-child) {
     margin-bottom: ${({ mb = 32 }) => `${mb}px`};

aff09cb5828cffb3f2a322dfd5e40a2818992e95
Filipe Merker <filipesmerker@gmail.com>
2023-02-27 14:39:35 +0000

    feat: add creation by template (CT-1628) (#6741)
    This adds the ability to create assistants from templates on dashbaord V2
    
    Co-authored-by: Filipe Merker <filipe.merker@voiceflow.com>

--- packages/creator-app/src/pages/DashboardV2/pages/ProjectList/components/TemplateSection.tsx
+++ packages/creator-app/src/pages/DashboardV2/pages/ProjectList/components/TemplateSection.tsx
@@ -1,45 +1,68 @@
+import * as Platform from '@voiceflow/platform-config';
 import { AssistantCard, Box, Button } from '@voiceflow/ui';
 import React from 'react';
 
 import { chatTemplate, teamsTemplate, whatsappTemplate } from '@/assets';
+import * as Project from '@/ducks/project';
+import * as Router from '@/ducks/router';
+import { useDispatch } from '@/hooks';
 
 import * as S from '../styles';
 
-const TemplateSection: React.FC = () => (
-  <Box fullWidth>
-    <S.Title>Start with a template</S.Title>
+const PAYMENT_ACCOUNT_TEMPLATE_TAG = 'paymentsAccount';
+const RETAIL_PURCHASES_TEMPLATE_TAG = 'retailPurchases';
+const SUPPORT_CHATBOT_TEMPLATE_TAG = 'supportChatbot';
 
-    <S.Grid>
-      <AssistantCard
-        image={<AssistantCard.Image src={chatTemplate} backgroundColor="#f4f8fe" />}
-        action={<Button>Copy Template</Button>}
-        title="Webchat - Book a Demo"
-        subtitle="By Voiceflow"
-        icon="slack"
-      />
-      <AssistantCard
-        image={<AssistantCard.Image src={whatsappTemplate} backgroundColor="#f5faf6" />}
-        action={<Button>Copy Template</Button>}
-        title="Whatsapp Customer Support"
-        subtitle="By Voiceflow"
-        icon="logoWhatsapp"
-      />
-      <AssistantCard
-        image={<AssistantCard.Image src={teamsTemplate} backgroundColor="#f4f4fb" />}
-        action={<Button>Copy Template</Button>}
-        title="Microsoft Teams Internal Assistant"
-        subtitle="By Voiceflow"
-        icon="logoMicrosoftTeams"
-      />
-      <AssistantCard
-        image={<AssistantCard.Image src={chatTemplate} backgroundColor="#f4f8fe" />}
-        action={<Button>Copy Template</Button>}
-        title="Webchat - Book a Demo"
-        subtitle="By Voiceflow"
-        icon="slack"
-      />
-    </S.Grid>
-  </Box>
-);
+const TemplateSection: React.FC = () => {
+  const goToDomain = useDispatch(Router.goToDomain);
+  const createProject = useDispatch(Project.createProject);
+
+  const createAndGo = async (tag: string, platform: Platform.Constants.PlatformType) => {
+    const { versionID } = await createProject({
+      nluType: Platform.Constants.NLUType.VOICEFLOW,
+      platform,
+      templateTag: `dashboard:${tag}`,
+      projectType: Platform.Constants.ProjectType.CHAT,
+      tracking: {
+        language: 'English (en-US)',
+        onboarding: true,
+      },
+    });
+
+    goToDomain({ versionID });
+  };
+
+  return (
+    <Box fullWidth>
+      <S.Title>Start with a template</S.Title>
+
+      <S.Grid>
+        <AssistantCard
+          image={<AssistantCard.Image src={chatTemplate} backgroundColor="#f4f8fe" />}
+          action={<Button onClick={() => createAndGo(PAYMENT_ACCOUNT_TEMPLATE_TAG, Platform.Constants.PlatformType.VOICEFLOW)}>Copy Template</Button>}
+          title="Payments & Accounts (IVR)"
+          subtitle="By Voiceflow"
+          icon="slack"
+        />
+        <AssistantCard
+          image={<AssistantCard.Image src={whatsappTemplate} backgroundColor="#f5faf6" />}
+          action={
+            <Button onClick={() => createAndGo(RETAIL_PURCHASES_TEMPLATE_TAG, Platform.Constants.PlatformType.VOICEFLOW)}>Copy Template</Button>
+          }
+          title="Retail Purchases (Chat)"
+          subtitle="By Voiceflow"
+          icon="logoWhatsapp"
+        />
+        <AssistantCard
+          image={<AssistantCard.Image src={teamsTemplate} backgroundColor="#f4f4fb" />}
+          action={<Button onClick={() => createAndGo(SUPPORT_CHATBOT_TEMPLATE_TAG, Platform.Constants.PlatformType.WEBCHAT)}>Copy Template</Button>}
+          title="Support Chatbot (Webchat)"
+          subtitle="By Voiceflow"
+          icon="logoMicrosoftTeams"
+        />
+      </S.Grid>
+    </Box>
+  );
+};
 
 export default TemplateSection;

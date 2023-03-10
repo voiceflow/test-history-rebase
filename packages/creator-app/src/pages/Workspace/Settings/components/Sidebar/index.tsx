7fb26ff606df6af3d3edd6cc9fe0311c6f27763c
astopo <amy.soetopo@gmail.com>
2023-03-10 22:58:07 +0000

    feat: remove developer tab from workspace settings (PL-472) (#6805)
    **Fixes or implements PL-472**
    
    ### Brief description. What is this change?
    
    Workspace keys are deprecated, and we removed the API route for getting them on the Developer tab of the Workspace Settings page. This PR removes the Developer tab.
    
    <img width="1049" alt="Screen Shot 2023-03-10 at 16 57 19" src="https://user-images.githubusercontent.com/3986977/224443619-fa4ae098-67a1-4d38-a775-b2ada622ba70.png">
    
    ### Checklist
    
    - [ ] changes have been validated in an ephemeral environment
    - [ ] this is a breaking change and should publish a new major version
    - [ ] appropriate tests have been written
    - [ ] any new env vars have been added to the [notion doc](https://www.notion.so/voiceflow/Add-Environment-Variables-be1b0136479f45f1adece7995a7adbfb) and infra has been notified

--- packages/creator-app/src/pages/Workspace/Settings/components/Sidebar/index.tsx
+++ packages/creator-app/src/pages/Workspace/Settings/components/Sidebar/index.tsx
@@ -1,4 +1,3 @@
-import * as Realtime from '@voiceflow/realtime-sdk';
 import { Box } from '@voiceflow/ui';
 import React from 'react';
 import { generatePath } from 'react-router-dom';
@@ -7,15 +6,13 @@ import NavLinkSidebar from '@/components/NavLinkSidebar';
 import PlanBubble from '@/components/PlanBubble';
 import { Path } from '@/config/routes';
 import { Permission } from '@/constants/permissions';
-import { useActiveWorkspace, useFeature, usePermission } from '@/hooks';
+import { useActiveWorkspace, usePermission } from '@/hooks';
 
 const Sidebar: React.FC = () => {
   const workspace = useActiveWorkspace();
-  const disableIntegration = useFeature(Realtime.FeatureFlag.DISABLE_INTEGRATION)?.isEnabled;
 
   const [canConfigureOrganization] = usePermission(Permission.CONFIGURE_ORGANIZATION);
   const [canConfigureWorkspaceBilling] = usePermission(Permission.CONFIGURE_WORKSPACE_BILLING);
-  const canConfigureWorkspaceDeveloper = usePermission(Permission.CONFIGURE_WORKSPACE_DEVELOPER)[0] && !disableIntegration;
 
   const canManageSSO = canConfigureOrganization && workspace?.organizationID;
 
@@ -40,10 +37,6 @@ const Sidebar: React.FC = () => {
           : null,
 
         canManageSSO ? { to: generatePath(Path.WORKSPACE_SSO_SETTINGS, pathParams), key: 'sso', label: 'SSO' } : null,
-
-        canConfigureWorkspaceDeveloper
-          ? { to: generatePath(Path.WORKSPACE_DEVELOPER_SETTINGS, pathParams), key: 'developer', label: 'Developer' }
-          : null,
       ]}
     />
   );

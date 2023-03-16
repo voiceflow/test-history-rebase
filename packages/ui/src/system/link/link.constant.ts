896a28f66e559f607df47870f166cdcaed33b4a2
Filipe Merker <filipesmerker@gmail.com>
2023-03-16 20:32:07 +0000

    feat: fix protocol regex (CT-1834) (#6828)
    This fixes the link protocol regex to allow for `mailto:` links.
    
    This is only used to format `href` links that go to react router anchors.
    
    Co-authored-by: Filipe Merker <filipe.merker@voiceflow.com>

--- packages/ui/src/system/link/link.constant.ts
+++ packages/ui/src/system/link/link.constant.ts
@@ -1,6 +1,6 @@
 import { Color } from './link.enum';
 
-export const PROTOCOL_POSTFIX_REGEXP = /:?\/\//;
+export const PROTOCOL_POSTFIX_REGEXP = /(:?\/\/|mailto:[\w%+.-]+@[\d.A-Za-z-]+\.[A-Za-z]{2,})/;
 
 export const ACTIVE_COLOR_MAP: Record<Color, string> = {
   [Color.DARK]: '#5E96E4',

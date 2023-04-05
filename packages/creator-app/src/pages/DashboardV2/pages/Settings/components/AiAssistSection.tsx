85e3ca9a9dbc92961ee4489fd4364378d71305ff
Amanda <22350614+amandasteinhauer@users.noreply.github.com>
2023-04-05 01:08:47 +0000

    fix: workspace settings styling (CT-1869) (#6885)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements CT-1869**
    
    ### Brief description. What is this change?
    
    Fix dynamic width of ai assistant and delete workspace sections for workspace settings

--- packages/creator-app/src/pages/DashboardV2/pages/Settings/components/AiAssistSection.tsx
+++ packages/creator-app/src/pages/DashboardV2/pages/Settings/components/AiAssistSection.tsx
@@ -2,6 +2,7 @@ import * as Realtime from '@voiceflow/realtime-sdk';
 import { Badge, Box, Link, Toggle } from '@voiceflow/ui';
 import React from 'react';
 
+import Page from '@/components/Page';
 import * as Settings from '@/components/Settings';
 import { AI_GENERAL_LINK } from '@/constants';
 import * as Workspace from '@/ducks/workspace';
@@ -25,34 +26,34 @@ const AiAssistSection: React.FC = () => {
   if (!isAssistantAiEnabled) return null;
 
   return (
-    <Settings.Section
-      title={
-        <Box.Flex>
-          Voiceflow AI Assist <Badge.Descriptive marginLeft={9}>Beta</Badge.Descriptive>
-        </Box.Flex>
+    <Page.Section
+      header={
+        <Page.Section.Header>
+          <Page.Section.Title>
+            Voiceflow AI Assist <Badge.Descriptive marginLeft={9}>Beta</Badge.Descriptive>
+          </Page.Section.Title>
+          <Page.Section.Description>
+            {' '}
+            <>
+              Leverage practical AI to design, manage and improve your assistants faster than previously possible.{' '}
+              <Link onClick={() => openURLInANewTab(AI_GENERAL_LINK)}>Learn more</Link>
+            </>
+          </Page.Section.Description>
+        </Page.Section.Header>
       }
-      description={
-        <>
-          Leverage practical AI to design, manage and improve your assistants faster than previously possible.{' '}
-          <Link onClick={() => openURLInANewTab(AI_GENERAL_LINK)}>Learn more</Link>
-        </>
-      }
-      w={700}
     >
-      <Settings.Card>
-        <Settings.SubSection contentProps={{ topOffset: 3 }}>
-          <Box.FlexApart fullWidth>
-            <div>
-              <Settings.SubSection.Title>Voiceflow AI Assist</Settings.SubSection.Title>
+      <Settings.SubSection contentProps={{ topOffset: 3 }} splitView>
+        <Box.FlexApart fullWidth>
+          <div>
+            <Settings.SubSection.Title>Voiceflow AI Assist</Settings.SubSection.Title>
 
-              <Settings.SubSection.Description>Enable or disable workspace members to access AI Assist features.</Settings.SubSection.Description>
-            </div>
+            <Settings.SubSection.Description>Enable or disable workspace members to access AI Assist features.</Settings.SubSection.Description>
+          </div>
 
-            <Toggle checked={!!workspace?.settings?.aiAssist} size={Toggle.Size.EXTRA_SMALL} onChange={handleToggle} hasLabel />
-          </Box.FlexApart>
-        </Settings.SubSection>
-      </Settings.Card>
-    </Settings.Section>
+          <Toggle checked={!!workspace?.settings?.aiAssist} size={Toggle.Size.EXTRA_SMALL} onChange={handleToggle} hasLabel />
+        </Box.FlexApart>
+      </Settings.SubSection>
+    </Page.Section>
   );
 };
 
--- packages/creator-app/src/pages/DashboardV2/pages/Settings/components/DeleteWorkspaceSection.tsx
+++ packages/creator-app/src/pages/DashboardV2/pages/Settings/components/DeleteWorkspaceSection.tsx
@@ -1,7 +1,7 @@
 import { Box, Button } from '@voiceflow/ui';
 import React from 'react';
 
-import * as Settings from '@/components/Settings';
+import Page from '@/components/Page';
 import { Permission } from '@/constants/permissions';
 import { useActiveWorkspace, usePermission } from '@/hooks';
 import * as ModalsV2 from '@/ModalsV2';
@@ -14,21 +14,22 @@ const GeneralSettingsPage: React.FC = () => {
   const boardDeleteModal = ModalsV2.useModal(ModalsV2.Board.Delete);
 
   return (
-    <Box width={700}>
-      {!!workspace && canDeleteWorkspace && (
-        <Box.FlexApart alignItems="flex-start">
-          <Settings.Section
-            title={<Box.Flex>Delete Workspace</Box.Flex>}
-            description={<>Delete the workspace, including all assistants. Proceed with caution.</>}
-            mb={0}
-            w={500}
-          />
-          <Button variant={Button.Variant.SECONDARY} onClick={() => boardDeleteModal.openVoid({ workspaceID: workspace.id })}>
-            Delete Workspace
-          </Button>
-        </Box.FlexApart>
-      )}
-    </Box>
+    <Page.Section
+      header={
+        !!workspace &&
+        canDeleteWorkspace && (
+          <Box.FlexApart fullWidth>
+            <Page.Section.Header>
+              <Page.Section.Title>Delete Workspace</Page.Section.Title>
+              <Page.Section.Description>Delete the workspace, including all assistants. Proceed with caution.</Page.Section.Description>
+            </Page.Section.Header>
+            <Button variant={Button.Variant.SECONDARY} onClick={() => boardDeleteModal.openVoid({ workspaceID: workspace.id })}>
+              Delete Workspace
+            </Button>
+          </Box.FlexApart>
+        )
+      }
+    />
   );
 };
 

e216e501d63105fd01f57d4bd5f14efb45258f3f
Matheus Poleza <matheus.poleza@gmail.com>
2023-04-18 16:59:04 +0000

    refactor: removed ml gateway integration ff (CT-1947) (#6917)
    **Fixes or implements CT-1947**
    
    ### Brief description. What is this change?
    - Removed ml gateway integration ff
    - If `NLUManager` ff is not enable does not connect to ml-gateway

--- libs/realtime-sdk/src/config/features.ts
+++ libs/realtime-sdk/src/config/features.ts
@@ -4,9 +4,6 @@ export enum FeatureFlag {
   ASR_BYPASS = 'asr_bypass',
   MOTOROLA_SSO = 'motorola_sso',
 
-  // permanent circuit breaker for ml-gateway integration
-  ML_GATEWAY_INTEGRATION = 'ml_gateway_integration',
-
   // permanent circuit breaker for API/integration access
   DISABLE_INTEGRATION = 'disable_integration',
 

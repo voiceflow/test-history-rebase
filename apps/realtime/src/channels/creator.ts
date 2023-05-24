c6f926d856fcf93e8bff8fad7ad71c562ff4480b
Tyler Han <tyler@voiceflow.com>
2023-05-24 20:21:10 +0000

    fix: replay creator fetch in order [bugfix] (CT-000) (#7037)
    This is a bit of a crazy race condition bug.
    
    so normally we have loading gates to ensure that one resource is loaded before the next one starts, such as [AccountSubscriptionGate](https://github.com/voiceflow/creator-app/blob/650eb57b4e81b11bc665b118ed28e3cce96e14da/apps/creator-app/src/gates/AccountSubscriptionGate.tsx#L8-L18)
    
    HOWEVER, when the user loses their connection temporarily (server restarts, etc) - there is no guarantee that one channel will finish loading before the next one.
    
    The bug we see with the KB disappearing is because we rely on the WorkspaceChannel subscription to inject attributes such as "settings.aiAssist" or "settings.kanban".
    
    On the creator gate, all settings are just blank: 
    https://github.com/voiceflow/creator-app/blob/92cfc07bc1b065a4be4ef692cb6ff2bcc4138d44/apps/realtime/src/services/workspace/index.ts#L94
    
    If you have a ton of workspaces with a ton of members, which a lot of VF members do, the creator channel subscription will resolve AFTER the workspace subscription, it means the `Realtime.workspaces.crud.patch` for the active workspace (with settings metadata) comes first, and then the `Realtime.workspaces.crud.replace` completely nukes all the data already there.
    
    to reproduce the bug/issue simply add some artificial latency to your identity service:
    ![image](https://github.com/voiceflow/creator-app/assets/5643574/6e448969-4363-4ee8-a6e0-a73fb84857d1)
    
    What is really important is that we can NOT assume that channels will replay in order unless we explicitly tag timestamps and metadata.
    
    This fix injects artificial metadata to the creator channel events so that logux will "replay" it and put it before the workspace channel events. `Realtime.workspaces.crud.replace`  is replayed before `Realtime.workspaces.crud.patch`
    
    Co-authored-by: Tyler Han <tylerhan97@gmail.com>

--- apps/realtime/src/channels/creator.ts
+++ apps/realtime/src/channels/creator.ts
@@ -12,12 +12,18 @@ class CreatorChannel extends AbstractChannelControl<Realtime.Channels.CreatorCha
   };
 
   protected load = async (ctx: ChannelContext<Realtime.Channels.CreatorChannelParams>): Promise<SendBackActions> => {
+    // the timestamp of Realtime.Channels.creator it is called, not when it is resolved
+    const [workspacesMeta, organizationsMeta] = [{ id: this.server.log.generateId() }, { id: this.server.log.generateId() }];
+
     const [workspaces, organizations] = await Promise.all([
       this.services.workspace.getAll(Number(ctx.userId)).then(Realtime.Adapters.workspaceAdapter.mapFromDB),
       this.services.organization.getAll(Number(ctx.userId)).then(Realtime.Adapters.Identity.organization.mapFromDB),
     ]);
 
-    return [Realtime.workspace.crud.replace({ values: workspaces }), Realtime.organization.crud.replace({ values: organizations })];
+    return [
+      [Realtime.workspace.crud.replace({ values: workspaces }), workspacesMeta],
+      [Realtime.organization.crud.replace({ values: organizations }), organizationsMeta],
+    ];
   };
 }
 

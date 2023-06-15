d97f979ff31958051f6da7078c96f085a82d41d8
Tyler Han <tylerhan97@gmail.com>
2023-06-15 00:31:52 -0700

    fix: disable speak steps on text projects [bugfix] (CT-000)

--- apps/creator-app/src/pages/Canvas/managers/Text/components/RootEditor.tsx
+++ apps/creator-app/src/pages/Canvas/managers/Text/components/RootEditor.tsx
@@ -1,8 +1,10 @@
+import * as Platform from '@voiceflow/platform-config';
 import * as Realtime from '@voiceflow/realtime-sdk';
-import { Box } from '@voiceflow/ui';
+import { Alert, Box, SectionV2 } from '@voiceflow/ui';
 import React from 'react';
 
 import * as GPT from '@/components/GPT';
+import { useActiveProjectType } from '@/hooks/platformConfig';
 import EditorV2 from '@/pages/Canvas/components/EditorV2';
 import { PromptsSection, PromptsSectionRef } from '@/pages/Canvas/managers/components';
 import useCanvasVisibilityOption from '@/pages/Canvas/managers/hooks/useCanvasVisibilityOption';
@@ -21,6 +23,20 @@ const Editor: React.FC = () => {
   });
 
   const canvasVisibilityOption = useCanvasVisibilityOption(editor.data.canvasVisibility, (canvasVisibility) => editor.onChange({ canvasVisibility }));
+
+  const projectType = useActiveProjectType();
+
+  if (projectType === Platform.Constants.ProjectType.VOICE) {
+    return (
+      <EditorV2 header={<EditorV2.DefaultHeader />}>
+        <SectionV2.SimpleSection>
+          <Alert>
+            This <b>text</b> step does not function properly on voice based projects. Use a <b>speak</b> step instead.
+          </Alert>
+        </SectionV2.SimpleSection>
+      </EditorV2>
+    );
+  }
 
   return (
     <EditorV2

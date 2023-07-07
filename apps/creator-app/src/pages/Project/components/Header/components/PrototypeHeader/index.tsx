935377af7fb60fe830de890b1b740ec146f3b173
Filipe Merker <filipesmerker@gmail.com>
2023-07-07 16:17:44 +0000

    fix: fix prototype segment (BUG-474) (#7087)
    This PR makes it so that if the users closes an ongoing test session it will fire the `trackProjectPrototypeEnd`.
    
    If the prototype tool is IDLE (didn't start) or has already ENDED, it will not fire.
    
    Co-authored-by: Filipe Merker <filipe.merker@voiceflow.com>

--- apps/creator-app/src/pages/Project/components/Header/components/PrototypeHeader/index.tsx
+++ apps/creator-app/src/pages/Project/components/Header/components/PrototypeHeader/index.tsx
@@ -2,15 +2,28 @@ import { Box } from '@voiceflow/ui';
 import React from 'react';
 
 import Page from '@/components/Page';
+import { PrototypeStatus } from '@/constants/prototype';
 import * as Router from '@/ducks/router';
-import { useDispatch } from '@/hooks';
+import { useDispatch, useTrackingEvents } from '@/hooks';
 import { Hotkey } from '@/keymap';
 import * as ModalsV2 from '@/ModalsV2';
+import { PrototypeContext } from '@/pages/Prototype/context';
 
 import { Content, Settings, Share, Timer } from './components';
 
 const PrototypeHeader: React.FC = () => {
   const goToCurrentCanvas = useDispatch(Router.goToCurrentCanvas);
+  const [trackingEvents] = useTrackingEvents();
+  const prototypeAPI = React.useContext(PrototypeContext);
+  const { state } = prototypeAPI;
+
+  const closePrototype = () => {
+    goToCurrentCanvas();
+
+    if (state.status === PrototypeStatus.ACTIVE) {
+      trackingEvents.trackProjectPrototypeEnd();
+    }
+  };
 
   const variableStateManageModal = ModalsV2.useModal(ModalsV2.VariableStates.Manage);
   const variableStateCreateModal = ModalsV2.useModal(ModalsV2.VariableStates.Create);
@@ -18,11 +31,11 @@ const PrototypeHeader: React.FC = () => {
   const disableEscHotkey = variableStateManageModal.opened || variableStateCreateModal.opened;
 
   return (
-    <Page.Header renderLogoButton={() => <Page.Header.BackButton onClick={() => goToCurrentCanvas()} />}>
+    <Page.Header renderLogoButton={() => <Page.Header.BackButton onClick={closePrototype} />}>
       <Page.Header.HotkeyToAction
         label="back to designer"
         hotkey={Hotkey.CLOSE_CANVAS_MODE}
-        onHotkey={goToCurrentCanvas}
+        onHotkey={closePrototype}
         disabled={disableEscHotkey}
         preventDefault
       />

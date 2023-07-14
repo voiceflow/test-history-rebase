0e3f945dc5923126decc0598698c0ed73f34ee35
Xavientois <34867186+Xavientois@users.noreply.github.com>
2023-07-14 13:17:47 -0400

    ci: fix old e2e

---
+++ apps/creator-app/currents.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  projectId: '75gu5n',
+  recordKey: 'xxx', // We are using sorry-cypress, so this can be any value
+  cloudServiceUrl: 'https://sorry-cypress-director.cm4-vf-dev-2-0-p0.voiceflow.com/',
+};

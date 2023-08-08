9b70783e446e4f7ce8deaf479897527bceb6cbea
Tyler Han <tyler@voiceflow.com>
2023-08-08 13:30:51 +0000

    fix: markdown serializer (CT-000) (#7115)
    read the actual serializer [here](https://github.com/voiceflow/slate-serializer/pull/11).
    
    This is the rationale for the change: https://www.notion.so/Response-Markdown-Formatting-ae59bbec18d04c538458ed26a24f53ee
    
    I have done some testing comparing the two different components (old serializer vs new one):
    ![image](https://github.com/voiceflow/creator-app/assets/5643574/371c0ccf-fbf5-4068-b23d-89fa1f71fa25)
    
    I think they are similar enough that the user would not notice.
    
    
    Co-authored-by: Tyler Han <tylerhan97@gmail.com>

--- apps/creator-app/src/pages/Prototype/components/PrototypeDialog/Message/variants/Text/index.tsx
+++ apps/creator-app/src/pages/Prototype/components/PrototypeDialog/Message/variants/Text/index.tsx
@@ -1,7 +1,9 @@
 import { BaseNode } from '@voiceflow/base-types';
+import { serializeToMarkdown } from '@voiceflow/slate-serializer/markdown';
+import Markdown from 'markdown-to-jsx';
 import React from 'react';
 
-import SlateEditable from '@/components/SlateEditable';
+import { styled } from '@/hocs/styled';
 
 import BaseMessage, { BaseMessageProps } from '../../Base';
 
@@ -10,14 +12,20 @@ interface TextProps extends Omit<BaseMessageProps, 'iconProps'> {
   ai?: boolean;
 }
 
-const Text: React.FC<TextProps> = ({ slate, ai, ...props }) => {
-  const content = React.useMemo(() => SlateEditable.serializeToJSX(slate.content), [slate.content]);
+const MarkdownWrapper = styled(Markdown)`
+  p: {
+    margin-bottom: 0;
+  }
+`;
+
+export const MarkdownText: React.FC<TextProps> = ({ slate, ai, ...props }) => {
+  const content = React.useMemo(() => serializeToMarkdown(slate.content) || '', [slate.content]);
 
   return (
     <BaseMessage {...props} isAiMessage={ai}>
-      {content}
+      <MarkdownWrapper>{content}</MarkdownWrapper>
     </BaseMessage>
   );
 };
 
-export default Text;
+export default MarkdownText;

4c5c5247c6c8f051344cc639964bcdffe08b7ac8
Vinicius Dacal <viniciusldacal@gmail.com>
2023-08-09 02:35:30 +0000

    feat: update templates (BUG-514) (#7112)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements BUG-514**
    
    ### Brief description. What is this change?
    
    Update templates

--- apps/creator-app/src/pages/DashboardV2/pages/ProjectList/components/constants.ts
+++ apps/creator-app/src/pages/DashboardV2/pages/ProjectList/components/constants.ts
@@ -1,9 +1,7 @@
 export const TRAVEL_ASSISTANT_IMAGE = 'https://cm4-production-assets.s3.amazonaws.com/1680619531160-168114-photos-travel-icon-free-hd-image.jpeg';
 export const SUPPORT_CHATBOT_IMAGE = 'https://cm4-production-assets.s3.amazonaws.com/1680619509819-vf-2.png';
 export const RETAIL_PURCHASES_IMAGE = 'https://cm4-production-assets.s3.amazonaws.com/1677254079988-shopping-cart-gef00a8b31_1920.png';
-export const PAYMENT_ACCOUNT_IMAGE = 'https://s3.amazonaws.com/com.getstoryflow.api.images/1631673640544-mobility-bank-og-logo.png';
 
-export const PAYMENT_ACCOUNT_TEMPLATE_TAG = 'paymentsAccount';
 export const RETAIL_PURCHASES_TEMPLATE_TAG = 'retailPurchases';
 export const SUPPORT_CHATBOT_TEMPLATE_TAG = 'supportChatbot';
 export const TRAVEL_ASSISTANT_TEMPLATE_TAG = 'travelAssistant';

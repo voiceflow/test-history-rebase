777ea6545f5b123215229abf17333b7cff13517b
Tyler Han <tyler@voiceflow.com>
2023-08-14 20:40:54 +0000

    fix: chunk limit (NLU-804) (#7127)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-XXX**
    
    ### Brief description. What is this change?
    ![Screenshot 2023-08-14 at 4 38 03 PM](https://github.com/voiceflow/creator-app/assets/5643574/f391b8c2-12e1-4da6-b4e3-9a3225933b16)
    
    Btw it's fine to modify `Input.Range` it gets used nowhere else, I double checked.
    
    Co-authored-by: Tyler Han <tylerhan97@gmail.com>

--- apps/creator-app/src/pages/KnowledgeBase/Settings/content.tsx
+++ apps/creator-app/src/pages/KnowledgeBase/Settings/content.tsx
@@ -1,4 +1,5 @@
 import { BaseModels } from '@voiceflow/base-types';
+import { Box, Input, SectionV2, TippyTooltip } from '@voiceflow/ui';
 import React from 'react';
 
 import PromptSettings from '@/pages/Canvas/managers/components/AI/components/PromptSettings';
@@ -9,7 +10,7 @@ export interface KnowledgeBaseSettingsProps {
 }
 
 const KnowledgeBaseSettings: React.FC<KnowledgeBaseSettingsProps> = ({ settings, setSettings }) => {
-  const { summarization } = settings;
+  const { summarization, search } = settings;
 
   const update = React.useCallback(
     <T extends keyof BaseModels.Project.KnowledgeBaseSettings>(property: T) =>
@@ -19,7 +20,28 @@ const KnowledgeBaseSettings: React.FC<KnowledgeBaseSettingsProps> = ({ settings,
     []
   );
 
-  return <PromptSettings data={summarization} onChange={update('summarization')} my={24} />;
+  return (
+    <>
+      <PromptSettings data={summarization} onChange={update('summarization')} my={24} />
+      <SectionV2.Divider />
+      <SectionV2.Content py={24}>
+        <Box.Flex gap={12} maxWidth={100}>
+          <Box.FlexColumn gap={11} alignItems="flex-start">
+            <TippyTooltip
+              delay={250}
+              placement="top-start"
+              content="Determines how many data source chunks (per document) will be passed to the LMM as context to generate a response.  Recommend 2-3 to avoid too much noise and content in prompt."
+            >
+              <SectionV2.Title secondary bold>
+                Chunk Limit
+              </SectionV2.Title>
+            </TippyTooltip>
+            <Input.Range defaultValue={3} min={1} max={10} value={search.limit} onChange={(limit) => update('search')({ limit })} />
+          </Box.FlexColumn>
+        </Box.Flex>
+      </SectionV2.Content>
+    </>
+  );
 };
 
 export default KnowledgeBaseSettings;
--- libs/ui/src/components/Input/components/RangeInput.tsx
+++ libs/ui/src/components/Input/components/RangeInput.tsx
@@ -1,54 +1,34 @@
-import Box from '@ui/components/Box';
 import { useLinkedState } from '@ui/hooks';
-import { ThemeColor } from '@ui/styles';
 import { withTargetValue } from '@ui/utils/dom';
 import React from 'react';
 
 import DefaultInput from './DefaultInput';
 
-const EditLength: React.FC<{ value: number; onChange: (value: number) => void; max: number; min: number; defaultValue: number }> = ({
-  min,
-  max,
-  value,
-  onChange,
-  defaultValue,
-}) => {
+type RangeInputProps = Omit<React.ComponentProps<typeof DefaultInput>, 'onChange' | 'onBlur' | 'value'> & {
+  value: number;
+  onChange: (value: number) => void;
+  max: number;
+  min: number;
+};
+
+const RangeInput: React.FC<RangeInputProps> = ({ min, max, value, onChange, ...props }) => {
   const [length, setLength] = useLinkedState<string>(String(value));
-  const [error, setError] = React.useState(false);
 
   const onSave = () => {
-    onChange((!error && Number(length)) || defaultValue);
+    let nextValue = Number(length) ?? min;
+
+    nextValue = Math.max(min, nextValue);
+    nextValue = Math.min(max, nextValue);
+
+    onChange(nextValue);
   };
 
   const onUpdate = (input: string) => {
     const newLength = input.replace(/\D/g, '').substring(0, 4);
     setLength(newLength);
-
-    const result = Number(newLength);
-    if (result < min || result > max) {
-      setError(true);
-    } else {
-      setError(false);
-    }
   };
 
-  return (
-    <>
-      <DefaultInput
-        placeholder={String(defaultValue)}
-        type="text"
-        value={length}
-        onChange={withTargetValue(onUpdate)}
-        onBlur={withTargetValue(onSave)}
-        error={error}
-      />
-      {error && (
-        <Box mt={11} fontSize={13} color={ThemeColor.RED}>
-          Value must be {min}-{max}
-        </Box>
-      )}
-    </>
-  );
+  return <DefaultInput {...props} type="text" value={length} onChange={withTargetValue(onUpdate)} onBlur={withTargetValue(onSave)} />;
 };
 
-export default EditLength;
+export default RangeInput;

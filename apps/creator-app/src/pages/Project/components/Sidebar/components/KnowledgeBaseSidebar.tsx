9ded2c6558f8290ee2c7f5a2cf17717a8f7bf270
Tyler Han <tyler@voiceflow.com>
2023-08-27 03:23:50 +0000

    fix: add kb api link (NLU-829) (#7147)
    Pretty simple
    ![Screenshot 2023-08-26 at 2 43 17 AM](https://github.com/voiceflow/creator-app/assets/5643574/60d991b9-ea72-4714-8e4f-8f792779e771)
    
    
    Co-authored-by: Tyler Han <tylerhan97@gmail.com>

--- apps/creator-app/src/pages/Project/components/Sidebar/components/KnowledgeBaseSidebar.tsx
+++ apps/creator-app/src/pages/Project/components/Sidebar/components/KnowledgeBaseSidebar.tsx
@@ -4,7 +4,7 @@ import { generatePath } from 'react-router-dom';
 
 import NavigationSidebar from '@/components/NavigationSidebar';
 import { Path } from '@/config/routes';
-import { LEARN_KNOWLEDGE_BASE } from '@/constants';
+import { KNOWLEDGE_BASE_API_DOCS, LEARN_KNOWLEDGE_BASE } from '@/constants';
 import * as Session from '@/ducks/session';
 import { useSelector } from '@/hooks';
 
@@ -22,6 +22,11 @@ const SettingsSidebar: React.FC = () => {
           <NavigationSidebar.NavItem icon="preview" to={generatePath(Path.PROJECT_KNOWLEDGE_BASE, { versionID })} title="Data Sources" />
           <System.Link.Anchor href={LEARN_KNOWLEDGE_BASE} color={System.Link.Color.INHERIT}>
             <NavigationSidebar.Item icon="noMatch" title="Docs">
+              <NavigationSidebar.Item.LinkIcon />
+            </NavigationSidebar.Item>
+          </System.Link.Anchor>
+          <System.Link.Anchor href={KNOWLEDGE_BASE_API_DOCS} color={System.Link.Color.INHERIT}>
+            <NavigationSidebar.Item icon="integrations" title="APIs">
               <NavigationSidebar.Item.LinkIcon />
             </NavigationSidebar.Item>
           </System.Link.Anchor>

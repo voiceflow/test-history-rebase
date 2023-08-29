76c12a6d33b73098fc6ed7c3ff21afdd637ffe71
Amanda <22350614+amandasteinhauer@users.noreply.github.com>
2023-08-29 18:16:17 +0000

    fix: intent confidence score (CV3-330) (#7155)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements CV3-330**
    
    ### Brief description. What is this change?
    
    show score from 0-100 instead of 0-1 in slider text
    
    Co-authored-by: Tyler Han <tylerhan97@gmail.com>

--- apps/creator-app/src/pages/Settings/components/GeneralSettings/Sections/GlobalLogic/components/IntentConfidence.tsx
+++ apps/creator-app/src/pages/Settings/components/GeneralSettings/Sections/GlobalLogic/components/IntentConfidence.tsx
@@ -9,12 +9,17 @@ import { useDispatch, useSelector } from '@/hooks';
 
 const IntentConfidence: React.FC = () => {
   const patchSettings = useDispatch(Version.patchSettings);
-  const intentConfidence = useSelector(VersionV2.active.intentConfidenceSelector) ?? 0.6;
+  const intentConfidence = (useSelector(VersionV2.active.intentConfidenceSelector) ?? 0.6) * 100;
+
+  const patchIntentConfidence = React.useCallback(
+    (confidence: number) => patchSettings({ intentConfidence: Number((confidence / 100).toFixed(2)) }),
+    []
+  );
 
   return (
     <Settings.SubSection header="Intent Confidence" splitView>
       <SliderInputGroup
-        sliderProps={{ min: 0, max: 1, step: 0.01 }}
+        sliderProps={{ min: 0, max: 100, step: 1 }}
         inputWidth={140}
         inputProps={{
           rightAction: (
@@ -24,7 +29,7 @@ const IntentConfidence: React.FC = () => {
           ),
         }}
         value={intentConfidence}
-        onChange={(confidence) => patchSettings({ intentConfidence: confidence })}
+        onChange={patchIntentConfidence}
       />
       <Settings.SubSection.Description>
         The threshold of confidence to classify an intent instead of <b>Fallback Intent</b>.

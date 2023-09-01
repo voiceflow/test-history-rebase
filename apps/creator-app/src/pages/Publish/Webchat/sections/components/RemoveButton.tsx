e73af9352484b15b452b197a3908c728f94515e5
Amanda <22350614+amandasteinhauer@users.noreply.github.com>
2023-09-01 14:21:42 +0000

    fix: remove webchat config images (CV3-338) (#7161)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements CV3-338**
    
    ### Brief description. What is this change?
    
    Add ability to remove the avatar,  image and launcher images in the webchat appearance section. 
    For the avatar and image images, replace with the default voiceflow image on removal. 
    
    
    <img width="361" alt="Screenshot 2023-09-01 at 8 49 37 AM" src="https://github.com/voiceflow/creator-app/assets/22350614/fb334dcd-38f5-4626-b257-61b56ab950eb">

---
+++ apps/creator-app/src/pages/Publish/Webchat/sections/components/RemoveButton.tsx
@@ -0,0 +1,12 @@
+import { SvgIcon } from '@voiceflow/ui';
+import React from 'react';
+
+import * as S from './styles';
+
+export const RemoveButton: React.FC<{ top?: number; right?: number; onClick: () => Promise<void> }> = ({ top, right, onClick }) => (
+  <S.RemoveButton top={top} right={right} onClick={onClick}>
+    <SvgIcon size={8} icon="close" color="#8da2b5" />
+  </S.RemoveButton>
+);
+
+export default RemoveButton;
--- apps/creator-app/src/pages/Publish/Webchat/sections/components/styles.ts
+++ apps/creator-app/src/pages/Publish/Webchat/sections/components/styles.ts
@@ -36,3 +36,17 @@ export const Header = styled.div`
     opacity: 0.85;
   }
 `;
+
+export const RemoveButton = styled(Box.FlexCenter)<{ top?: number; right?: number }>`
+  cursor: pointer;
+  position: absolute;
+
+  top: ${({ top = 10 }) => `${top}px`};
+  right: ${({ right = 10 }) => `${right}px`};
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  background: white;
+  box-shadow: 0px 2px 4px rgba(17, 49, 96, 0.16), 0px 0px 0px rgba(17, 49, 96, 0.04);
+  z-index: 10;
+`;

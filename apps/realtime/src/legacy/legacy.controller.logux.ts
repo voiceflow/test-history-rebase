903a1b35f956ba804887bf02ab6e938a2b8ce43d
Matheus Poleza <matheus.poleza@voiceflow.com>
2023-09-22 18:40:49 -0300

    fix: adds use request context decorator (CT-000)

--- apps/realtime/src/legacy/legacy.controller.logux.ts
+++ apps/realtime/src/legacy/legacy.controller.logux.ts
@@ -1,6 +1,8 @@
-// import { UseRequestContext } from '@mikro-orm/core';
+import { MikroORM, UseRequestContext } from '@mikro-orm/core';
+import { getMikroORMToken } from '@mikro-orm/nestjs';
 import { Controller, Inject } from '@nestjs/common';
 import { Action } from '@voiceflow/nestjs-logux';
+import { DatabaseTarget } from '@voiceflow/orm-designer';
 import * as Realtime from '@voiceflow/realtime-sdk/backend';
 
 // import { UserID } from '@voiceflow/sdk-auth/nestjs';
@@ -8,13 +10,16 @@ import { MigrationService } from './migration.service';
 
 @Controller()
 export class LegacyLoguxController {
-  constructor(@Inject(MigrationService) private readonly migrationService: MigrationService) {
+  constructor(
+    @Inject(getMikroORMToken(DatabaseTarget.POSTGRES)) private readonly orm: MikroORM,
+    @Inject(MigrationService) private readonly migrationService: MigrationService
+  ) {
     // eslint-disable-next-line no-console
     console.log('LEGACY LOGUX CONTROLLER CONSTRUCTOR');
   }
 
   @Action(Realtime.version.schema.negotiate.started)
-  // @UseRequestContext()
+  @UseRequestContext()
   public async negotiate() {
     // eslint-disable-next-line no-console
     console.log('NEGOTIATE NEW ACTION');

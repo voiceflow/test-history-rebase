cdda16ea6904ff5bf74230580bfe5f4163cdd037
Matheus Poleza <matheus.poleza@voiceflow.com>
2023-09-22 18:44:10 -0300

    feat: adds payload to legacy controller

--- apps/realtime/src/legacy/legacy.controller.logux.ts
+++ apps/realtime/src/legacy/legacy.controller.logux.ts
@@ -1,7 +1,7 @@
 import { MikroORM, UseRequestContext } from '@mikro-orm/core';
 import { getMikroORMToken } from '@mikro-orm/nestjs';
 import { Controller, Inject } from '@nestjs/common';
-import { Action } from '@voiceflow/nestjs-logux';
+import { Action, Payload } from '@voiceflow/nestjs-logux';
 import { DatabaseTarget } from '@voiceflow/orm-designer';
 import * as Realtime from '@voiceflow/realtime-sdk/backend';
 
@@ -13,16 +13,13 @@ export class LegacyLoguxController {
   constructor(
     @Inject(getMikroORMToken(DatabaseTarget.POSTGRES)) private readonly orm: MikroORM,
     @Inject(MigrationService) private readonly migrationService: MigrationService
-  ) {
-    // eslint-disable-next-line no-console
-    console.log('LEGACY LOGUX CONTROLLER CONSTRUCTOR');
-  }
+  ) {}
 
   @Action(Realtime.version.schema.negotiate.started)
   @UseRequestContext()
-  public async negotiate() {
+  public async negotiate(@Payload() payload: any) {
     // eslint-disable-next-line no-console
-    console.log('NEGOTIATE NEW ACTION');
+    console.log('NEGOTIATE NEW ACTION', { payload });
     return this.migrationService.migrate({ payload: { creatorID: 1, versionID: '23321231', proposedSchemaVersion: 1 }, ctx: {} as any });
   }
 }

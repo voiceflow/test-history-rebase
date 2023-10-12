22637e870338de20ff20674ccaa189dbd5973e0c
Tyler Han <tyler@voiceflow.com>
2023-10-12 14:55:59 +0000

    fix: diagram history composite reducer [bugfix] (BUG-610) (#7251)
    this was initially broken by:
    https://github.com/voiceflow/creator-app/issues/7157
    
    when we merged `creator` and `creatorV2`.
    
    the problem with `composite-reducer` is the root reducer ALWAYS runs first:
    https://github.com/voiceflow/composite-reducer/blob/bf79c911ee87c841f329c36f5b06e9c5835710e2/src/index.ts#L28
    
    This means in the initializeReducer's nextState:
    https://github.com/voiceflow/creator-app/blob/ba01c0970a4cf980e8ef942f6d5bb22812dc7ca5/apps/creator-app/src/ducks/creatorV2/reducers/initialize.ts#L7-L15
    
    It would just delete the `DiagramsHistory.DIAGRAMS_HISTORY_STATE_KEY` every single time the `Realtime.creator.initialize` call comes in.
    
    We could update `composite-reducer` so that sub-reducers manage their own state independently, but I'm worried about other side effects there.
    
    Co-authored-by: Tyler Han <tylerhan97@gmail.com>

--- apps/creator-app/src/ducks/creatorV2/reducers/initialize.ts
+++ apps/creator-app/src/ducks/creatorV2/reducers/initialize.ts
@@ -1,11 +1,11 @@
 import * as Realtime from '@voiceflow/realtime-sdk';
 
-import { createInitialState } from '../constants';
+import { createInitializeState } from '../constants';
 import { importSnapshot } from './importSnapshot';
 import { createReducer } from './utils';
 
-const initializeReducer = createReducer(Realtime.creator.initialize, (_, { diagramID, ...entities }) => {
-  const nextState = createInitialState();
+const initializeReducer = createReducer(Realtime.creator.initialize, (state, { diagramID, ...entities }) => {
+  const nextState = createInitializeState(state);
 
   nextState.activeDiagramID = diagramID;
 

81dbd08ea20fff2b3ff36f2913959a999bf25ff2
Xavientois <34867186+Xavientois@users.noreply.github.com>
2023-10-27 14:52:03 +0000

    ci: point to new sorry-cypress instance (IN-917) (#7301)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements IN-917**
    
    ### Brief description. What is this change?
    
    Point to new sorry cypress instance in new cluster

--- apps/creator-app/currents.config.js
+++ apps/creator-app/currents.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   projectId: '75gu5n',
   recordKey: 'xxx', // We are using sorry-cypress, so this can be any value
-  cloudServiceUrl: 'https://sorry-cypress-director.cm4-vf-dev-2-0-p0.voiceflow.com/',
+  cloudServiceUrl: 'https://sorry-cypress-director.cm4-vf-dev-2-0-p1.voiceflow.com/',
 };

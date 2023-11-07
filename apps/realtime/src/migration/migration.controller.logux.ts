330c847926303f935267cb03f41504edf6f0fcba
Tyler Han <tyler@voiceflow.com>
2023-11-07 13:13:21 +0000

    fix: project permissions for migration (CT-000) (#7335)
    There are no longer any more version level permissions. (Users don't have special or individual permissions specifically for versions, you can be a project editor or whatever, but not a version editor).
    
    https://github.com/voiceflow/platform/pull/791
    
    You can put in a version resource, and let it resolve up to the project permissions.
    
    Co-authored-by: Tyler Han <tylerhan97@gmail.com>

--- apps/realtime/src/migration/migration.controller.logux.ts
+++ apps/realtime/src/migration/migration.controller.logux.ts
@@ -17,7 +17,7 @@ export class MigrationLoguxController {
     @Inject(SchemaService) private readonly schemaService: SchemaService
   ) {}
 
-  @Authorize.Permissions<Realtime.version.schema.NegotiatePayload>([Permission.VERSION_PLATFORM_DATA_UPDATE], ({ versionID }) => [
+  @Authorize.Permissions<Realtime.version.schema.NegotiatePayload>([Permission.PROJECT_UPDATE], ({ versionID }) => [
     {
       id: versionID,
       kind: 'version',

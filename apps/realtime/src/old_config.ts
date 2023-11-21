95aa42e95366360b8212d43a6ee4bb8e1fc22de6
Tyler Han <tylerhan97@gmail.com>
2023-11-21 11:57:01 -0500

    fix: force realtime deploy [bugfix] (CT-000)

--- apps/realtime/src/old_config.ts
+++ apps/realtime/src/old_config.ts
@@ -5,7 +5,7 @@ dotenv.config({ path: '.env.local' });
 setupEnv();
 
 const CONFIG = {
-  // Feature flags
+  // feature flags
   FEATURE_OVERRIDES: Object.fromEntries(
     Object.entries(process.env).flatMap(([key, value]) => (key.startsWith('FF_') ? [[key.substring(3).toLowerCase(), value === 'true']] : []))
   ),

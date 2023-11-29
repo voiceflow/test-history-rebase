7be2d242d306c3f7dad001c801be2017b2b11c53
Yauheni <overhawlin@gmail.com>
2023-11-29 14:51:59 +0100

    fix: rebase

--- apps/realtime/src/prototype-program/prototype-program.service.ts
+++ apps/realtime/src/prototype-program/prototype-program.service.ts
@@ -1,9 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { PrototypeProgramEntity, PrototypeProgramORM } from '@voiceflow/orm-designer';
+import { PrototypeProgramORM } from '@voiceflow/orm-designer';
 
 import { MutableService } from '@/common';
-
-import { UpsertManyPrototypeProgramRequest } from './dtos/upsert-many-prototype-program-request.dto';
 
 @Injectable()
 export class PrototypeProgramService extends MutableService<PrototypeProgramORM> {
@@ -12,12 +10,6 @@ export class PrototypeProgramService extends MutableService<PrototypeProgramORM>
     protected readonly orm: PrototypeProgramORM
   ) {
     super();
-  }
-
-  async upsertMany(programs: UpsertManyPrototypeProgramRequest) {
-    const programsEntities = programs.map((program) => new PrototypeProgramEntity(program));
-
-    return this.orm.upsertMany(programsEntities);
   }
 
   async deleteOlderThanForVersion(versionID: string, date: Date) {
--- libs/orm-designer/src/mongo/prototype-program/prototype-program.orm.ts
+++ libs/orm-designer/src/mongo/prototype-program/prototype-program.orm.ts
@@ -4,10 +4,6 @@ import { MongoAtomicORM } from '../common';
 import { PrototypeProgramEntity } from './prototype-program.entity';
 
 export class PrototypeProgramORM extends MongoAtomicORM(PrototypeProgramEntity) {
-  async upsertMany(programs: PrototypeProgramEntity[]) {
-    return this.em.upsertMany(PrototypeProgramEntity, programs);
-  }
-
   async deleteOlderThanForVersion(versionID: string, date: Date) {
     this.em.nativeDelete(PrototypeProgramEntity, { versionID: new ObjectId(versionID), updatedAt: { $lt: date } });
   }

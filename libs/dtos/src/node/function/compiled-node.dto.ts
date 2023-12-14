16d92702f3095ba3d9d96310b5850f3a710b0de8
Brennan Lu <brennan.lu314@gmail.com>
2023-12-14 09:42:45 -0500

    Revert "feat: changed compiled function node to contain s3 link instead of actual code string (CV3-728) (#7443)"
    This reverts commit f23b48d131b99d332066f772b1e64217e05689b6.

--- libs/dtos/src/node/function/compiled-node.dto.ts
+++ libs/dtos/src/node/function/compiled-node.dto.ts
@@ -5,50 +5,35 @@ import { BaseCompiledNodeDTO } from '../base/compiled-node.dto';
 import { NodeType } from '../node-type.enum';
 import { FunctionVariableType } from './function-variable-type.enum';
 
-export const FunctionCompiledVariableDeclarationDTO = z.object({
+export const FunctionCompiledVariableConfigDTO = z.object({
   type: z
-    .nativeEnum(FunctionVariableType)
-    .refine((val) => val === FunctionVariableType.STRING, {
-      message: `Function variables currently only support the 'string' type`,
-    })
+    .literal(FunctionVariableType.STRING)
     .describe('The type of the Function variable. Used to render suitable UI and perform data validation.'),
 });
 
-export type FunctionCompiledVariableDeclaration = z.infer<typeof FunctionCompiledVariableDeclarationDTO>;
-
-export const FunctionCompiledDefinitionDTO = z.object({
-  codeId: z.string().describe("Amazon S3 bucket key of the .js file containing this Function's code"),
-  inputVars: z
-    .record(FunctionCompiledVariableDeclarationDTO)
-    .describe('Mapping of input variable name to its variable declaration.'),
-  outputVars: z
-    .record(FunctionCompiledVariableDeclarationDTO)
-    .describe('Mapping of output variable name to its variable declaration.'),
-  pathCodes: z.array(z.string()).describe('List of valid return codes for a function'),
-});
-
-export type FunctionCompiledDefinition = z.infer<typeof FunctionCompiledDefinitionDTO>;
-
-export const FunctionCompiledInvocationDTO = z.object({
-  inputVars: z.record(z.string()).describe('Mapping of input variable name to its argument value'),
-  outputVars: z
-    .record(z.string().nullable())
-    .describe('Mapping of output variable name to its assignment target, a canvas variable'),
-  paths: z.record(z.string()).describe('Mapping of path code to next step id'),
-});
-
-export type FunctionCompiledInvocation = z.infer<typeof FunctionCompiledInvocationDTO>;
+export type FunctionCompiledVariableConfig = z.infer<typeof FunctionCompiledVariableConfigDTO>;
 
 export const FunctionCompiledDataDTO = z.object({
-  definition: FunctionCompiledDefinitionDTO,
-  invocation: FunctionCompiledInvocationDTO,
+  code: z.string().describe('The code of the Function step'),
+  inputVars: z
+    .record(FunctionCompiledVariableConfigDTO)
+    .describe('Mapping of input variable name to its configuration.'),
+  outputVars: z
+    .record(FunctionCompiledVariableConfigDTO)
+    .describe('Mapping of output variable name to its configuration.'),
+  pathCodes: z.array(z.string()).describe('List of valid return codes for a function'),
 });
 
 export type FunctionCompiledData = z.infer<typeof FunctionCompiledDataDTO>;
 
 export const FunctionCompiledNodeDTO = BaseCompiledNodeDTO.extend({
   type: z.literal(NodeType.FUNCTION),
-  data: FunctionCompiledDataDTO,
+  data: z.object({
+    functionDefinition: FunctionCompiledDataDTO,
+    inputMapping: z.record(z.string()),
+    outputMapping: z.record(z.string().nullable()),
+    paths: z.record(z.string()),
+  }),
 });
 
 export type FunctionCompiledNode = InferCompiledNode<typeof FunctionCompiledNodeDTO>;

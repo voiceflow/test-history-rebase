f2596019bb8dea70e74a9438ea4096d4a569c436
Matheus Poleza <matheus.poleza@voiceflow.com>
2023-12-21 10:48:55 -0300

    refactor: deletes nlu docs (CT-000)

--- apps/creator-app/src/contexts/NLUContext/hooks.ts
+++ apps/creator-app/src/contexts/NLUContext/hooks.ts
@@ -1,14 +1,7 @@
-import { MenuTypes, toast, usePersistFunction } from '@voiceflow/ui';
+import { MenuTypes, usePersistFunction } from '@voiceflow/ui';
 import React from 'react';
 
-import * as NLP from '@/config/nlp';
 import { InteractionModelTabType } from '@/constants';
-import { Permission } from '@/constants/permissions';
-import * as Export from '@/ducks/export';
-import * as Tracking from '@/ducks/tracking';
-import { useUpgradeModal } from '@/hooks/modal.hook';
-import { usePermissionAction } from '@/hooks/permission';
-import { useDispatch } from '@/hooks/realtime';
 
 import { NLUContext } from './Context';
 
@@ -27,44 +20,10 @@ interface useNLUItemMenuProps {
 
 export const useNLUItemMenu = ({ itemID, itemType, isBuiltIn, onRename: onRenameProp, onDelete: onDeleteProp }: useNLUItemMenuProps) => {
   const nlu = React.useContext(NLUContext);
-  const upgradeModal = useUpgradeModal();
-
-  const [exporting, setIsExporting] = React.useState(false);
-
-  const exportModel = useDispatch(Export.exportModel);
+  const [exporting] = React.useState(false);
 
   const onRename = usePersistFunction(onRenameProp);
   const onDelete = usePersistFunction(onDeleteProp);
-
-  const onExportModel = usePersistFunction(async (nlpType: NLP.Constants.NLPType) => {
-    if (!itemID) return;
-
-    setIsExporting(true);
-
-    toast.info('Exporting...');
-
-    await exportModel({ origin: Tracking.ModelExportOriginType.SHARE_MENU, nlpType, intents: [itemID] });
-
-    toast.success('Successfully Exported');
-
-    setIsExporting(false);
-  });
-
-  const onExportAll = usePersistFunction(
-    usePermissionAction(Permission.NLU_EXPORT_ALL, {
-      onAction: onExportModel,
-
-      onPlanForbid: ({ args: [nlpType], planConfig }) => upgradeModal.openVoid(planConfig.upgradeModal({ nlpType })),
-    })
-  );
-
-  const onExportCSV = usePersistFunction(
-    usePermissionAction(Permission.NLU_EXPORT_CSV, {
-      onAction: () => onExportModel(NLP.Constants.NLPType.VOICEFLOW),
-
-      onPlanForbid: ({ planConfig }) => upgradeModal.openVoid(planConfig.upgradeModal()),
-    })
-  );
 
   const canDelete = !!itemID && nlu.canDeleteItem(itemID, itemType);
   const canRename = !!itemID && !!onRenameProp && nlu.canRenameItem(itemID, itemType) && itemType !== InteractionModelTabType.VARIABLES;
@@ -97,7 +56,7 @@ export const useNLUItemMenu = ({ itemID, itemType, isBuiltIn, onRename: onRename
       });
     }
     return options;
-  }, [itemID, itemType, onRename, isBuiltIn, canDelete, canRename, nlu.deleteItem, onExportCSV, onExportAll]);
+  }, [itemID, itemType, onRename, isBuiltIn, canDelete, canRename, nlu.deleteItem]);
 
   return {
     options: memoizedOptions,

2b96d37cb5237fc4369a2bed245a7d50fedf066a
Matheus Poleza <matheus.poleza@gmail.com>
2023-12-23 13:04:20 +0000

    fix: allow project api to call assistant export endpoint (BUG-681) (#7543)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary
    
    
    
    Co-authored-by: Matheus Poleza <matheus.poleza@voiceflow.com>

--- apps/realtime/src/assistant/assistant-private.http.controller.ts
+++ apps/realtime/src/assistant/assistant-private.http.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, HttpStatus, Inject, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
-import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
-import { ZodApiBody, ZodApiResponse } from '@voiceflow/nestjs-common';
+import { Body, Controller, Get, Headers, HttpStatus, Inject, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
+import { ApiHeader, ApiOperation, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ZodApiBody, ZodApiQuery, ZodApiResponse } from '@voiceflow/nestjs-common';
+import { Identity, Permission } from '@voiceflow/sdk-auth';
+import { Authorize, Principal } from '@voiceflow/sdk-auth/nestjs';
+import type { Request } from 'express';
 import { ZodValidationPipe } from 'nestjs-zod';
 
 import { HashedWorkspaceIDPayloadPipe, HashedWorkspaceIDPayloadType } from '@/common';
 import { ProjectSerializer } from '@/project/project.serializer';
+import { VersionIDAlias } from '@/version/version.constant';
 
 import { AssistantSerializer } from './assistant.serializer';
 import { AssistantService } from './assistant.service';
 import { AssistantPublishService } from './assistant-publish.service';
 import { AssistantCreateFixtureRequest } from './dtos/assistant-create-fixture.request';
+import { AssistantExportImportDataDTO } from './dtos/assistant-export-import-data.dto';
+import { AssistantExportJSONQuery } from './dtos/assistant-export-json.query';
 import { AssistantImportJSONResponse } from './dtos/assistant-import-json.response';
 import { AssistantPublishResponse } from './dtos/assistant-publish.response';
 
@@ -71,5 +77,30 @@ export class AssistantPrivateHTTPController {
     return {
       project: this.projectSerializer.serialize(project),
     };
+  }
+
+  @Get('export-json/:environmentID')
+  @Authorize.Permissions<Request<{ environmentID: string }>>([Permission.PROJECT_READ], (request) => ({
+    id: request.params.environmentID,
+    kind: 'version',
+  }))
+  @ApiParam({
+    name: 'environmentID',
+    type: 'string',
+    description: `Accepts environmentID or environment id alias (${Object.values(VersionIDAlias).join(', ')})`,
+  })
+  @ApiHeader({
+    name: 'assistantID',
+    schema: { type: 'string', description: 'Required if environment id alias is used' },
+  })
+  @ZodApiQuery({ schema: AssistantExportJSONQuery })
+  @ZodApiResponse({ status: HttpStatus.CREATED, schema: AssistantExportImportDataDTO })
+  exportJSON(
+    @Principal() principal: Identity & { createdBy: number },
+    @Param('environmentID') environmentID: string,
+    @Headers('assistantID') assistantID: string | undefined,
+    @Query(AssistantExportJSONQuery) query: AssistantExportJSONQuery
+  ): Promise<AssistantExportImportDataDTO> {
+    return this.service.exportJSON({ ...query, userID: principal.createdBy, assistantID, environmentID });
   }
 }

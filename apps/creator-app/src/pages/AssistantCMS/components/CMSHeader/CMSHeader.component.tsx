10b36879ab4458a263ac635902d85df39a2a8ddd
Amanda <22350614+amandasteinhauer@users.noreply.github.com>
2024-01-06 01:08:11 +0000

    fix: kb cms review fixes (CV3-788) (#7594)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements CV3-788**
    
    ### Brief description. What is this change?
    
    - toast with spinner
    - disabled collapsible 
    - "Never" default refresh rate
    - close Sitemap modal on Esc
    - CMS page loader header height

--- apps/creator-app/src/pages/AssistantCMS/components/CMSHeader/CMSHeader.component.tsx
+++ apps/creator-app/src/pages/AssistantCMS/components/CMSHeader/CMSHeader.component.tsx
@@ -1,13 +1,14 @@
 import { Header } from '@voiceflow/ui-next';
 import React from 'react';
 
+import { headerStyles } from './CMSHeader.css';
 import type { ICMSHeader } from './CMSHeader.interface';
 import { CMSHeaderMembers } from './CMSHeaderMembers/CMSHeaderMembers.component';
 import { CMSHeaderSearch } from './CMSHeaderSearch/CMSHeaderSearch.component';
 import { CMSHeaderShare } from './CMSHeaderShare/CMSHeaderShare.component';
 
 export const CMSHeader: React.FC<ICMSHeader> = ({ rightActions, searchPlaceholder, hideShare, hideMembers }) => (
-  <Header variant="search">
+  <Header variant="search" className={headerStyles}>
     <Header.Section.Left>
       <CMSHeaderSearch placeholder={searchPlaceholder} />
     </Header.Section.Left>
--- apps/creator-app/src/pages/AssistantCMS/pages/CMSKnowledgeBase/components/CMSKnowledgeBaseTable/CMSKnowledgeBaseTableStatusCell/CMSKnowledgeBaseTableStatusCell.css.ts
+++
@@ -1,9 +0,0 @@
-import { style } from '@voiceflow/style';
-import { Tokens } from '@voiceflow/ui-next/styles';
-
-export const errorCaptionTextStyle = style({
-  color: Tokens.colors.accent.accent200,
-  display: 'inline',
-  paddingLeft: 4,
-  cursor: 'pointer',
-});

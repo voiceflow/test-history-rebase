2cf0fc311ee425524258c2b9959d1ee205dccd33
Matheus Poleza <matheus.poleza@gmail.com>
2024-01-09 16:40:00 +0000

    fix: functions polish (CT-000) [bugfix] (#7617)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-XXX**
    
    ### Brief description. What is this change?
    fixed: https://www.notion.so/voiceflow/Functions-a8de9eaffb7f4c88a5e09c3b023a3520?p=3dc3ec5f623b4f80abbb2f6dffe7de21&pm=s and https://www.notion.so/voiceflow/Functions-a8de9eaffb7f4c88a5e09c3b023a3520?p=688c4e4f722244e198d8c027055af487&pm=s
    
    Co-authored-by: Matheus Poleza <matheus.poleza@voiceflow.com>

--- apps/creator-app/src/pages/AssistantCMS/pages/CMSFunction/components/CMSFunctionTableNavigation/CMSFunctionTableNavigation.component.tsx
+++ apps/creator-app/src/pages/AssistantCMS/pages/CMSFunction/components/CMSFunctionTableNavigation/CMSFunctionTableNavigation.component.tsx
@@ -1,30 +1,49 @@
+import { Box } from '@voiceflow/ui-next';
 import React from 'react';
+import { useHistory } from 'react-router';
 
 import { Designer } from '@/ducks';
+import { useGetAtomValue } from '@/hooks/atom.hook';
 import { useDispatch, useSelector } from '@/hooks/store.hook';
 import * as ModalsV2 from '@/ModalsV2';
 import { CMSTableNavigation } from '@/pages/AssistantCMS/components/CMSTableNavigation/CMSTableNavigation.component';
+import { useCMSManager } from '@/pages/AssistantCMS/contexts/CMSManager';
+import { useCMSRouteFolders } from '@/pages/AssistantCMS/contexts/CMSRouteFolders';
 
 import { CMSResourceActions } from '../../../../components/CMSResourceActions';
 
 export const CMSFunctionTableNavigation: React.FC = () => {
   const filesModal = ModalsV2.useModal(ModalsV2.Function.Import);
   const count = useSelector(Designer.Function.selectors.count);
+  const getAtomValue = useGetAtomValue();
+  const routeFolders = useCMSRouteFolders();
+  const cmsManager = useCMSManager();
+  const navigate = useHistory();
+  const getFolderPath = () => getAtomValue(routeFolders.activeFolderURL) ?? getAtomValue(cmsManager.url);
+  const label = `All functions (${count})`;
 
   const importMany = useDispatch(Designer.Function.effect.importMany);
 
   const onImportClickHandler = () => filesModal.open({ onSave: ([file]: File[]) => importMany(file) });
 
   return (
-    <CMSTableNavigation
-      label={`All functions (${count})`}
-      actions={
-        <>
-          <CMSResourceActions.Export />
-          <CMSResourceActions.Delete />
-        </>
-      }
-      onImportClick={onImportClickHandler}
-    />
+    <Box
+      onClick={(event) => {
+        if ((event.target as HTMLElement).textContent === label) return;
+        event.stopPropagation();
+      }}
+    >
+      <CMSTableNavigation
+        label={label}
+        actions={
+          <>
+            <CMSResourceActions.Export />
+            <CMSResourceActions.Delete />
+          </>
+        }
+        onImportClick={onImportClickHandler}
+        onLabelClick={() => navigate.push(getFolderPath())}
+      />
+    </Box>
   );
 };

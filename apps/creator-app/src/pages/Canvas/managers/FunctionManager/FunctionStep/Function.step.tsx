53b1bbea6b88846f4a1956dc8cd2247f9d1ab95c
Filipe Merker <filipe.merker@voiceflow.com>
2024-01-11 14:35:18 -0300

    feat: fix step label

--- apps/creator-app/src/pages/Canvas/managers/FunctionManager/FunctionStep/Function.step.tsx
+++ apps/creator-app/src/pages/Canvas/managers/FunctionManager/FunctionStep/Function.step.tsx
@@ -15,7 +15,7 @@ export const FunctionStep: ConnectedStep<Realtime.NodeData.Function> = ({ data, 
   const functionPathMap = React.useContext(FunctionPathMapContext)!;
   const functionPathByFunctionID = useMemoizedPropertyFilter(Object.values(functionPathMap), { functionID: data.functionID! });
   const { functionID } = data;
-  const { name, image } = getItemFromMap(functionMap, functionID);
+  const { name, image, description } = getItemFromMap(functionMap, functionID);
 
   const nextPortID = ports.out.builtIn[BaseModels.PortType.NEXT];
 
@@ -41,15 +41,16 @@ export const FunctionStep: ConnectedStep<Realtime.NodeData.Function> = ({ data, 
           portID={isLast && !hasPaths ? nextPortID : null}
           icon={functionID ? undefined : 'systemCode'}
           palette={palette}
-          label={name}
+          title={name}
+          label={description}
         />
       </Step.Section>
 
       {withPorts && hasPaths && (
-        <Step.Section>
-          {paths.map((path) => {
-            return <Step.Item key={path.portID} label={path.label} placeholder="Enter path name" portID={path.portID} multilineLabel />;
-          })}
+        <Step.Section v2>
+          {paths.map((path) => (
+            <Step.Item v2 key={path.portID} label={path.label} placeholder="Enter path name" portID={path.portID} multilineLabel />
+          ))}
         </Step.Section>
       )}
     </Step>

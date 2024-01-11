f84f8782fe30030fbfefdcd0ee09b789641a3715
Amanda <22350614+amandasteinhauer@users.noreply.github.com>
2024-01-11 19:57:15 +0000

    fix: kb ui fixes (CV3-845) (#7633)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements CV3-845**
    
    ### Brief description. What is this change?
    
    - fix kb preview settings textarea maxHeight (just needed to add in height of instructions)
    - fix width of RefreshRate button in kb table navigation so the delete button appears in the right place

--- apps/creator-app/src/ModalsV2/modals/KnowledgeBase/KnowledgeBasePreviewQuestion/KBPreviewSettings.css.ts
+++ apps/creator-app/src/ModalsV2/modals/KnowledgeBase/KnowledgeBasePreviewQuestion/KBPreviewSettings.css.ts
@@ -9,5 +9,5 @@ export const popperStyles = style({
 
 export const textareaStyles = style({
   // TODO: fix growing text area in modals DX-709
-  maxHeight: 'calc(100vh - (32px * 2) - 41px - 84px - 81px - 81px - 36px - 24px)',
+  maxHeight: 'calc(100vh - (32px * 2) - 41px - 84px - 81px - 81px - 84px - 36px - 24px)',
 });

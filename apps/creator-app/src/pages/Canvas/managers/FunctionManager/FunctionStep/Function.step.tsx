060b60992c13a64ca71e0129b3410eafe36f5fc5
Filipe Merker <filipe.merker@voiceflow.com>
2024-01-16 10:14:11 -0300

    feat: minor fix (#7668)

--- apps/creator-app/src/pages/Canvas/managers/FunctionManager/FunctionStep/Function.step.tsx
+++ apps/creator-app/src/pages/Canvas/managers/FunctionManager/FunctionStep/Function.step.tsx
@@ -35,14 +35,15 @@ export const FunctionStep: ConnectedStep<Realtime.NodeData.Function> = ({ data, 
 
   const hasFunctions = Object.values(functionMap).length > 0;
   const hasPaths = !!paths.length;
-  const placeholder = hasFunctions ? 'Select function' : 'No function added';
+  const emptyPlaceholder = hasFunctions ? 'Select function' : 'No function added';
+  const descriptionPlaceholder = description || undefined;
 
   return (
     <Step nodeID={data.nodeID} dividerOffset={22}>
       <Step.Section v2>
         <Step.Item
           v2
-          placeholder={description ? placeholder : undefined}
+          placeholder={!functionID ? emptyPlaceholder : descriptionPlaceholder}
           prefix={functionID ? <Thumbnail src={image} mr={16} /> : null}
           portID={isLast && !hasPaths ? nextPortID : null}
           icon={functionID ? undefined : 'systemCode'}

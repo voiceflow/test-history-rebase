702073466a77be5fd2edb83b5f3becf4648a24da
Amanda <22350614+amandasteinhauer@users.noreply.github.com>
2024-02-02 14:20:01 -0500

    fix: use adapter

--- apps/creator-app/src/client/knowledge-base.ts
+++ apps/creator-app/src/client/knowledge-base.ts
@@ -2,7 +2,7 @@ import { BaseModels } from '@voiceflow/base-types';
 
 import {
   DBKnowledgeBaseDocument,
-  KnowledgeBaseIntegration,
+  DBKnowledgeBaseIntegration,
   ZendeskCountFilters,
   ZendeskFilters,
   ZendeskFilterUserSegment,
@@ -61,7 +61,7 @@ export const knowledgeBaseClient = {
       .then(({ data }) => data),
 
   getAllIntegrations: (projectID: string) =>
-    apiV3.fetch.get<KnowledgeBaseIntegration[]>(`/projects/${projectID}/knowledge-base/integrations`).then(({ data }) => data),
+    apiV3.fetch.get<DBKnowledgeBaseIntegration[]>(`/projects/${projectID}/knowledge-base/integrations`).then(({ data }) => data),
 
   importIntegration: (projectID: string, integrationType: string, data: { filters: ZendeskCountFilters; refreshRate: string }) =>
     apiV3.fetch.post(`/projects/${projectID}/knowledge-base/integrations/${integrationType}`, data),

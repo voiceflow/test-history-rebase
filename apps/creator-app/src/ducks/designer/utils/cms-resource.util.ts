0191ce0128aee8748436238fe2aa3714cea4e0a4
Filipe Merker <filipe.merker@voiceflow.com>
2024-02-15 15:16:14 -0300

    feat: finish review

--- apps/creator-app/src/ducks/designer/utils/cms-resource.util.ts
+++ apps/creator-app/src/ducks/designer/utils/cms-resource.util.ts
@@ -4,6 +4,7 @@ import { match } from 'ts-pattern';
 import { CMSRoute } from '@/config/routes';
 
 import * as Entity from '../entity';
+import * as Flow from '../flow';
 import * as Function from '../function';
 import * as Intent from '../intent';
 import * as KnowledgeBase from '../knowledge-base';
@@ -15,6 +16,7 @@ export const getCMSResourceOneByIDSelector = (scope: FolderScope) =>
     .with(FolderScope.INTENT, () => Intent.selectors.oneByID)
     .with(FolderScope.FUNCTION, () => Function.selectors.oneByID)
     .with(FolderScope.VARIABLE, () => Variable.selectors.oneByID)
+    .with(FolderScope.FLOW, () => Flow.selectors.oneByID)
     .with(FolderScope.KNOWLEDGE_BASE, () => KnowledgeBase.Document.selectors.oneByID)
     .otherwise(() => {
       throw new Error('Unsupported scope');
@@ -26,6 +28,7 @@ export const getCMSResourceCountSelector = (scope: FolderScope) =>
     .with(FolderScope.INTENT, () => Intent.selectors.countWithoutFallback)
     .with(FolderScope.FUNCTION, () => Function.selectors.count)
     .with(FolderScope.VARIABLE, () => Variable.selectors.count)
+    .with(FolderScope.FLOW, () => Flow.selectors.count)
     .with(FolderScope.KNOWLEDGE_BASE, () => KnowledgeBase.Document.selectors.count)
     .otherwise(() => {
       throw new Error('Unsupported scope');
@@ -37,6 +40,7 @@ export const getCMSResourceAllByIDsSelector = (scope: FolderScope) =>
     .with(FolderScope.INTENT, () => Intent.selectors.allByIDs)
     .with(FolderScope.FUNCTION, () => Function.selectors.allByIDs)
     .with(FolderScope.VARIABLE, () => Variable.selectors.allByIDs)
+    .with(FolderScope.FLOW, () => Flow.selectors.allByIDs)
     .with(FolderScope.KNOWLEDGE_BASE, () => KnowledgeBase.Document.selectors.allByIDs)
     .otherwise(() => {
       throw new Error('Unsupported scope');
@@ -48,6 +52,7 @@ export const getCMSResourceAllByFolderIDsSelector = (scope: FolderScope) =>
     .with(CMSRoute.INTENT, () => Intent.selectors.allByFolderIDs)
     .with(CMSRoute.FUNCTION, () => Function.selectors.allByFolderIDs)
     .with(CMSRoute.VARIABLE, () => Variable.selectors.allByFolderIDs)
+    .with(CMSRoute.FLOW, () => Flow.selectors.allByFolderIDs)
     .with(CMSRoute.KNOWLEDGE_BASE, () => KnowledgeBase.Document.selectors.allByFolderIDs)
     .otherwise(() => {
       throw new Error('Unsupported scope');
--- apps/creator-app/src/pages/AssistantCMS/pages/CMSFlow/components/CMSFlowTableNavigation/CMSFlowTableNavigation.component.tsx
+++ apps/creator-app/src/pages/AssistantCMS/pages/CMSFlow/components/CMSFlowTableNavigation/CMSFlowTableNavigation.component.tsx
@@ -1,20 +1,17 @@
-import { useAtomValue } from 'jotai';
 import React from 'react';
 
 import { CMSTableNavigation } from '@/pages/AssistantCMS/components/CMSTableNavigation/CMSTableNavigation.component';
 
 import { CMSResourceActions } from '../../../../components/CMSResourceActions';
-import { useFlowCMSManager } from '../../CMSFlow.hook';
 
 export const CMSFlowTableNavigation: React.FC = () => {
-  const cmsManager = useFlowCMSManager();
-  const count = useAtomValue(cmsManager.dataToRenderSize);
-
   return (
     <CMSTableNavigation
-      label={`All components (${count})`}
+      label="All components"
       actions={
         <>
+          <CMSResourceActions.CreateFolder />
+          <CMSResourceActions.MoveToFolder />
           <CMSResourceActions.Delete />
         </>
       }

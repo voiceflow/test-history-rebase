64085ae0b83484166057b6195e04aaff0431c4d3
Matheus Poleza <matheus.poleza@voiceflow.com>
2024-02-27 13:59:18 -0300

    fix: fixes conflicts (CV3-846)

--- apps/realtime/src/legacy/services/index.ts
+++ apps/realtime/src/legacy/services/index.ts
@@ -75,11 +75,7 @@ interface Options {
     hashedID: HashedIDService;
     assistant: AssistantService;
     projectList: ProjectListService;
-<<<<<<< HEAD
     organization: OrganizationIdentityService;
-=======
-    organization: OrganizationService;
->>>>>>> master
     requestContext: {
       createAsync: <T>(callback: () => Promise<T>) => Promise<T>;
     };

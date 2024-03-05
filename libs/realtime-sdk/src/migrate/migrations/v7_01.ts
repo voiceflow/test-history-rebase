fc77a8d5fe7d2fe3eb091268a98032cc59f3e583
Tyler Han <tylerhan97@gmail.com>
2024-03-05 08:31:44 -0500

    fix: do not add existing

--- libs/realtime-sdk/src/migrate/migrations/v7_01.ts
+++ libs/realtime-sdk/src/migrate/migrations/v7_01.ts
@@ -12,8 +12,11 @@ const migrateToV7_01: Transform = ({ cms, diagrams }, { project, creatorID }) =>
     diagramVariables.push(...diagram.variables);
   });
 
+  const existingCMSVariableNames = new Set(cms.variables.map((variable) => variable.name));
+  const newVariables = diagramVariables.filter((variable) => !existingCMSVariableNames.has(variable));
+
   // this should only be an upsert, existing cms variables should not be affected
-  cms.variables = variableToLegacyVariableAdapter.mapToDB(Utils.array.unique(diagramVariables), {
+  cms.variables = variableToLegacyVariableAdapter.mapToDB(Utils.array.unique(newVariables), {
     creatorID,
     assistantID: project.id,
     environmentID: project.versionID,

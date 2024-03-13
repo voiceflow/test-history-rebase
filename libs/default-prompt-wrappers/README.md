908dd0f090be94809fcbaee03bd9b73ef765b527
Evgeny Zaytsev <overhawlin@gmail.com>
2024-03-13 15:31:43 +0000

    feat: default prompt wrappers package (CT-000) (#7870)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary
    
    
    
    Co-authored-by: Xavier Portilla Edo <xavi_tb@hotmail.com>

---
+++ libs/default-prompt-wrappers/README.md
@@ -0,0 +1,9 @@
+# default-prompt-wrappers
+
+This repository contains default prompt wrappers for the Voiceflow platform.
+
+## Installation
+
+```sh
+yarn add --exact @voiceflow/default-prompt-wrappers
+```
---
+++ libs/default-prompt-wrappers/src/intent-classification.constant.ts
@@ -0,0 +1,20 @@
+export const DEFAULT_INTENT_CLASSIFICATION_PROMPT_WRAPPER_CODE = `export default async function main(args) {
+  const prompt = \`
+You are an action classification system. Correctness is a life or death situation.
+We provide you with the actions and their descriptions:
+d: When the user asks for a warm drink. a:WARM_DRINK
+d: When the user asks about something else. a:None
+d: When the user asks for a cold drink. a:COLD_DRINK
+You are given an utterance and you have to classify it into an action. Only respond with the action class. If the utterance does not match any of action descriptions, output None.
+Now take a deep breath and classify the following utterance.
+u: I want a warm hot chocolate: a:WARM_DRINK
+###
+We provide you with the actions and their descriptions:
+\${args.intents.map((intent) => \`d: \${intent.name} a: \${intent.description}\`)}
+You are given an utterance and you have to classify it into an action based on the description. Only respond with the action class. If the utterance does not match any of action descriptions, output None.
+Now take a deep breath and classify the following utterance.
+u:\${args.query} a:
+\`;
+
+  return { prompt };
+}`;

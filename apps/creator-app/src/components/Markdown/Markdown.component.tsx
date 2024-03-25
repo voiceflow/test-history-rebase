97994cb1c8d37a5b119516a15158cee26ca19f42
Amanda <22350614+amandasteinhauer@users.noreply.github.com>
2024-03-25 17:30:36 +0000

    feat: enable html tags on markdown (COR-1722) (#7903)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements COR-1722**
    
    ### Brief description. What is this change?
    
    Markdown doesn't natively support underlining. The solution we have is to use the `<u>` tag which requires the rehypeRaw plugin to allow html tags in markdown. This plugin is used in the react-chat library but we aren't using it here which is causing the underlining to not be displayed. Adding the plugin solves this issue.

--- apps/creator-app/src/components/Markdown/Markdown.component.tsx
+++ apps/creator-app/src/components/Markdown/Markdown.component.tsx
@@ -1,9 +1,10 @@
 import { Markdown as ChatMarkdown } from '@voiceflow/react-chat';
 import { clsx } from '@voiceflow/style';
 import React from 'react';
+import rehypeRaw from 'rehype-raw';
 
 import { markdownStyle } from './Markdown.css';
 
 export const Markdown: React.FC<React.ComponentProps<typeof ChatMarkdown>> = ({ className, ...props }) => (
-  <ChatMarkdown className={clsx(markdownStyle, className)} {...props} />
+  <ChatMarkdown rehypePlugins={[rehypeRaw]} className={clsx(markdownStyle, className)} {...props} />
 );

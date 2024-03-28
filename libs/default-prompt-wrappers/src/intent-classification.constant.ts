21377a08f967871992333bb059de048050407fae
theProf <theProf@gerggle.com>
2024-03-28 15:37:31 +0000

    fix: prompt wrapper description and name into correct places (PL-000) (#7917)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    swap intent name and description locations to match up with prompt
    
    Co-authored-by: Greg O'Grady <gregory.ogrady@voiceflow.com>

--- libs/default-prompt-wrappers/src/intent-classification.constant.ts
+++ libs/default-prompt-wrappers/src/intent-classification.constant.ts
@@ -10,7 +10,7 @@ Now take a deep breath and classify the following utterance.
 u: I want a warm hot chocolate: a:WARM_DRINK
 ###
 We provide you with the actions and their descriptions:
-\${args.intents.map((intent) => \`d: \${intent.name} a: \${intent.description}\`)}
+\${args.intents.map((intent) => \`d: \${intent.description} a: \${intent.name}\`)}
 You are given an utterance and you have to classify it into an action based on the description. Only respond with the action class. If the utterance does not match any of action descriptions, output None.
 Now take a deep breath and classify the following utterance.
 u:\${args.query} a:

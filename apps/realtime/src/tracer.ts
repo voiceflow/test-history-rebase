6557226720a72b4b9d42b1c80e55307aa3ca3eaf
edison-vflow <edison.mukadah@voiceflow.com>
2024-04-03 22:39:16 +0000

    feat: enable datadog profiling features (IN-913) (#7918)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    ### Fixes or implements 
    
    * Linear [ticket](https://linear.app/voiceflow/issue/IN-913/enable-datadog-on-creator-app-services)
    
    ### Brief description. What is this change?
    
    * Notion [document](https://www.notion.so/voiceflow/Observability-Monitoring-and-Profiling-Improvements-c8b7ac985abc4cafbeb9c25c05ae26c9) of feature capabilities 
    
    * Enable profiling to help with bottleneck analysis and heap memory analysis 
    * Enable profiling to help with CPU analysis 
    * Enable `logInjection` to autolink APM traces and Datadog logs 
    * Upgrade dd-trace to the latest possible for `node16` , which is `4.33.0` 
    * Add application name so that metric collection and attribution can work correctly for `nodejs` runtime profiling
    
    Together with these changes, there will be env var changes added in doppler.
    These are experimental features and cant be enabled via code 
    
       ```
    DD_PROFILING_EXPERIMENTAL_CPU_ENABLED="true"
       DD_PROFILING_TIMELINE_ENABLED="true"
    ```
    
    Datadog takes a percentage of the traces it captures (Sampling) 
    In order to capture all traces, we can configure dd-trace library constructor
    The change has been implemented but commented out so that it can be enabled when needed.
    
    This will help with troubleshooting an [issue](https://www.notion.so/voiceflow/consig-ai-Latency-Investigations-354c5934b2474985810c1ab41a40d7ea) like the one for consig.ai where we need to capture all traces for at least a given period
    
    
    
    ### Related PRs
    
    - https://github.com/voiceflow/infrastructure/pull/633
    - https://github.com/voiceflow/general-runtime/pull/730
    - https://github.com/voiceflow/platform/pull/930
    
    ### Checklist
    
    - [x] Changes tested on vftest-82 
    
    
    
    Co-authored-by: edison-vflow <127775867+edison-vflow@users.noreply.github.com>

--- apps/realtime/src/tracer.ts
+++ apps/realtime/src/tracer.ts
@@ -1,7 +1,17 @@
 import tracer from 'dd-trace';
 
+import { APP_NAME } from './config';
+
 tracer.init({
+  service: APP_NAME,
   runtimeMetrics: true,
+  profiling: true,
+  logInjection: true,
+  // Datadog samples traces it captures.Uncomment below to capture 100% of samples if this is required for debugging
+  /* ingestion: {
+  // Any traces started will be sampled at 100.00% with a rate limit of 100 per second
+  sampleRate: 1.0,
+}, */
 });
 
 export default tracer;

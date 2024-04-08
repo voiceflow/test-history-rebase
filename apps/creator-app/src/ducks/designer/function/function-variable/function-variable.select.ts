362ece9e155ed57be7fc17c39710e72a1a1fe43a
Evgeny Zaytsev <overhawlin@gmail.com>
2024-04-08 13:42:48 +0000

    fix: utterances order (COR-1765) (#7957)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary

--- apps/creator-app/src/ducks/designer/function/function-variable/function-variable.select.ts
+++ apps/creator-app/src/ducks/designer/function/function-variable/function-variable.select.ts
@@ -2,6 +2,7 @@ import { FunctionVariableKind } from '@voiceflow/dtos';
 import { createSelector } from 'reselect';
 
 import { createSubSelector } from '@/ducks/utils';
+import { sortCreatableCMSResources } from '@/utils/cms.util';
 
 import { createDesignerCRUDSelectors, functionIDParamSelector } from '../../utils/selector.util';
 import * as FunctionSelect from '../function.select';
@@ -12,11 +13,7 @@ const root = createSubSelector(FunctionSelect.root, STATE_KEY);
 export const { hasOneByID, hasAllByIDs, oneByID, getOneByID, allByIDs, getAllByIDs, all, map, count, isEmpty } = createDesignerCRUDSelectors(root);
 
 export const allByFunctionID = createSelector(all, functionIDParamSelector, (functionVariables, functionID) => {
-  return !functionID
-    ? []
-    : functionVariables
-        .filter((functionVariable) => functionVariable.functionID === functionID)
-        .sort((l, r) => new Date(r.createdAt).getTime() - new Date(l.createdAt).getTime());
+  return !functionID ? [] : sortCreatableCMSResources(functionVariables.filter((functionVariable) => functionVariable.functionID === functionID));
 });
 
 export const countByFunctionID = createSelector(allByFunctionID, (functionVariables) => functionVariables.length);
--- apps/creator-app/src/utils/cms.util.ts
+++ apps/creator-app/src/utils/cms.util.ts
@@ -7,3 +7,6 @@ export const getFolderScopeLabel = (name: string) =>
   match(name)
     .with(FolderScope.FLOW, () => 'component')
     .otherwise(() => name);
+
+export const sortCreatableCMSResources = <T extends { id: string; createdAt: string }>(array: T[]) =>
+  [...array].sort((l, r) => new Date(r.createdAt).getTime() - new Date(l.createdAt).getTime() || r.id.localeCompare(l.id));

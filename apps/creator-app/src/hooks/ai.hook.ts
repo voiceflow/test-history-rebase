2b7990dd2b1249a0f17089ac16a2068749bb6075
Josh <josh@voiceflow.com>
2024-04-10 08:49:23 +0000

    fix: also check assistant_ai flag (PL-923) (#7967)
    create a single centralized hook to check both INTENT_CLASSIFICATION ff and ASSISTANT_AI ff, both should be enabled for any LLM Classification ff check
    
    Co-authored-by: Yauheni <overhawlin@gmail.com>

--- apps/creator-app/src/hooks/ai.hook.ts
+++ apps/creator-app/src/hooks/ai.hook.ts
@@ -1,6 +1,14 @@
 import { DEFAULT_AI_MODEL, DEFAULT_AI_MODEL_PAID_PLAN } from '@voiceflow/dtos';
+import { FeatureFlag } from '@voiceflow/realtime-sdk';
 
 import { useAIModelEntitlement } from './entitlements';
+import { useFeature } from './feature';
+
+export const useIsAIFeaturesEnabled = () => {
+  const assistantAI = useFeature(FeatureFlag.ASSISTANT_AI);
+
+  return assistantAI.isEnabled;
+};
 
 export const useDefaultAIModel = () => {
   const aiModelEntitlement = useAIModelEntitlement();

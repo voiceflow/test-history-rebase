dfac2bb32151866109ada481ba565f009f4e4674
Tyler Stewart <hello@tylerstewart.ca>
2024-04-19 15:47:51 +0000

    feat: use service uri env var (PL-900) (#7944)
    Use `X_SERVICE_URI` instead of `X_SERVICE_HOST`.
    
    
    Co-authored-by: Tyler Stewart <git@tylerstewart.ca>

--- libs/sdk-http-designer/src/nestjs/designer.env.ts
+++ libs/sdk-http-designer/src/nestjs/designer.env.ts
@@ -1,7 +1,7 @@
 import { z } from 'nestjs-zod/z';
 
 export const DesignerEnvironmentVariables = z.object({
-  REALTIME_API_SERVICE_HOST: z.string(),
+  REALTIME_API_SERVICE_URI: z.string(),
   REALTIME_API_SERVICE_PORT: z.string().transform(Number),
 });
 

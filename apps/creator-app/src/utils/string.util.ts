42d1099e8deb35c19567f7dbf848f51c0e6bca49
Evgeny Zaytsev <overhawlin@gmail.com>
2024-04-25 12:13:49 +0000

    feat: improve url regex (COR-1967) (#8024)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements COR-1967**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary

--- apps/creator-app/src/utils/string.util.ts
+++ apps/creator-app/src/utils/string.util.ts
@@ -3,7 +3,118 @@ import { SLOT_REGEXP } from '@voiceflow/common';
 
 export const SPACE_REGEXP = / /g;
 
-const URL_REGEXP_STR = '(((https?:)?\\/\\/)?(www\\.)?[\\w#%+.:=@~-]{2,256}\\.[a-z]{2,10}\\b(([\\w#%&()+./:=?@~,!-]*[^!,.])|([\\w#%&()+./:=?@~-]*)))';
+const DOMAINS_LIST = [
+  'com',
+  'cn',
+  'de',
+  'net',
+  'uk',
+  'org',
+  'nl',
+  'info',
+  'ru',
+  'br',
+  'eu',
+  'fr',
+  'au',
+  'it',
+  'ca',
+  'pl',
+  'top',
+  'biz',
+  'ch',
+  'in',
+  'us',
+  'es',
+  'xyz',
+  'se',
+  'be',
+  'jp',
+  'loan',
+  'dk',
+  'cz',
+  'at',
+  'za',
+  'club',
+  'kr',
+  'pt',
+  'mx',
+  'ir',
+  'ro',
+  'рф',
+  'vip',
+  'no',
+  'online',
+  'hu',
+  'nz',
+  'tw',
+  'win',
+  'cl',
+  'ua',
+  'ar',
+  'nu',
+  'fi',
+  'ltd',
+  'mobi',
+  'vn',
+  'men',
+  'shop',
+  'tr',
+  'site',
+  'sk',
+  'my',
+  'bid',
+  'wang',
+  'work',
+  'pro',
+  'stream',
+  'hk',
+  'ie',
+  'id',
+  'il',
+  'review',
+  'space',
+  'app',
+  'asia',
+  'tech',
+  'website',
+  'lt',
+  'sg',
+  'xin',
+  'store',
+  'live',
+  'life',
+  'trade',
+  'blog',
+  'kz',
+  'name',
+  'cloud',
+  'date',
+  'si',
+  'by',
+  'download',
+  'party',
+  'link',
+  'su',
+  'ee',
+  'ng',
+  'cat',
+  'ren',
+  'pe',
+  'hr',
+  'edu',
+  'gov',
+  'mil',
+  'rs',
+  'io',
+  'chat',
+];
+
+// this regex is copied from the slack source code
+const URL_REGEXP_STR = `([*_]?)((?:([A-Za-z][\\w+.-]{1,50}):(/{1,3})|www[\\.]|(?:[0-9A-Za-z][\\w-]{0,50}\\.(?=([0-9A-Za-z][\\w-]{0,50}\\.){0,20}(?:${DOMAINS_LIST.join(
+  '|'
+)})(?:/|$|\\)|:|\\s))))(?:[^"\\s&()]|\\([^"\\s&()]*\\)|&amp;|&|&quot;)+(?:\\([^"\\s&()]*\\)|[^!"#$%&'()+,\\.:;<=>?@\\[\\]^\`{|}~\\s]))(?=\\1)#?`;
+
 export const URL_ONLY_REGEXP = new RegExp(`^${URL_REGEXP_STR}$`);
 export const ALL_URLS_REGEX = new RegExp(URL_REGEXP_STR, 'g');
 
@@ -21,7 +132,7 @@ export const getURLWithProtocol = (str: string) => {
   return `https://${str}`;
 };
 
-export const isValidURLMatch = (str: string): boolean => getURLWithProtocol(str).match(URL_ONLY_REGEXP) !== null;
+export const isValidURLMatch = (str: string): boolean => str.match(URL_ONLY_REGEXP) !== null;
 
 export const isValidURL = (str: string): boolean => {
   try {

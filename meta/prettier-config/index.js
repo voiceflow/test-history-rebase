cc3322c447411a723d7df7bb9b20390bd6b1af74
Ben Teichman <ben@voiceflow.com>
2024-04-26 15:44:26 -0400

    feat: initial toolchain update

--- meta/prettier-config/index.js
+++
@@ -1,8 +0,0 @@
-const prettierConfig = require("@voiceflow/prettier-config");
-
-/** @type {import('prettier').Options} */
-module.exports = {
-  ...prettierConfig,
-  printWidth: 120,
-  plugins: ["prettier-plugin-packagejson"],
-};

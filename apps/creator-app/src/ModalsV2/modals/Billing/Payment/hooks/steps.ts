a90458d44cb9c2e77d37071fc870e9d05ef82fa6
Vinicius Dacal <viniciusldacal@gmail.com>
2024-04-29 17:58:21 +0000

    feat: fix checkout showing annual value instead of monthly value on plans [bugfix] (COR-1983) (#8051)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements COR-1983**
    
    ### Brief description. What is this change?
     - We should display monthly value on plans list, regardless of the selected period. The selected period will impact in the monthly value, we are accounting for that too.
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary

--- apps/creator-app/src/ModalsV2/modals/Billing/Payment/hooks/steps.ts
+++ apps/creator-app/src/ModalsV2/modals/Billing/Payment/hooks/steps.ts
@@ -1,21 +1,14 @@
-import { BillingPeriod } from '@voiceflow/internal';
 import { useAtom } from 'jotai';
 
 import * as atoms from '../Payment.atoms';
 import { Step } from '../Payment.constants';
-import { usePricing } from './pricing';
 
 export const usePaymentSteps = () => {
   const [activeStep, setActiveStep] = useAtom(atoms.stepAtom);
-  const { onChangePeriod } = usePricing();
 
   const onNext = () => (activeStep === Step.PLAN ? setActiveStep(Step.BILLING) : setActiveStep(Step.PAYMENT));
 
   const onBack = () => {
-    if (activeStep === Step.BILLING) {
-      onChangePeriod(BillingPeriod.MONTHLY);
-    }
-
     setActiveStep(activeStep === Step.BILLING ? Step.PLAN : Step.BILLING);
   };
 

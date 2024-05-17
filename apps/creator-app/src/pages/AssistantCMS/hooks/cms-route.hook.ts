1bded3f77af4db065a88b9591769102c51334e78
Evgeny Zaytsev <overhawlin@gmail.com>
2024-05-17 13:42:57 +0000

    fix: redirects [bugfix] (COR-000) (#8135)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary

--- apps/creator-app/src/pages/AssistantCMS/hooks/cms-route.hook.ts
+++ apps/creator-app/src/pages/AssistantCMS/hooks/cms-route.hook.ts
@@ -1,10 +1,15 @@
+import {FeatureFlag} from '@voiceflow/realtime-sdk';
+
 import { CMSRoute } from '@/config/routes';
 import * as Router from '@/ducks/router';
+import { useFeature } from '@/hooks/feature';
 import { useSessionStorageState } from '@/hooks/storage.hook';
 import { useDispatch } from '@/hooks/store.hook';
 
 export const useCMSRoute = () => {
-  const [activeCMSRoute, updateActiveCMSRoute] = useSessionStorageState<CMSRoute>('cms-active-route', CMSRoute.INTENT);
+  const cmsWorkflows = useFeature(FeatureFlag.CMS_WORKFLOWS)
+
+  const [activeCMSRoute, updateActiveCMSRoute] = useSessionStorageState<CMSRoute>('cms-active-route', cmsWorkflows.isEnabled ? CMSRoute.WORKFLOW : CMSRoute.INTENT);
   const goToCMSResource = useDispatch(Router.goToCMSResource);
 
   const goToActiveCMSRoute = () => goToCMSResource(activeCMSRoute);

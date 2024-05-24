aeffe045762292a8b7fada3ad7242d791100dd07
Gilly Barr <gillyb@gmail.com>
2024-05-24 12:36:35 +0000

    fix: check assistant_ai FF in hook (COR-2136) (#8162)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements COR-2136**
    
    ### Brief description. What is this change?
    
    Seems like the AI features were relying on a workspace setting instead of the feature flag in some places.  
    Changed this to check the feature flag.
    
    ### Checklist
    
    - [ ] changes have been validated in an ephemeral environment
    - [ ] this is a breaking change and should publish a new major version
    - [ ] appropriate tests have been written
    - [ ] any new env vars have been added to the [notion doc](https://www.notion.so/voiceflow/Add-Environment-Variables-be1b0136479f45f1adece7995a7adbfb) and infra has been notified

--- apps/creator-app/src/components/GPT/hooks/feature.ts
+++ apps/creator-app/src/components/GPT/hooks/feature.ts
@@ -10,7 +10,10 @@ import { useActiveWorkspace } from '@/hooks/workspace';
 export const useGPTQuotas = () => {
   const workspace = useActiveWorkspace();
 
-  const quotaData = React.useMemo(() => workspace?.quotas?.find((quota: any) => quota.quotaDetails.name === QuotaNames.TOKENS), [workspace?.quotas]);
+  const quotaData = React.useMemo(
+    () => workspace?.quotas?.find((quota: any) => quota.quotaDetails.name === QuotaNames.TOKENS),
+    [workspace?.quotas]
+  );
 
   return {
     quota: quotaData?.quota ?? 0,
@@ -19,7 +22,8 @@ export const useGPTQuotas = () => {
 };
 
 export const useWorkspaceAIAssist = (): boolean => {
-  return useActiveWorkspace()?.settings?.aiAssist ?? false;
+  const assistantAiFlag = useFeature(Realtime.FeatureFlag.ASSISTANT_AI);
+  return assistantAiFlag.isEnabled;
 };
 
 export const useProjectAIPlayground = (): boolean => {

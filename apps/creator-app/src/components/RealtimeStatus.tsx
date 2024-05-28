e0fcc84c13a69bfc2b4649473e3f04d43f2fa3ca
Evgeny Zaytsev <overhawlin@gmail.com>
2024-05-28 09:21:23 +0000

    fix: handle channel loading errors (COR-1986) (#8181)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements COR-1986**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary

--- apps/creator-app/src/components/RealtimeStatus.tsx
+++ apps/creator-app/src/components/RealtimeStatus.tsx
@@ -1,13 +1,16 @@
+import { datadogRum } from '@datadog/browser-rum';
 import { ChannelDeniedError, status as loguxStatus } from '@logux/client';
-import { toast, useDebouncedCallback, useThrottledCallback } from '@voiceflow/ui';
+import { useDebouncedCallback, useThrottledCallback } from '@voiceflow/ui';
+import { Link, notify } from '@voiceflow/ui-next';
 import React from 'react';
 
+import { GET_HELP_LINK } from '@/constants/link.constant';
 import * as Router from '@/ducks/router';
 import * as Workspace from '@/ducks/workspaceV2';
 import { useDispatch, useRealtimeClient } from '@/hooks/realtime';
 import * as ModalsV2 from '@/ModalsV2';
 
-const isChannelDeniedAction = (action: unknown): action is ChannelDeniedError['action'] =>
+const isChannelErrorAction = (action: unknown): action is ChannelDeniedError['action'] =>
   !!action && typeof (action as ChannelDeniedError['action'])?.action?.channel === 'string';
 
 const RealtimeStatus: React.FC = () => {
@@ -18,15 +21,15 @@ const RealtimeStatus: React.FC = () => {
   const goToDashboard = useDispatch(Router.goToDashboard);
   const goToNextWorkspace = useDispatch(Workspace.goToNextWorkspace);
 
-  const debouncedWarnToast = useDebouncedCallback(300, toast.warn);
+  const debouncedWarnToast = useDebouncedCallback(300, notify.long.warning);
 
   const throttledProjectDenied = useThrottledCallback(1000, () => {
-    debouncedWarnToast(`You don't have access to that project.`);
+    debouncedWarnToast("You don't have access to that project.");
     goToDashboard();
   });
 
   const throttledWorkspaceDenied = useThrottledCallback(3000, () => {
-    debouncedWarnToast(`You don't have access to that workspace.`);
+    debouncedWarnToast("You don't have access to that workspace.");
     goToNextWorkspace();
   });
 
@@ -34,10 +37,41 @@ const RealtimeStatus: React.FC = () => {
     () =>
       loguxStatus(client, (status, details) => {
         switch (status) {
+          case 'error':
+            datadogRum.addError('RealtimeError', details);
+
+            if (details && 'action' in details && isChannelErrorAction(details.action)) {
+              const { channel } = details.action.action;
+              const channelParts = channel.split('/');
+              const resource = channelParts[channelParts.length - 2] ?? channel;
+
+              notify.long.warning(
+                <>
+                  Unable to load {resource}, please try again. If the issue continues,{' '}
+                  <Link inline weight="semiBold" label="contact support" href={GET_HELP_LINK} target="_blank" />.
+                </>,
+                {
+                  autoClose: false,
+                  bodyClassName: 'vfui',
+                  actionButtonProps: {
+                    label: 'Refresh',
+                    onClick: () => window.location.reload(),
+                  },
+                }
+              );
+            }
+
+            break;
+
           case 'denied':
             {
+              datadogRum.addError('RealtimeDenied', details);
+
               const isWorkspace =
-                details && 'action' in details && isChannelDeniedAction(details.action) && details.action.action.channel.startsWith('workspace');
+                details &&
+                'action' in details &&
+                isChannelErrorAction(details.action) &&
+                details.action.action.channel.startsWith('workspace');
 
               if (isWorkspace) {
                 throttledWorkspaceDenied();
@@ -48,6 +82,7 @@ const RealtimeStatus: React.FC = () => {
             break;
 
           case 'protocolError':
+            datadogRum.addError('RealtimeProtocolError', details);
             refreshModal.openVoid({ version: Date.now() });
             refreshModal.preventClose();
             break;

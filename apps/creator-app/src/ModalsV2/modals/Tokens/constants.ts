bfcd51c52e3027e28d1ab10af2273cd0f82949f6
Filipe Merker <filipesmerker@gmail.com>
2024-05-31 19:56:32 +0000

    feat: bump topup tokens [bugfix] (COR-2263) (#8208)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary
    
    
    
    Co-authored-by: Filipe Merker <filipe.merker@voiceflow.com>

--- apps/creator-app/src/ModalsV2/modals/Tokens/constants.ts
+++ apps/creator-app/src/ModalsV2/modals/Tokens/constants.ts
@@ -1,42 +1,45 @@
+// Value is the amount of tokens (in millions) to purchase.
+// It differs from the label because there is some logic in platform that calculates this value.
+
 export const PURCHASE_OPTIONS = [
   {
     value: '1',
-    label: '1 Million tokens ($5)',
+    label: '2 Million tokens ($5)',
   },
   {
     value: '2',
-    label: '2 Million tokens ($10)',
+    label: '4 Million tokens ($10)',
   },
   {
     value: '3',
-    label: '3 Million tokens ($15)',
+    label: '6 Million tokens ($15)',
   },
   {
     value: '4',
-    label: '4 Million tokens ($20)',
+    label: '8 Million tokens ($20)',
   },
   {
     value: '5',
-    label: '5 Million tokens ($25)',
+    label: '10 Million tokens ($25)',
   },
   {
     value: '6',
-    label: '6 Million tokens ($30)',
+    label: '12 Million tokens ($30)',
   },
   {
     value: '7',
-    label: '7 Million tokens ($35)',
+    label: '14 Million tokens ($35)',
   },
   {
     value: '8',
-    label: '8 Million tokens ($40)',
+    label: '16 Million tokens ($40)',
   },
   {
     value: '9',
-    label: '9 Million tokens ($45)',
+    label: '18 Million tokens ($45)',
   },
   {
     value: '10',
-    label: '10 Million tokens ($50)',
+    label: '20 Million tokens ($50)',
   },
 ];

e1596d718d6b5ff5503bdfaecf0e3124aac0e8d2
Peter Jiang <2892084432j@gmail.com>
2024-06-01 12:57:58 +0000

    fix: remove unused deprecated components (ENG-48) (#8206)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Implements ENG-48**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    Remove some of the unused components that are being deprecated from `libs/ui`

--- libs/ui/src/components/CheckboxMultiple/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './Button';
-export { default as ButtonContainer } from './ButtonContainer';
-export { default as Container } from './Container';
-export { default as IconContainer } from './IconContainer';
--- libs/ui/src/components/Disable/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { DisableBox } from './components';
-
-const DisableContainer: React.FC<React.PropsWithChildren<{ disabled: boolean }>> = ({ disabled, children }) => {
-  return <DisableBox disabled={disabled}>{children}</DisableBox>;
-};
-
-export default DisableContainer;

4766eeb80c997e83f97bd1b36d93ea4d1fcfefe4
Matheus Poleza <matheus.poleza@voiceflow.com>
2024-06-10 15:08:11 -0300

    fix: adds path order back again (COR-2306)

--- libs/orm-designer/src/postgres/migrations/Migration20240610141832.ts
+++ libs/orm-designer/src/postgres/migrations/Migration20240610141832.ts
@@ -1,16 +1,17 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20240610141832 extends Migration {
-
   async up(): Promise<void> {
     this.addSql('alter table "designer"."response_variant" drop constraint if exists "response_variant_type_check";');
 
-    this.addSql('alter table "designer"."response" add column "type" text check ("type" in (\'prompt\', \'text\')) null default \'text\';');
-
-    this.addSql('alter table "designer"."function" drop column "path_order";');
+    this.addSql(
+      'alter table "designer"."response" add column "type" text check ("type" in (\'prompt\', \'text\')) null default \'text\';'
+    );
 
     this.addSql('alter table "designer"."response_variant" alter column "type" type text using ("type"::text);');
-    this.addSql('alter table "designer"."response_variant" add constraint "response_variant_type_check" check ("type" in (\'prompt\', \'text\'));');
+    this.addSql(
+      'alter table "designer"."response_variant" add constraint "response_variant_type_check" check ("type" in (\'prompt\', \'text\'));'
+    );
     this.addSql('alter table "designer"."response_variant" alter column "type" set default \'text\';');
     this.addSql('alter table "designer"."response_variant" alter column "type" drop not null;');
   }
@@ -20,12 +21,11 @@ export class Migration20240610141832 extends Migration {
 
     this.addSql('alter table "designer"."response" drop column "type";');
 
-    this.addSql('alter table "designer"."function" add column "path_order" text[] not null default \'{}\';');
-
     this.addSql('alter table "designer"."response_variant" alter column "type" drop default;');
     this.addSql('alter table "designer"."response_variant" alter column "type" type text using ("type"::text);');
-    this.addSql('alter table "designer"."response_variant" add constraint "response_variant_type_check" check ("type" in (\'prompt\', \'text\'));');
+    this.addSql(
+      'alter table "designer"."response_variant" add constraint "response_variant_type_check" check ("type" in (\'prompt\', \'text\'));'
+    );
     this.addSql('alter table "designer"."response_variant" alter column "type" set not null;');
   }
-
 }

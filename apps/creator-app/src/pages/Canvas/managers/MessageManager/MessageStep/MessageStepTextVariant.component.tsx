02644acff9918348d066415d8695e0ed2cdcbdd0
Matheus Poleza <matheus.poleza@voiceflow.com>
2024-06-17 16:46:11 -0300

    fix: fixes step (COR-1945)

--- apps/creator-app/src/pages/Canvas/managers/MessageManager/MessageStep/MessageStepTextVariant.component.tsx
+++ apps/creator-app/src/pages/Canvas/managers/MessageManager/MessageStep/MessageStepTextVariant.component.tsx
@@ -1,4 +1,4 @@
-import { TextResponseVariant } from '@voiceflow/dtos';
+import { ResponseMessage } from '@voiceflow/dtos';
 import { isMarkupEmpty } from '@voiceflow/utils-designer';
 import React from 'react';
 
@@ -8,20 +8,20 @@ import Step from '@/pages/Canvas/components/Step';
 import { markupToSlate } from '@/utils/markup.util';
 
 export interface IResponseStepTextVariantProps {
-  variant: TextResponseVariant;
+  message: ResponseMessage;
   nextPortID?: string;
   palette: HSLShades;
   nodeID: string;
 }
 
-export const ResponseStepTextVariant = ({ variant, nextPortID, palette, nodeID }: IResponseStepTextVariantProps) => {
-  const value = React.useMemo(() => markupToSlate.fromDB(variant.text), [variant.text]);
+export const ResponseStepTextVariant = ({ message, nextPortID, palette, nodeID }: IResponseStepTextVariantProps) => {
+  const value = React.useMemo(() => markupToSlate.fromDB(message.text), [message.text]);
 
   return (
     <Step nodeID={nodeID} dividerOffset={22}>
       <Step.Section v2>
         <Step.Item placeholder="Enter agent says" portID={nextPortID} palette={palette} v2>
-          {isMarkupEmpty(variant.text) ? null : <SlatePreviewWithVariables value={value} />}
+          {isMarkupEmpty(message.text) ? null : <SlatePreviewWithVariables value={value} />}
         </Step.Item>
       </Step.Section>
     </Step>

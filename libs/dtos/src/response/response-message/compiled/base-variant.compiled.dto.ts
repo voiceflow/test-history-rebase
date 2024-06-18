f28b8d3c7431a64c631e7093638a582b9a3bfed9
Zhi Heng (Brennan) Lu <brennan.lu314@gmail.com>
2024-06-18 14:38:30 +0000

    fix: multiple conditions per variant (PL-1042) (#8327)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements PL-1042**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    Fixing a mistyping of the message DTOs. Each message variant has only one condition. The UI is not allowed to define multiple distinct conditions for a single variant.

--- libs/dtos/src/response/response-message/compiled/base-variant.compiled.dto.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-import { CMSBaseResourceDTO } from '@/common';
-import { AnyCompiledConditionDTO } from '@/condition/compiled/condition.compiled.dto';
-
-export const BaseCompiledResponseVariantDTO = CMSBaseResourceDTO.extend({
-  conditions: z.array(AnyCompiledConditionDTO).optional(),
-  data: z.unknown().optional(),
-}).strict();

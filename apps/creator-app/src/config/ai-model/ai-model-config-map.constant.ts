638a8c13d1ca1c6fd1966b554f70846e1b47f6eb
Tyler <tylerhan97@gmail.com>
2024-06-18 14:21:09 +0100

    fix: add ui

--- apps/creator-app/src/config/ai-model/ai-model-config-map.constant.ts
+++ apps/creator-app/src/config/ai-model/ai-model-config-map.constant.ts
@@ -4,9 +4,10 @@ import { AIModel } from '@voiceflow/dtos';
 import { AIModelConfig } from './ai-model.interface';
 import { ANTHROPIC_MODEL_CONFIGS } from './anthropic-model.config';
 import { GOOGLE_MODEL_CONFIGS } from './google-model.config';
+import { GROQ_MODEL_CONFIGS } from './groq-model.config';
 import { OPEN_AI_MODEL_CONFIGS } from './open-ai-model.config';
 
 export const AI_MODEL_CONFIG_MAP = Utils.array.createMap<AIModelConfig, AIModel>(
-  [...OPEN_AI_MODEL_CONFIGS, ...ANTHROPIC_MODEL_CONFIGS, ...GOOGLE_MODEL_CONFIGS],
+  [...OPEN_AI_MODEL_CONFIGS, ...ANTHROPIC_MODEL_CONFIGS, ...GOOGLE_MODEL_CONFIGS, ...GROQ_MODEL_CONFIGS],
   (model) => model.type
 );

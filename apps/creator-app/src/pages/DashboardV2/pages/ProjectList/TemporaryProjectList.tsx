d65021cda5bbf88276ea528404ad26297943532e
Vinicius Dacal <viniciusldacal@gmail.com>
2024-06-19 15:44:15 +0000

    fix: display trial expired lockscreen on kanban view (COR-2413) (#8347)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements COR-2413**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary

--- apps/creator-app/src/pages/DashboardV2/pages/ProjectList/TemporaryProjectList.tsx
+++ apps/creator-app/src/pages/DashboardV2/pages/ProjectList/TemporaryProjectList.tsx
@@ -1,15 +1,30 @@
+import * as Realtime from '@voiceflow/realtime-sdk';
 import React from 'react';
 
+import TrialExpiredPage from '@/components/TrialExpiredPage';
 import * as WorkspaceV2 from '@/ducks/workspaceV2';
+import { useFeature } from '@/hooks/feature';
 import { useSelector } from '@/hooks/redux';
 
 import ProjectList from '.';
 import { KanbanList } from './components';
 
 const TemporaryProjectList: React.FC = () => {
+  const isTrialExpired = useSelector(WorkspaceV2.active.organizationTrialExpiredSelector);
+  const proReverseTrial = useFeature(Realtime.FeatureFlag.PRO_REVERSE_TRIAL);
+  const isEnterprise = useSelector(WorkspaceV2.active.isEnterpriseSelector);
+  const showLockScreen = proReverseTrial.isEnabled && isTrialExpired && !isEnterprise;
+
   const dashboardKanban = useSelector(WorkspaceV2.active.dashboardKanbanSettingsSelector);
 
-  return dashboardKanban ? <KanbanList /> : <ProjectList />;
+  return dashboardKanban ? (
+    <>
+      {showLockScreen && <TrialExpiredPage />}
+      <KanbanList />
+    </>
+  ) : (
+    <ProjectList showLockScreen={showLockScreen} />
+  );
 };
 
 export default TemporaryProjectList;

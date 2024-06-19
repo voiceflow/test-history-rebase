dbb23fb930e35af5c25df80c08a36deac06740fb
Zhi Heng (Brennan) Lu <brennan.lu314@gmail.com>
2024-06-19 18:43:32 +0000

    fix: wrong parent type for condition DTOs (PL-1042) (#8339)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements PL-1042**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    `BaseCompiledCondition` inherits from `CMSBaseResource` which is wrong because the `CMSBaseResource` refers to raw, uncompiled CMS resources and contains unnecessary properties like `id`.

--- libs/dtos/src/condition/compiled/base-condition.compiled.dto.ts
+++ libs/dtos/src/condition/compiled/base-condition.compiled.dto.ts
@@ -1,13 +1,11 @@
 import { z } from 'zod';
 
-import { CMSBaseResourceDTO } from '@/common';
-
 import { ConditionType } from '../condition-type.enum';
 import { CompiledExpressionConditionDataDTO } from './expression-condition/expression-condition-data.compiled.dto';
 import { CompiledPromptConditionDataDTO } from './prompt-condition/prompt-condition-data.compiled.dto';
 import { CompiledScriptConditionDataDTO } from './script-condition/script-condition-data.compiled.dto';
 
-export const BaseCompiledConditionDTO = CMSBaseResourceDTO.extend({
+export const BaseCompiledConditionDTO = z.object({
   type: z.nativeEnum(ConditionType),
   data: z.union([CompiledPromptConditionDataDTO, CompiledScriptConditionDataDTO, CompiledExpressionConditionDataDTO]),
 }).strict();

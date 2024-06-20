f56bc1fb5256617b8717cfc8e1a4bbe4f814bce8
Evgeny Zaytsev <overhawlin@gmail.com>
2024-06-20 20:38:20 +0000

    feat: add ref message type (COR-000) (#8324)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements COR-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary

--- libs/dtos/src/reference/reference-resource/reference-resource-type.enum.ts
+++ libs/dtos/src/reference/reference-resource/reference-resource-type.enum.ts
@@ -3,9 +3,10 @@ import type { Enum } from '@/utils/type/enum.util';
 export const ReferenceResourceType = {
   NODE: 'node',
   INTENT: 'intent',
+  PROMPT: 'prompt',
+  MESSAGE: 'message',
   DIAGRAM: 'diagram',
   FUNCTION: 'function',
-  RESPONSE: 'response',
 } as const;
 
 export type ReferenceResourceType = Enum<typeof ReferenceResourceType>;

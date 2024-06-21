331e97aab3c79e0140c91d1700fde3b2810629ae
Evgeny Zaytsev <overhawlin@gmail.com>
2024-06-21 16:12:33 +0000

    fix: remove unused feature hooks (COR-000) (#8352)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements COR-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary

--- apps/creator-app/src/hooks/feature.ts
+++ apps/creator-app/src/hooks/feature.ts
@@ -1,5 +1,4 @@
 import * as Realtime from '@voiceflow/realtime-sdk';
-import { usePersistFunction } from '@voiceflow/ui';
 import React from 'react';
 import { Tail } from 'reselect/es/types';
 
@@ -11,34 +10,11 @@ export interface Feature {
   isEnabled: boolean;
 }
 
-export const useFeatures = () => {
-  const featureState = React.useContext(FeatureFlagsContext);
-
-  const allFeatures = React.useMemo(() => {
-    Object.entries(featureState).reduce((acc, [featureKey, feature]) => ({ ...acc, [featureKey]: feature.isEnabled }), {});
-  }, [featureState]);
-
-  const enabledFeatures = React.useMemo(() => {
-    return Object.entries(featureState).reduce<string[]>((acc, [featureKey, feature]) => {
-      if (!feature.isEnabled) return acc;
-      return [...acc, featureKey];
-    }, []);
-  }, [featureState]);
-
-  return { allFeatures, enabledFeatures };
-};
-
 export const useFeature = (feature: Realtime.FeatureFlag): Feature => {
   const featureState = React.useContext(FeatureFlagsContext)[feature] ?? { isEnabled: false };
   const { isEnabled } = featureState;
 
   return { isEnabled };
-};
-
-export const useIsFeatureEnabled = (): ((feature: Realtime.FeatureFlag) => boolean) => {
-  const features = React.useContext(FeatureFlagsContext);
-
-  return usePersistFunction((feature: Realtime.FeatureFlag) => features[feature]?.isEnabled ?? false);
 };
 
 export const createUseFeatureSelector =

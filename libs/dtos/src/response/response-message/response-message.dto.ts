e2e7ddb40db0dd478ff1c655b584ff368310d7e4
Matheus Poleza <matheus.poleza@gmail.com>
2024-06-21 21:37:19 +0000

    Revert "refactor: changes response and messages schema (COR-1969)" (#8378)
    …374)"
    
    This reverts commit 9dd5709c6fdeb5ebd52a76aa157adbfe0a1fa7d2.
    
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements COR-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary
    
    
    
    Co-authored-by: Matheus Poleza <matheus.poleza@voiceflow.com>

--- libs/dtos/src/response/response-message/response-message.dto.ts
+++ libs/dtos/src/response/response-message/response-message.dto.ts
@@ -12,7 +12,7 @@ export const ResponseMessageDTO = CMSObjectResourceDTO.partial({
     discriminatorID: z.string(),
     conditionID: z.string().nullable(),
     text: MarkupDTO,
-    delay: z.number().nullable(),
+    delay: z.number().nullable().optional(),
   })
   .strict();
 
--- libs/orm-designer/src/postgres/response/response.entity.ts
+++ libs/orm-designer/src/postgres/response/response.entity.ts
@@ -1,19 +1,8 @@
-import { Entity, Enum, Index, Property, Unique } from '@mikro-orm/core';
-import { ResponseType } from '@voiceflow/dtos';
+import { Entity, Index, Unique } from '@mikro-orm/core';
 
 import { PostgresCMSTabularEntity } from '../common';
 
 @Entity({ tableName: 'designer.response' })
 @Unique({ properties: ['id', 'environmentID'] })
 @Index({ properties: ['environmentID'] })
-export class ResponseEntity extends PostgresCMSTabularEntity {
-  @Enum({
-    items: () => ResponseType,
-    nullable: true,
-    default: ResponseType.MESSAGE,
-  })
-  type?: ResponseType | null;
-
-  @Property({ type: 'boolean', nullable: true, default: false })
-  draft?: boolean | null;
-}
+export class ResponseEntity extends PostgresCMSTabularEntity {}

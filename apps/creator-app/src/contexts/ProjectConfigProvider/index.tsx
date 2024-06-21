4949ee92220e615ad313f78e82f40d40de47ddc5
Evgeny Zaytsev <overhawlin@gmail.com>
2024-06-21 15:43:56 +0000

    fix: cleanup project config overrides (COR-000) (#8351)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements COR-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary

--- apps/creator-app/src/contexts/ProjectConfigProvider/index.tsx
+++ apps/creator-app/src/contexts/ProjectConfigProvider/index.tsx
@@ -3,22 +3,21 @@ import React from 'react';
 
 import * as NLU from '@/config/nlu';
 import * as ProjectV2 from '@/ducks/projectV2';
-import { useFeatures } from '@/hooks/feature';
 import { useSelector } from '@/hooks/redux';
-
-import { FEATURE_FLAGS_OVERRIDES } from './constants';
 
 export const ProjectNLUTypeContext = React.createContext<Platform.Constants.NLUType>(NLU.Base.CONFIG.type);
 export const { Provider: ProjectNLUTypeProvider, Consumer: ProjectNLUTypeConsumer } = ProjectNLUTypeContext;
 
 export const ProjectNLUTypeConfigContext = React.createContext<NLU.Base.Config>(NLU.Base.CONFIG);
-export const { Provider: ProjectNLUTypeConfigProvider, Consumer: ProjectNLUTypeConfigConsumer } = ProjectNLUTypeConfigContext;
+export const { Provider: ProjectNLUTypeConfigProvider, Consumer: ProjectNLUTypeConfigConsumer } =
+  ProjectNLUTypeConfigContext;
 
 export const ProjectPlatformContext = React.createContext<Platform.Constants.PlatformType>(Platform.Base.CONFIG.type);
 export const { Provider: ProjectPlatformProvider, Consumer: ProjectPlatformConsumer } = ProjectPlatformContext;
 
 export const ProjectPlatformConfigContext = React.createContext<Platform.Base.Config>(Platform.Base.CONFIG);
-export const { Provider: ProjectPlatformConfigProvider, Consumer: ProjectPlatformConfigConsumer } = ProjectPlatformConfigContext;
+export const { Provider: ProjectPlatformConfigProvider, Consumer: ProjectPlatformConfigConsumer } =
+  ProjectPlatformConfigContext;
 
 export const ProjectTypeContext = React.createContext<Platform.Constants.ProjectType>(Platform.Base.Type.CONFIG.type);
 export const { Provider: ProjectTypeProvider, Consumer: ProjectTypeConsumer } = ProjectTypeContext;
@@ -30,19 +29,15 @@ export const ProjectConfigProvider: React.FC<React.PropsWithChildren> = ({ child
   const nluType = useSelector(ProjectV2.active.nluTypeSelector);
   const platform = useSelector(ProjectV2.active.platformSelector);
   const projectType = useSelector(ProjectV2.active.projectTypeSelector);
-  const { enabledFeatures } = useFeatures();
 
   const [nluConfig, platformConfig, platformTypeConfig] = React.useMemo(() => {
-    const platformOverride = FEATURE_FLAGS_OVERRIDES.find(
-      (override) => override.featureFlag && enabledFeatures.includes(override.featureFlag) && override.platform === platform
-    );
-    let platformConfiguration = Platform.Config.get(platform);
+    const platformConfiguration = Platform.Config.get(platform);
 
-    if (platformOverride) {
-      platformConfiguration = { ...platformConfiguration, ...platformOverride.config };
-    }
-
-    return [NLU.Config.get(nluType), platformConfiguration, Platform.Config.getTypeConfig({ type: projectType, platform })];
+    return [
+      NLU.Config.get(nluType),
+      platformConfiguration,
+      Platform.Config.getTypeConfig({ type: projectType, platform }),
+    ];
   }, [nluType, platform, projectType]);
 
   return (

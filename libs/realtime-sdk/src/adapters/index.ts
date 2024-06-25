55d9b72c390f51be9870fc67c2618d599406a4ac
Evgeny Zaytsev <overhawlin@gmail.com>
2024-06-25 10:24:33 +0000

    fix: drop producs adapter and types (COR-000) (#8386)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements COR-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary

--- libs/realtime-sdk/src/adapters/index.ts
+++ libs/realtime-sdk/src/adapters/index.ts
@@ -7,7 +7,6 @@ export * as Identity from './identity';
 export * from './legacy/entity-to-legacy-slot.adapter';
 export * from './legacy/intent-to-legacy-intent/intent-to-legacy-intent.adapter';
 export * from './legacy/variable-to-legacy-variable.adapter';
-export { default as productAdapter } from './product';
 export { default as projectAdapter } from './project';
 export { default as projectListAdapter } from './projectList';
 export { slotAdapter, slotSmartAdapter } from './slot';

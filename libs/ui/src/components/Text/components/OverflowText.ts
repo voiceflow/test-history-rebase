9fa5f46fc394095c92cfed4351f84364e7fef1ae
Gilly Barr <gillyb@gmail.com>
2024-06-25 15:32:09 +0000

    fix: hide overflowing text in card/carousel block (COR-2448) (#8383)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements COR-2448**
    
    ### Brief description. What is this change?
    
    Hide the overflowing text in a Card/Carousel block in the canvas.
    
    Instead of using `overflow-x: clip` changed this to use `overflow-x: hidden` since clip doesn't always hide the overflowing text apparently.
    
    ### Checklist
    
    - [ ] changes have been validated in an ephemeral environment
    - [ ] this is a breaking change and should publish a new major version
    - [ ] appropriate tests have been written
    - [ ] any new env vars have been added to the [notion doc](https://www.notion.so/voiceflow/Add-Environment-Variables-be1b0136479f45f1adece7995a7adbfb) and infra has been notified

--- libs/ui/src/components/Text/components/OverflowText.ts
+++ libs/ui/src/components/Text/components/OverflowText.ts
@@ -1,10 +1,9 @@
-import { IS_CHROME } from '@ui/config';
 import { css, styled } from '@ui/styles';
 
 import Text from './Text';
 
 export const overflowTextStyles = css`
-  overflow-x: ${IS_CHROME ? 'clip' : 'hidden'};
+  overflow-x: hidden;
 
   white-space: nowrap;
   text-overflow: ellipsis;

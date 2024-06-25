1d746c28e6e35508b97d7fe471eea1ea4b586214
Evgeny Zaytsev <overhawlin@gmail.com>
2024-06-25 19:45:32 +0000

    fix: reference popper ui (COR-2476) (#8405)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements COR-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    copilot:summary

--- apps/creator-app/src/pages/AssistantCMS/components/CMSTableLinkMenuCell/CMSTableLinkMenuCell.interface.ts
+++ apps/creator-app/src/pages/AssistantCMS/components/CMSTableLinkMenuCell/CMSTableLinkMenuCell.interface.ts
@@ -3,7 +3,6 @@ import type { IconName } from '@voiceflow/icons';
 export interface ICMSTableLinkMenuCell<Item> {
   label: string;
   items: Item[];
-  onClick: VoidFunction;
   updates?: number;
   iconName?: IconName;
   children: (props: { items: Item[] }) => React.ReactNode[];

4709ac04d54f0e60aa2a017223a4464136729b10
Yoyo Yang <yoyo.yang@voiceflow.com>
2024-06-26 16:55:01 -0400

    fix: google request format

--- apps/ml-gateway/src/llm/google/google.util.ts
+++ apps/ml-gateway/src/llm/google/google.util.ts
@@ -3,9 +3,26 @@ import type { AIMessage, AIParams } from '@voiceflow/dtos';
 import { AIMessageRole } from '@voiceflow/dtos';
 
 export const formatRequest = (messages: AIMessage[], params: AIParams): GenerateContentRequest => {
+  const googleMessages = messages.reduce<{ role: AIMessageRole; content: string }[]>((acc, message) => {
+    const previous = acc[acc.length - 1];
+
+    if (previous && previous.role === message.role) {
+      previous.content += `\n${message.content}`;
+    } else {
+      acc.push({ role: message.role, content: message.content });
+    }
+
+    return acc;
+  }, []);
+
+  // Insert artificial user input if the first message is from the assistant as Google requires the first message to be from the user
+  if (googleMessages[0]?.role === AIMessageRole.ASSISTANT) {
+    googleMessages.unshift({ role: AIMessageRole.USER, content: '.' });
+  }
+
   return {
-    contents: messages.map((item, index) => ({
-      role: index === 0 && item.role === AIMessageRole.ASSISTANT ? AIMessageRole.USER : item.role,
+    contents: googleMessages.map((item) => ({
+      role: item.role,
       parts: [{ text: item.content }],
     })),
     systemInstruction: params.system || undefined,
